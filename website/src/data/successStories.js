cb2c524d3c4f3d2af54b00ad7309f36d0c5b6602
Yangshun Tay <tay.yang.shun@gmail.com>
2020-07-19 00:42:10 +0800

    website: update success stories

--- website/src/data/successStories.js
+++ website/src/data/successStories.js
@@ -11,10 +11,10 @@ export default [
         wasn't sure what to expect and where to start. This handbook together
         with the{' '}
         <a
-          href="https://github.com/yangshun/front-end-interview-handbook"
+          href="https://github.com/yangshun/tech-interview-handbook"
           target="_blank"
           rel="noreferrer noopener">
-          Front End Interview Handbook
+          Tech Interview Handbook
         </a>{' '}
         was a great starting point for me. It clearly describes each part of the
         process and has tons of awesome tips and resources. With this handbook
@@ -30,37 +30,10 @@ export default [
     quote: (
       <>
         Before discovering the handbook, I didn't know what to expect for my
-        frontend interviews. Thanks to it, I had a better understanding and even
-        learned new things about frontend development. The handbook is an
+        front end interviews. Thanks to it, I had a better understanding and
+        even learned new things about frontend development. The handbook is an
         immense resource, one that I still use to refresh and reinforce my
         knowledge to tackle interviews confidently!
-      </>
-    ),
-  },
-  {
-    name: 'Kevin Huang',
-    title: 'Software Engineer, Uber',
-    thumbnail: 'https://avatars1.githubusercontent.com/u/25380336?s=460&v=4',
-    quote: (
-      <>
-        The Tech Interview Handbook played a crucial role in the success of my
-        previous job search. The contents are carefully curated and well
-        organized. It served as an excellent roadmap for my interview prep.
-        <br />
-        <br />
-        In addition to the thorough Data Structures and Algorithms section, the
-        handbook also provides a lot of resources on other aspects of the
-        application process that helped me see the tech interviews in a more
-        holistic way. My favorite non-technical part was "Questions To Ask"! I
-        used quite a few insightful questions from there to challenge and
-        impress my interviewers. The results were great!
-        <br />
-        <br />
-        With the help of Tech Interview Handbook, I was able to land offers from
-        Google, Amazon, Uber and several other great companies. Really
-        appreciate Yangshun and other contributors for putting out such quality
-        content for the community. I'd wholeheartedly recommend this handbook to
-        anyone!
       </>
     ),
   },
