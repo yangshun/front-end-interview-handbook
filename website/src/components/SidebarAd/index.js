f520e9f57f0156f839f184e9dad48c9ed1a4800a
Yangshun <tay.yang.shun@gmail.com>
2021-09-02 13:16:06 +0800

    website: use <BrowserOnly> to fix SSR mismatch bug

--- website/src/components/SidebarAd/index.js
+++ website/src/components/SidebarAd/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 import clsx from 'clsx';
 
@@ -15,34 +15,43 @@ const BACKGROUNDS = [
 export default React.memo(function SidebarAd() {
   const backgroundClass =
     BACKGROUNDS[Math.floor(Math.random() * BACKGROUNDS.length)];
-
-  return Math.random() > 0.5 ? (
-    <a
-      className={clsx(styles.container, backgroundClass)}
-      href="https://www.moonchaser.io/?utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_docs_sidebar"
-      target="_blank"
-      rel="noreferrer noopener"
-      onClick={() => {
-        window.gtag('event', 'moonchaser.sidebar.click');
-      }}>
-      <p className={styles.tagline}>
-        <strong>Get paid more.</strong> Receive risk-free salary negotiation
-        help from Moonchaser. You pay nothing unless your offer is increased.
-      </p>
-    </a>
-  ) : (
-    <a
-      className={clsx(styles.container, backgroundClass)}
-      href="https://www.levels.fyi/services/?ref=TechInterviewHandbook&utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_docs_sidebar"
-      target="_blank"
-      rel="noreferrer noopener"
-      onClick={() => {
-        window.gtag('event', 'levelsfyi.sidebar.click');
-      }}>
-      <p className={styles.tagline}>
-        <strong>Get paid, not played.</strong> Chat with former tech recruiters
-        who’ll guide you on exactly what to say to negotiate a higher offer.
-      </p>
-    </a>
+  // Because the SSR and client output can differ and hydration doesn't patch attribute differences,
+  // we'll render this on the browser only.
+  return (
+    <BrowserOnly>
+      {() =>
+        Math.random() > 0.5 ? (
+          <a
+            className={clsx(styles.container, backgroundClass)}
+            href="https://www.moonchaser.io/?utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_docs_sidebar"
+            target="_blank"
+            rel="noreferrer noopener"
+            onClick={() => {
+              window.gtag('event', 'moonchaser.sidebar.click');
+            }}>
+            <p className={styles.tagline}>
+              <strong>Get paid more.</strong> Receive risk-free salary
+              negotiation help from Moonchaser. You pay nothing unless your
+              offer is increased.
+            </p>
+          </a>
+        ) : (
+          <a
+            className={clsx(styles.container, backgroundClass)}
+            href="https://www.levels.fyi/services/?ref=TechInterviewHandbook&utm_source=techinterviewhandbook&utm_medium=referral&utm_content=website_docs_sidebar"
+            target="_blank"
+            rel="noreferrer noopener"
+            onClick={() => {
+              window.gtag('event', 'levelsfyi.sidebar.click');
+            }}>
+            <p className={styles.tagline}>
+              <strong>Get paid, not played.</strong> Chat with former tech
+              recruiters who’ll guide you on exactly what to say to negotiate a
+              higher offer.
+            </p>
+          </a>
+        )
+      }
+    </BrowserOnly>
   );
 });
