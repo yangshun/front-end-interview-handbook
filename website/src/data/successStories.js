bef2673a26e8fe3741832a8b0098bd6a4c469273
Yangshun <tay.yang.shun@gmail.com>
2022-02-07 06:45:11 +0800

    website: add Zhenghao success story

--- website/src/data/successStories.js
+++ website/src/data/successStories.js
@@ -24,6 +24,27 @@ export default [
     ),
   },
   {
+    name: 'Zhenghao He',
+    title: 'Senior Software Engineer, Instacart',
+    thumbnail: 'https://github.com/zhenghaohe.png',
+    quote: (
+      <>
+        There aren't a lot of high quality front end interview prep materials
+        online. That's why the Front End Interview Handbook is my go-to prep
+        resource. With the help of the interview handbook, I was able to easily
+        identify my weak points and work on them. As a result, I managed to get
+        offers from Meta, Amazon, Twitch, Microsoft, Coinbase, etc.
+        <br />
+        <br />
+        I've learnt a few things and blogged about them on{' '}
+        <a href="https://zhenghao.io" rel="noopener">
+          https://zhenghao.io
+        </a>
+        , check them out if you're interested!
+      </>
+    ),
+  },
+  {
     name: 'Kai Li',
     title: 'Software Engineer, Quora',
     thumbnail: 'https://github.com/li-kai.png',
