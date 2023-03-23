8f5600dddb8e401a58b5c7d50b989a02287b45b8
Yangshun <tay.yang.shun@gmail.com>
2023-03-23 13:15:40 +0800

    prisma: add migration to grant default Supabase permissions

---
+++ apps/web/prisma/migrations/20230323051243_grant/migration.sql
@@ -0,0 +1,9 @@
+grant usage on schema public to postgres, anon, authenticated, service_role;
+
+grant all privileges on all tables in schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all functions in schema public to postgres, anon, authenticated, service_role;
+grant all privileges on all sequences in schema public to postgres, anon, authenticated, service_role;
+
+alter default privileges in schema public grant all on tables to postgres, anon, authenticated, service_role;
+alter default privileges in schema public grant all on functions to postgres, anon, authenticated, service_role;
+alter default privileges in schema public grant all on sequences to postgres, anon, authenticated, service_role;
