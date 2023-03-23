9d742b5d7407a61ad33f4b02d03ce020625dd439
Yangshun <tay.yang.shun@gmail.com>
2023-03-23 13:39:01 +0800

    hiring: open to IN as well

--- apps/web/src/components/hiring/utils.ts
+++ apps/web/src/components/hiring/utils.ts
@@ -1,19 +1,20 @@
 const hiringCountryCodes = new Set([
-  'BE',
-  'CZ',
-  'DE',
-  'DK',
-  'ES',
-  'FR',
-  'GB',
-  'IT',
-  'NL',
-  'PL',
-  'PT',
-  'RU',
-  'SE',
-  'CH',
-  'UA',
+  'BE', // Belgium
+  'CH', // Switzerland
+  'CZ', // Czech Republic
+  'DE', // Germany
+  'DK', // Denmark
+  'ES', // Spain
+  'FR', // France
+  'GB', // United Kingdom
+  'IN', // India
+  'IT', // Italy
+  'NL', // Netherlands
+  'PL', // Poland
+  'PT', // Portugal
+  'RU', // Russia
+  'SE', // Sweden
+  'UA', // Ukraine
 ]);
 
 export function isHiringCountry(countryCode: string): boolean {
