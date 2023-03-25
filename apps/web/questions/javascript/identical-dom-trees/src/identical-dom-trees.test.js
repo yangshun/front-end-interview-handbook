9d172562404631f5d44849b9005dcc6fc9dee46b
Zhang Ziqing <69516975+ziqing26@users.noreply.github.com>
2023-03-25 18:50:28 -0500

    qns(js): add test cases for identical dom trees (#23)

--- apps/web/questions/javascript/identical-dom-trees/src/identical-dom-trees.test.js
+++ apps/web/questions/javascript/identical-dom-trees/src/identical-dom-trees.test.js
@@ -34,11 +34,134 @@ describe('isSameTree', () => {
 
   test('nodes with different tagnames', () => {
     const treeA = new DOMParser().parseFromString(
-      `<div>hello</div>`,
+      `<div>Foo</div>`,
       'text/html',
     );
-    const treeB = new DOMParser().parseFromString(`<p>hello</p>`, 'text/html');
+    const treeB = new DOMParser().parseFromString(`<p>Foo</p>`, 'text/html');
 
+    expect(isSameTree(treeA.body, treeB.body)).toBe(false);
+  });
+
+  test('nodes with same attributes', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div class="foo" data-id="123">hello</div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div class="foo" data-id="123">hello</div>`,
+      'text/html',
+    );
+
+    expect(isSameTree(treeA.body, treeB.body)).toBe(true);
+  });
+
+  test('nodes with different attributes', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div class="foo">hello</div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div class="bar">hello</div>`,
+      'text/html',
+    );
+
+    expect(isSameTree(treeA.body, treeB.body)).toBe(false);
+  });
+
+  test('nodes with different styles', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div style="color: red;">hello</div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div style="color: blue;">hello</div>`,
+      'text/html',
+    );
+
+    expect(isSameTree(treeA.body, treeB.body)).toBe(false);
+  });
+
+  test('nodes with different comment and text nodes', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div><span>Foo</span><!-- comment --></div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div><span>Foo</span>text node</div>`,
+      'text/html',
+    );
+
+    expect(isSameTree(treeA.body, treeB.body)).toBe(false);
+  });
+
+  test('nodes with different numbers of children', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div>
+        <span>Foo</span>
+        <p>Para</p>
+      </div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div>
+        <span>Foo</span>
+        <p>Para</p>
+        <div></div>
+      </div>`,
+      'text/html',
+    );
+    expect(isSameTree(treeA.body, treeB.body)).toBe(false);
+  });
+
+  test('nodes with nested elements', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div>
+        <span>Foo</span>
+        <p>Para</p>
+        <div>
+          <span>Bar</span>
+          <p>Para</p>
+        </div>
+      </div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div>
+        <span>Foo</span>
+        <p>Para</p>
+        <div>
+          <span>Bar</span>
+          <p>Para</p>
+        </div>
+      </div>`,
+      'text/html',
+    );
+    expect(isSameTree(treeA.body, treeB.body)).toBe(true);
+  });
+
+  test('nodes with nested elements in different order', () => {
+    const treeA = new DOMParser().parseFromString(
+      `<div>
+        <span>Foo</span>
+        <p>Para</p>
+        <div>
+          <span>Bar</span>
+          <p>Para</p>
+        </div>
+      </div>`,
+      'text/html',
+    );
+    const treeB = new DOMParser().parseFromString(
+      `<div>
+        <div>
+          <p>Para</p>
+          <span>Bar</span>
+        </div>
+        <span>Foo</span>
+        <p>Para</p>
+      </div>`,
+      'text/html',
+    );
     expect(isSameTree(treeA.body, treeB.body)).toBe(false);
   });
 });
