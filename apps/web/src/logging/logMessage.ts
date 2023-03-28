862ec646c8b01112ff5e99416e28324797643fe3
Yangshun <tay.yang.shun@gmail.com>
2023-03-28 21:46:12 +0800

    analytics: shorten client commit SHA

--- apps/web/src/logging/logMessage.ts
+++ apps/web/src/logging/logMessage.ts
@@ -24,7 +24,7 @@ export default async function logMessage({
       body: JSON.stringify({
         level,
         message,
-        sha: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+        sha: (process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? '').slice(0, 8),
         title,
         user_identifier: userIdentifier,
       }),
