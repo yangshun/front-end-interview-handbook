afdb35ed2115ff267436ee5961db80220e7c5886
Yangshun <tay.yang.shun@gmail.com>
2023-03-28 16:01:25 +0800

    analytics: add more GDPR countries

--- apps/web/src/components/hiring/gdprCountries.ts
+++ apps/web/src/components/hiring/gdprCountries.ts
@@ -1,19 +1,36 @@
 const gdprCountryCodes = new Set([
+  'AT', // Austria
   'BE', // Belgium
+  'BG', // Bulgaria
   'CH', // Switzerland
+  'CY', // Cyprus
   'CZ', // Czech Republic
   'DE', // Germany
   'DK', // Denmark
+  'EE', // Estonia
   'ES', // Spain
+  'FI', // Finland
   'FR', // France
   'GB', // United Kingdom
+  'GR', // Greece
+  'HR', // Croatia
+  'HU', // Hungary
+  'IE', // Ireland
+  'IS', // Iceland
   'IT', // Italy
+  'LI', // Liechtenstein
+  'LT', // Lithuania
+  'LU', // Luxembourg
+  'LV', // Latvia
+  'MT', // Malta
   'NL', // Netherlands
+  'NO', // Norway
   'PL', // Poland
   'PT', // Portugal
-  'RU', // Russia
+  'RO', // Romania
   'SE', // Sweden
-  'UA', // Ukraine
+  'SI', // Slovenia
+  'SK', // Slovakia
 ]);
 
 export default gdprCountryCodes;
