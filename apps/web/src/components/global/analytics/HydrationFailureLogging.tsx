bb783e4d6c9191d403f43e799dcff926cdc3b7e7
Yangshun <tay.yang.shun@gmail.com>
2023-03-29 13:10:52 +0800

    analytics: log hydration failure to db

--- apps/web/src/components/global/analytics/HydrationFailureLogging.tsx
+++ apps/web/src/components/global/analytics/HydrationFailureLogging.tsx
@@ -19,6 +19,16 @@ export default function HydrationFailureLogging({ countryCode }: Props) {
         __html: `(function (w) {
         w.setTimeout(function() {
           if (w.__hydrated) { return; }
+          w.fetch('/api/users/events', {
+            body: JSON.stringify({
+              action: 'hydration_failure',
+              payload: { clientCountry: '${countryCode}', url: message: window.location.href },
+              clientSHA: '${
+                process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? ''
+              }'.slice(0, 7) }),
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+          });
           w.fetch('/api/logging/message', {
             body: JSON.stringify({ level: 'error', message: window.location.href, title: 'Unhydrated page for ${countryCode}', sha: '${
           process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? ''
