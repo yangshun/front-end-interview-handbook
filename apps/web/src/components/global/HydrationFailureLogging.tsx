aab0b3dcaf79254eb744d90248d060983e039ce5
Yangshun <tay.yang.shun@gmail.com>
2023-03-29 09:42:10 +0800

    analytics: fix hydration logging

--- apps/web/src/components/global/HydrationFailureLogging.tsx
+++ apps/web/src/components/global/HydrationFailureLogging.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Script from 'next/script';
 import { useEffect } from 'react';
 
 type Props = Readonly<{
@@ -15,20 +14,28 @@ export default function HydrationFailureLogging({ countryCode }: Props) {
   }, []);
 
   return (
-    <Script id="unresponsive-logging">
-      {`(function (w) {
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function (w) {
         w.setTimeout(function() {
           if (w.__hydrated) { return; }
           w.fetch('/api/logging/message', {
             body: JSON.stringify({ level: 'error', message: window.location.href, title: 'Unhydrated page for ${countryCode}', sha: '${
-        process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? ''
-      }'.slice(0, 7) }),
+          process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? ''
+        }'.slice(0, 7) }),
             headers: { 'Content-Type': 'application/json' },
             method: 'POST',
           });
-        }, 5000);
+          ${
+            process.env.NODE_ENV === 'production'
+              ? 'window.location.reload();'
+              : ''
+          }
+        }, 7000);
       })(window);
-    `}
-    </Script>
+    `,
+      }}
+      id="hydration-reload"
+    />
   );
 }
