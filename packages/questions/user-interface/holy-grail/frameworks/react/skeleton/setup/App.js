7c0f8e7869000f00cddb1309c74fdcce9a3a0087
Yangshun <tay.yang.shun@gmail.com>
2023-04-16 13:12:05 +0800

    qns(ui): add react version of holy grail

---
+++ packages/questions/user-interface/holy-grail/frameworks/react/skeleton/setup/App.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <>
+      <header>Header</header>
+      <div>
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </>
+  );
+}
---
+++ packages/questions/user-interface/holy-grail/frameworks/react/solution/setup/App.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <>
+      <header>Header</header>
+      <div className="columns">
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </>
+  );
+}
