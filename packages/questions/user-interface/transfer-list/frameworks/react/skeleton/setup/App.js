73ad05cc91adf32138173e4b6c4261b7aa25b68a
Yangshun <tay.yang.shun@gmail.com>
2023-04-23 11:41:43 +0800

    qns(ui): publish transfer list

---
+++ packages/questions/user-interface/transfer-list/frameworks/react/skeleton/setup/App.js
@@ -0,0 +1,22 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>TypeScript</li>
+        </ul>
+        <ul>
+          <li>React</li>
+          <li>Angular</li>
+          <li>Vue</li>
+          <li>Svelte</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ packages/questions/user-interface/transfer-list/frameworks/react/solution/setup/App.js
@@ -0,0 +1,208 @@
+import { useId, useState } from 'react';
+import './styles.css';
+
+function CheckboxItem({ onChange, label, checked }) {
+  // Let React generate a unique ID for each item so as to maximize
+  // reusability of the component.
+  const id = useId();
+
+  return (
+    <div className="transfer-list__section__items__item">
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={onChange}
+      />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+}
+
+function ItemList({ items, setItems }) {
+  const [newItem, setNewItem] = useState('');
+
+  return (
+    <div className="transfer-list__section">
+      <form
+        onSubmit={(event) => {
+          // Prevent page reload on form submission.
+          event.preventDefault();
+
+          // Trim value.
+          const newItemValue = newItem.trim();
+
+          // No-op if input is empty.
+          if (newItemValue === '') {
+            return;
+          }
+
+          // Add new item to list.
+          const newItems = new Map(items);
+          newItems.set(newItem, false);
+          setItems(newItems);
+
+          setNewItem('');
+        }}>
+        <input
+          type="text"
+          value={newItem}
+          onChange={(event) => {
+            setNewItem(event.target.value);
+          }}
+        />
+      </form>
+      <ul className="transfer-list__section__items">
+        {Array.from(items.entries()).map(
+          ([label, checked]) => (
+            <li key={label}>
+              <CheckboxItem
+                label={label}
+                checked={checked}
+                onChange={() => {
+                  const newItems = new Map(items);
+                  newItems.set(label, !items.get(label));
+                  setItems(newItems);
+                }}
+              />
+            </li>
+          ),
+        )}
+      </ul>
+    </div>
+  );
+}
+
+const DEFAULT_ITEMS_LEFT = [
+  'HTML',
+  'JavaScript',
+  'CSS',
+  'TypeScript',
+];
+const DEFAULT_ITEMS_RIGHT = [
+  'React',
+  'Angular',
+  'Vue',
+  'Svelte',
+];
+
+// Convert the default array of items into a map with the item
+// name as a key and the value as a boolean.
+function generateItemsMap(items) {
+  return new Map(items.map((label) => [label, false]));
+}
+
+// Determine if the list has no selected items.
+function hasNoSelectedItems(items) {
+  return (
+    Array.from(items.values()).filter((val) => Boolean(val))
+      .length === 0
+  );
+}
+
+// Transfer all items from a source list to a destination list.
+function transferAllItems(
+  itemsSrc,
+  setItemsSrc,
+  itemsDst,
+  setItemsDst,
+) {
+  setItemsDst(new Map([...itemsDst, ...itemsSrc]));
+  setItemsSrc(new Map());
+}
+
+// Transfer selected items from a source list to a destination list.
+function transferSelectedItems(
+  itemsSrc,
+  setItemsSrc,
+  itemsDst,
+  setItemsDst,
+) {
+  const newItemsSrc = new Map(itemsSrc);
+  const newItemsDst = new Map(itemsDst);
+
+  // Remove selected items from source list and add to the destination list.
+  itemsSrc.forEach((value, key) => {
+    if (!value) {
+      return;
+    }
+
+    newItemsDst.set(key, value);
+    newItemsSrc.delete(key);
+  });
+  setItemsSrc(newItemsSrc);
+  setItemsDst(newItemsDst);
+}
+
+export default function App() {
+  const [itemsLeft, setItemsLeft] = useState(
+    generateItemsMap(DEFAULT_ITEMS_LEFT),
+  );
+  const [itemsRight, setItemsRight] = useState(
+    generateItemsMap(DEFAULT_ITEMS_RIGHT),
+  );
+
+  return (
+    <div className="transfer-list">
+      <ItemList items={itemsLeft} setItems={setItemsLeft} />
+      <div className="transfer-list__actions">
+        <button
+          aria-label="Transfer all items to left list"
+          disabled={itemsRight.size === 0}
+          onClick={() => {
+            transferAllItems(
+              itemsRight,
+              setItemsRight,
+              itemsLeft,
+              setItemsLeft,
+            );
+          }}>
+          <span aria-hidden={true}>&lt;&lt;</span>
+        </button>
+        <button
+          aria-label="Transfer selected items to left list"
+          disabled={hasNoSelectedItems(itemsRight)}
+          onClick={() => {
+            transferSelectedItems(
+              itemsRight,
+              setItemsRight,
+              itemsLeft,
+              setItemsLeft,
+            );
+          }}>
+          <span aria-hidden={true}>&lt;</span>
+        </button>
+        <button
+          aria-label="Transfer selected items to right list"
+          disabled={hasNoSelectedItems(itemsLeft)}
+          onClick={() => {
+            transferSelectedItems(
+              itemsLeft,
+              setItemsLeft,
+              itemsRight,
+              setItemsRight,
+            );
+          }}>
+          <span aria-hidden={true}>&gt;</span>
+        </button>
+        <button
+          aria-label="Transfer all items to right list"
+          disabled={itemsLeft.size === 0}
+          onClick={() => {
+            transferAllItems(
+              itemsLeft,
+              setItemsLeft,
+              itemsRight,
+              setItemsRight,
+            );
+          }}>
+          <span aria-hidden={true}>&gt;&gt;</span>
+        </button>
+      </div>
+      <ItemList
+        items={itemsRight}
+        setItems={setItemsRight}
+      />
+    </div>
+  );
+}
