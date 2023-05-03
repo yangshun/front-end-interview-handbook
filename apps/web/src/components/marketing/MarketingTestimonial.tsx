af784245265264ab0cc2a1ebbd8357e6f35b0cf4
Yangshun <tay.yang.shun@gmail.com>
2023-05-03 19:28:04 +0800

    marketing: change testimonial appearance

--- apps/web/src/components/marketing/MarketingTestimonial.tsx
+++ apps/web/src/components/marketing/MarketingTestimonial.tsx
@@ -2,41 +2,28 @@ import type { ReactNode } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import Anchor from '~/components/ui/Anchor';
-import Container from '~/components/ui/Container/Container';
 import Heading from '~/components/ui/Heading';
 import Section from '~/components/ui/Heading/HeadingContext';
 
 type Testimonial = Readonly<{
   authorThumbnailUrl?: string;
   authorUrl?: string;
+  key: string;
   name?: string;
   testimonial: ReactNode;
   title: string;
 }>;
 
-type TestimonialItem = Readonly<{ key: string; position: 'left' | 'right' }> &
-  Testimonial;
-
 function useTestimonials() {
   const intl = useIntl();
-  const TESTIMONIALS: ReadonlyArray<TestimonialItem> = [
+  const TESTIMONIALS: ReadonlyArray<Testimonial> = [
     {
       authorThumbnailUrl: '/img/testimonials/larry.jpg',
       authorUrl: 'https://www.linkedin.com/in/larrydalmeida/',
       key: 'larry',
       name: 'Larry Almeida',
-      position: 'left',
       testimonial: (
         <>
-          <FormattedMessage
-            defaultMessage="I got an opportunity to interview for a dream role with a critical
-            frontend team of an online retail giant with over 50m customers and > 5 billion renders per day.
-            A great opportunity to take my career to the next level. The problem — I wasn't ready to interview!"
-            description="Larry Almeida's testimonial"
-            id="c4SvsG"
-          />
-          <br />
-          <br />
           <FormattedMessage
             defaultMessage="I could have spent my time scouring the internet for resources.
             Instead I choose to invest my money wisely. GreatFrontEnd turned out
@@ -67,33 +54,24 @@ function useTestimonials() {
       }),
     },
     {
-      authorThumbnailUrl: '/img/testimonials/ryanlvv.jpg',
-      authorUrl: 'https://www.linkedin.com/in/ryanlvv/',
-      key: 'ryanlvv',
-      name: 'Ryan Van Valkenburg',
-      position: 'right',
+      key: 'india',
       testimonial: (
         <FormattedMessage
-          defaultMessage="GreatFrontEnd has been a great resource for preparing and continuing
-          in the ever-shifting front-end world. Allowing me to brush up on
-          fundamentals, hone new concepts and practices to put my best foot
-          forward in technical ability and confidence to be able to get
-          interviews AND offers."
-          description="Ryan Van Valkenburg's testimonial"
-          id="bGwCko"
+          defaultMessage="Amazing to see such a comprehensive resource for front end dev interviews. Coming from a more traditional backend role I found this website to be a treasure trove of knowledge and it really takes you from a beginner to advanced level."
+          description="Anonymous Front End Engineer's testimonial"
+          id="q4xRCW"
         />
       ),
       title: intl.formatMessage({
-        defaultMessage: 'Senior Software Engineer, Seattle, WA, USA',
-        description: "Ryan Van Valkenburg's job title",
-        id: 'Vqt5YP',
+        defaultMessage: 'Front End Engineer, India',
+        description: "Anonymous Front End Engineer's job title",
+        id: 'B7d1jK',
       }),
     },
     {
       authorUrl: 'https://www.linkedin.com/in/ananddharne/',
       key: 'anand',
       name: 'Anand Dharne',
-      position: 'right',
       testimonial: (
         <FormattedMessage
           defaultMessage="GreatFrontEnd has been a great help to me for prepping front end
@@ -115,7 +93,6 @@ function useTestimonials() {
 
     {
       key: 'delhi',
-      position: 'right',
       testimonial: (
         <FormattedMessage
           defaultMessage="As someone who has given as well as taken a lot of interviews, I can say GreatFrontEnd is a f'king goldmine. I would highly recommend GreatFrontEnd to anyone who is preparing for front end interviews or anyone who just wants to learn some new concepts. The content is well-organized, has some great practical coding challenges, and the system design part is just amazing!!!"
@@ -130,42 +107,31 @@ function useTestimonials() {
       }),
     },
     {
-      key: 'india',
-      position: 'right',
+      authorThumbnailUrl: '/img/testimonials/ryanlvv.jpg',
+      authorUrl: 'https://www.linkedin.com/in/ryanlvv/',
+      key: 'ryanlvv',
+      name: 'Ryan Van Valkenburg',
       testimonial: (
         <FormattedMessage
-          defaultMessage="Amazing to see such a comprehensive resource for front end dev interviews. Coming from a more traditional backend role I found this website to be a treasure trove of knowledge and it really takes you from a beginner to advanced level."
-          description="Anonymous Front End Engineer's testimonial"
-          id="q4xRCW"
+          defaultMessage="GreatFrontEnd has been a great resource for preparing and continuing
+          in the ever-shifting front-end world. Allowing me to brush up on
+          fundamentals, hone new concepts and practices to put my best foot
+          forward in technical ability and confidence to be able to get
+          interviews AND offers."
+          description="Ryan Van Valkenburg's testimonial"
+          id="bGwCko"
         />
       ),
       title: intl.formatMessage({
-        defaultMessage: 'Front End Engineer, India',
-        description: "Anonymous Front End Engineer's job title",
-        id: 'B7d1jK',
-      }),
-    },
-    {
-      key: 'vietnam',
-      position: 'left',
-      testimonial: (
-        <FormattedMessage
-          defaultMessage="You are doing great things for the Front End community. Especially for those who are Junior engineers like me, advanced solutions covering various aspects of front end development help a lot. Thank you to the team."
-          description="Anonymous Software Engineer's testimonial"
-          id="3HUfI+"
-        />
-      ),
-      title: intl.formatMessage({
-        defaultMessage: 'Software Engineer, Vietnam',
-        description: "Anonymous Software Engineer's job title",
-        id: 'd1vCxb',
+        defaultMessage: 'Senior Software Engineer, Seattle, WA, USA',
+        description: "Ryan Van Valkenburg's job title",
+        id: 'Vqt5YP',
       }),
     },
     {
       authorUrl: 'https://www.linkedin.com/in/gouse-basha-7a0902191/',
       key: 'india-2',
       name: 'Gouse Basha',
-      position: 'left',
       testimonial: (
         <FormattedMessage
           defaultMessage="If you are looking for front end interviews, GreatFrontEnd is for you. It has a wide variety of coding questions ranging from HTML, CSS, JavaScript, to React. It also covers formats like functions, algorithms, user interface, quiz etc. If you want to get into top tech companies for front end roles, GreatFrontEnd is best for you."
@@ -179,86 +145,92 @@ function useTestimonials() {
         id: '+7Wuw5',
       }),
     },
+    {
+      key: 'vietnam',
+      testimonial: (
+        <FormattedMessage
+          defaultMessage="You are doing great things for the Front End community. Especially for those who are Junior engineers like me, advanced solutions covering various aspects of front end development help a lot. Thank you to the team."
+          description="Anonymous Software Engineer's testimonial"
+          id="3HUfI+"
+        />
+      ),
+      title: intl.formatMessage({
+        defaultMessage: 'Software Engineer, Vietnam',
+        description: "Anonymous Software Engineer's job title",
+        id: 'd1vCxb',
+      }),
+    },
   ];
 
   return TESTIMONIALS;
-}
-
-function Testimonial({
-  authorUrl,
-  authorThumbnailUrl,
-  name,
-  testimonial,
-  title,
-}: Testimonial) {
-  return (
-    <blockquote>
-      <div className="relative text-base font-medium text-white md:flex-grow md:text-lg">
-        <svg
-          aria-hidden="true"
-          className="text-brand-600 absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform"
-          fill="currentColor"
-          viewBox="0 0 32 32">
-          <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-        </svg>
-        <p className="relative">{testimonial}</p>
-      </div>
-      <div className="mt-4 flex items-start">
-        {authorThumbnailUrl && (
-          <div className="mr-4 inline-flex flex-shrink-0 rounded-full border-2 border-white">
-            <img
-              alt={name}
-              className="h-12 w-12 rounded-full"
-              src={authorThumbnailUrl}
-            />
-          </div>
-        )}
-        <div>
-          {name &&
-            (() => {
-              const nameEl = (
-                <span className="text-base font-medium text-white">{name}</span>
-              );
-
-              if (authorUrl) {
-                return <Anchor href={authorUrl}>{nameEl}</Anchor>;
-              }
-
-              return nameEl;
-            })()}
-          <div className="text-brand-200 text-base font-medium">{title}</div>
-        </div>
-      </div>
-    </blockquote>
-  );
 }
 
 export default function MarketingTestimonial() {
   const testimonials = useTestimonials();
 
   return (
-    <section className="bg-brand-700">
-      <Heading className="sr-only">Testimonials</Heading>
-      <Section>
-        <Container variant="narrow">
-          <div className="grid grid-cols-1 gap-x-24 gap-y-16 py-16 px-4 md:grid-cols-2 md:py-24 md:px-0">
-            <div className="flex flex-col gap-y-16">
-              {testimonials
-                .filter(({ position }) => position === 'left')
-                .map(({ key, ...testimonial }) => (
-                  <Testimonial key={key} {...testimonial} />
-                ))}
-            </div>
-            <div className="flex flex-col gap-y-16">
-              {useTestimonials()
-                .filter(({ position }) => position === 'right')
-                .map(({ key, ...testimonial }) => (
-                  <Testimonial key={key} {...testimonial} />
-                ))}
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <Heading className="sr-only">Testimonials</Heading>
+          <p className="text-center text-3xl font-bold leading-8 tracking-tight text-slate-900 sm:text-4xl md:text-4xl lg:text-5xl">
+            <FormattedMessage
+              defaultMessage="We have helped thousands of Software Engineers"
+              description="Testimonial section subtitle"
+              id="mST7/q"
+            />
+          </p>
+        </div>
+        <Section>
+          <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
+            <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
+              {testimonials.map((testimonial) => (
+                <div
+                  key={testimonial.name}
+                  className="pt-6 sm:inline-block sm:w-full sm:px-3">
+                  <figure className="rounded-2xl border border-slate-200 p-6 text-sm leading-6">
+                    <blockquote className="text-slate-900">
+                      {testimonial.testimonial}
+                    </blockquote>
+                    <figcaption className="mt-4 flex items-center gap-x-4">
+                      {testimonial.authorThumbnailUrl && (
+                        <img
+                          alt=""
+                          className="h-10 w-10 rounded-full bg-slate-50"
+                          src={testimonial.authorThumbnailUrl}
+                        />
+                      )}
+                      <div>
+                        {testimonial.name &&
+                          (() => {
+                            const nameEl = (
+                              <span className="font-semibold text-slate-900">
+                                {testimonial.name}
+                              </span>
+                            );
+
+                            if (testimonial.authorUrl) {
+                              return (
+                                <Anchor href={testimonial.authorUrl}>
+                                  {nameEl}
+                                </Anchor>
+                              );
+                            }
+
+                            return nameEl;
+                          })()}
+                        <div className="text-xs text-slate-600">
+                          {testimonial.title}
+                        </div>
+                      </div>
+                    </figcaption>
+                  </figure>
+                </div>
+              ))}
             </div>
           </div>
-        </Container>
-      </Section>
-    </section>
+        </Section>
+      </div>
+    </div>
   );
 }
