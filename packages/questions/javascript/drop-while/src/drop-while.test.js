a23005541013e7d134af970ac8a406219f6a8b48
Yangshun <tay.yang.shun@gmail.com>
2023-05-05 10:43:35 +0800

    qns(js): fix drop-while and drop-right-while tests

--- packages/questions/javascript/drop-while/src/drop-while.test.js
+++ packages/questions/javascript/drop-while/src/drop-while.test.js
@@ -5,28 +5,24 @@ describe('dropWhile', () => {
   test('should drop elements until predicate returns falsey', () => {
     const array = [1, 2, 3, 4, 5];
     const predicate = (value) => value < 3;
-    const result = dropWhile(array, predicate);
-    expect(result).toEqual([3, 4, 5]);
+    expect(dropWhile(array, predicate)).toEqual([3, 4, 5]);
   });
 
   test('should return empty array if predicate always returns truthy', () => {
     const array = [1, 2, 3, 4, 5];
     const predicate = () => true;
-    const result = dropWhile(array, predicate);
-    expect(result).toEqual([]);
+    expect(dropWhile(array, predicate)).toEqual([]);
   });
 
   test('should return entire array if predicate always returns falsey', () => {
     const array = [1, 2, 3, 4, 5];
     const predicate = () => false;
-    const result = dropWhile(array, predicate);
-    expect(result).toEqual([1, 2, 3, 4, 5]);
+    expect(dropWhile(array, predicate)).toEqual([1, 2, 3, 4, 5]);
   });
 
   test('should handle sparse arrays', () => {
     const array = [1, , 3, 4, 5];
     const predicate = (value) => value === undefined;
-    const result = dropWhile(array, predicate);
-    expect(result).toEqual([1, , 3, 4, 5]);
+    expect(dropWhile(array, predicate)).toEqual([1, , 3, 4, 5]);
   });
 });
