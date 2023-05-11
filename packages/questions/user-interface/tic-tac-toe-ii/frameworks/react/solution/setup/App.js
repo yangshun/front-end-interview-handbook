fb18804bfbdf1f7a34bcecc5b7aaac78ca4f8c13
Yangshun <tay.yang.shun@gmail.com>
2023-05-11 20:16:38 +0800

    qns(ui): add tic-tac-toe qns

---
+++ packages/questions/user-interface/tic-tac-toe-ii/frameworks/react/solution/setup/App.js
@@ -0,0 +1,215 @@
+import { useCallback, useState, useEffect } from 'react';
+
+import './styles.css';
+
+export default function App() {
+  return <TicTacToe n={5} m={4} />;
+}
+
+function Cell({ index, disabled, mark, turn, onClick }) {
+  return (
+    <button
+      aria-label={
+        mark == null
+          ? `Mark cell ${index} as ${turn}`
+          : undefined
+      }
+      className="cell"
+      disabled={disabled}
+      onClick={onClick}>
+      <span aria-hidden={true}>{mark}</span>
+    </button>
+  );
+}
+
+function TicTacToe({ n, m }) {
+  const [board, setBoard] = useState(
+    Array(n * n).fill(null),
+  );
+  const [xIsPlaying, setIsXPlaying] = useState(true);
+  const [winner, setWinner] = useState(null);
+
+  const onReset = useCallback(() => {
+    setBoard(Array(n * n).fill(null));
+    setIsXPlaying(true);
+    setWinner(null);
+  }, [n]);
+
+  useEffect(() => {
+    onReset();
+  }, [n, m, onReset]);
+
+  if (m > n) {
+    throw Error('Invalid props. `m` must be <= `n`.');
+  }
+
+  function getStatusMessage() {
+    if (winner != null) {
+      return `Player ${winner} wins!`;
+    }
+
+    // All cells have been filled up.
+    if (!board.includes(null)) {
+      return `It's a draw!`;
+    }
+
+    return `Player ${xIsPlaying ? 'X' : 'O'} turn`;
+  }
+
+  return (
+    <div className="app">
+      <div aria-live="polite">{getStatusMessage()}</div>
+      <div
+        className="board"
+        style={{
+          gridTemplateColumns: `repeat(${n}, 1fr)`,
+        }}>
+        {Array(n * n)
+          .fill(null)
+          .map((_, index) => index)
+          .map((cellIndex) => {
+            const turn = xIsPlaying ? 'X' : 'O';
+            return (
+              <Cell
+                key={cellIndex}
+                disabled={
+                  board[cellIndex] != null || winner != null
+                }
+                index={cellIndex}
+                mark={board[cellIndex]}
+                turn={turn}
+                onClick={() => {
+                  const newBoard = board.slice();
+                  newBoard[cellIndex] = turn;
+                  setBoard(newBoard);
+                  setIsXPlaying(!xIsPlaying);
+                  setWinner(
+                    determineWinner(
+                      newBoard,
+                      cellIndex,
+                      n,
+                      m,
+                    ),
+                  );
+                }}
+              />
+            );
+          })}
+      </div>
+      <button
+        onClick={() => {
+          if (winner == null) {
+            // Confirm whether to reset the game.
+            const confirm = window.confirm(
+              'Are you sure you want to reset the game?',
+            );
+            if (!confirm) {
+              return;
+            }
+          }
+
+          onReset();
+        }}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+function determineWinner(board, i, n, m) {
+  const row = Math.floor(i / n);
+  const col = i % n;
+
+  // Get row
+  const rowLine = [];
+  for (let i = 0; i < n; i++) {
+    rowLine.push(row * n + i);
+  }
+
+  // Get column
+  const colLine = [];
+  for (let i = 0; i < n; i++) {
+    colLine.push(i * n + col);
+  }
+
+  const leftToRightDiagonalLine = getLeftToRightDiagonal(
+    i,
+    n,
+  );
+  const rightToLeftDiagonalLine = getRightToLeftDiagonal(
+    i,
+    n,
+  );
+
+  const lines = [
+    rowLine,
+    colLine,
+    leftToRightDiagonalLine,
+    rightToLeftDiagonalLine,
+  ];
+
+  for (const line of lines) {
+    let currentWinner = null;
+    let currentCountInARow = 0;
+    for (const i of line) {
+      if (board[i] == null) {
+        currentWinner = null;
+        currentCountInARow = 0;
+        continue;
+      }
+      if (board[i] === currentWinner) {
+        currentCountInARow++;
+      } else {
+        currentWinner = board[i];
+        currentCountInARow = 1;
+      }
+      if (currentCountInARow >= m) {
+        return currentWinner;
+      }
+    }
+  }
+
+  return null;
+}
+
+function getLeftToRightDiagonal(i, n) {
+  const row = Math.floor(i / n);
+  const col = i % n;
+
+  const stepsToStart = Math.min(col, row);
+  const startRow = row - stepsToStart;
+  const startCol = col - stepsToStart;
+  const line = [];
+
+  for (let i = 0; i < n; i++) {
+    const currentRow = startRow + i;
+    const currentCol = startCol + i;
+    if (currentRow >= n || currentCol >= n) {
+      break;
+    }
+    line.push(currentRow * n + currentCol);
+  }
+
+  return line;
+}
+
+function getRightToLeftDiagonal(i, n) {
+  const row = Math.floor(i / n);
+  const col = i % n;
+
+  const stepsToStart = Math.min(n - col - 1, row);
+  const startRow = row - stepsToStart;
+  const startCol = col + stepsToStart;
+  const line = [];
+
+  for (let i = 0; i < n; i++) {
+    const currentRow = startRow + i;
+    const currentCol = startCol - i;
+    if (currentRow >= n || currentCol < 0) {
+      break;
+    }
+    line.push(currentRow * n + currentCol);
+  }
+
+  return line;
+}
---
+++ packages/questions/user-interface/tic-tac-toe/frameworks/react/solution/setup/App.js
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+
+import './styles.css';
+
+// List of cell indices that are 3-in-a-row.
+const CELLS_IN_A_LINE = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// Determine if there's a winner for the board.
+function determineWinner(board) {
+  for (let i = 0; i < CELLS_IN_A_LINE.length; i++) {
+    const [x, y, z] = CELLS_IN_A_LINE[i];
+    // Determine if the cells in a line have the same mark.
+    if (
+      board[x] != null &&
+      board[x] === board[y] &&
+      board[y] === board[z]
+    ) {
+      return board[x];
+    }
+  }
+
+  // No winner yet.
+  return null;
+}
+
+function Cell({ index, disabled, mark, turn, onClick }) {
+  return (
+    <button
+      aria-label={
+        mark == null
+          ? `Mark cell ${index} as ${turn}`
+          : undefined
+      }
+      className="cell"
+      disabled={disabled}
+      onClick={onClick}>
+      <span aria-hidden={true}>{mark}</span>
+    </button>
+  );
+}
+
+export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsPlaying, setIsXPlaying] = useState(true);
+
+  const winner = determineWinner(board);
+
+  function onReset() {
+    setBoard(Array(9).fill(null));
+    setIsXPlaying(true);
+  }
+
+  function getStatusMessage() {
+    if (winner != null) {
+      return `Player ${winner} wins!`;
+    }
+
+    // All cells have been filled up.
+    if (!board.includes(null)) {
+      return `It's a draw!`;
+    }
+
+    return `Player ${xIsPlaying ? 'X' : 'O'} turn`;
+  }
+
+  return (
+    <div className="app">
+      <div aria-live="polite">{getStatusMessage()}</div>
+      <div className="board">
+        {Array(9)
+          .fill(null)
+          .map((_, index) => index)
+          .map((cellIndex) => {
+            const turn = xIsPlaying ? 'X' : 'O';
+            return (
+              <Cell
+                key={cellIndex}
+                disabled={
+                  board[cellIndex] != null || winner != null
+                }
+                index={cellIndex}
+                mark={board[cellIndex]}
+                turn={turn}
+                onClick={() => {
+                  const newBoard = board.slice();
+                  newBoard[cellIndex] = turn;
+                  setBoard(newBoard);
+                  setIsXPlaying(!xIsPlaying);
+                }}
+              />
+            );
+          })}
+      </div>
+      <button
+        onClick={() => {
+          if (winner == null) {
+            // Confirm whether to reset the game.
+            const confirm = window.confirm(
+              'Are you sure you want to reset the game?',
+            );
+            if (!confirm) {
+              return;
+            }
+          }
+
+          onReset();
+        }}>
+        Reset
+      </button>
+    </div>
+  );
+}
