4e2ed17eae48af93abbf6bd3691e00ebc3c141c0
Yangshun <tay.yang.shun@gmail.com>
2023-06-08 16:37:07 +0800

    ui: allow gap spacing prop for examples

--- apps/web/src/components/ui/misc/UIExamplesGroup.tsx
+++ apps/web/src/components/ui/misc/UIExamplesGroup.tsx
@@ -1,17 +1,26 @@
+import clsx from 'clsx';
+
 import Heading from '../Heading';
 import Section from '../Heading/HeadingContext';
+
+type GapSize = 'lg' | 'md';
+
+const gapClasses: Record<GapSize, string> = {
+  lg: 'gap-8',
+  md: 'gap-4',
+};
 
 export default function UIExamplesGroup({
   title,
   children,
-}: Readonly<{ children: React.ReactNode; title: string }>) {
+  gapSize = 'md',
+}: Readonly<{ children: React.ReactNode; gapSize?: GapSize; title: string }>) {
   return (
-    <div className="space-y-4">
-      <Heading className="sm:truncate" level="heading4">
-        {title}
-      </Heading>
+    <div className="grid gap-4">
+      <Heading level="heading2">{title}</Heading>
+      <hr />
       <Section>
-        <div className="space-y-4">{children}</div>
+        <div className={clsx('grid', gapClasses[gapSize])}>{children}</div>
       </Section>
     </div>
   );
