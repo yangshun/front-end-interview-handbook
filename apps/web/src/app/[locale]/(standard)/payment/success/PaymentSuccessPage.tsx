93e0c67ec2d4346b0fbc827e28ae4b35dc827aca
Yangshun <tay.yang.shun@gmail.com>
2023-06-13 09:31:03 +0800

    dark: payment success page

--- apps/web/src/app/[locale]/(standard)/payment/success/PaymentSuccessPage.tsx
+++ apps/web/src/app/[locale]/(standard)/payment/success/PaymentSuccessPage.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 import {
@@ -20,6 +21,13 @@ import DiscordIcon from '~/components/icons/DiscordIcon';
 import Anchor from '~/components/ui/Anchor';
 import Heading from '~/components/ui/Heading';
 import Section from '~/components/ui/Heading/HeadingContext';
+import Text from '~/components/ui/Text';
+import {
+  themeDivideColor,
+  themeLineColor,
+  themeTextInvertColor,
+  themeTextSecondaryColor,
+} from '~/components/ui/theme';
 
 import logEvent from '~/logging/logEvent';
 import logMessage from '~/logging/logMessage';
@@ -128,82 +136,83 @@ export default function PaymentSuccess({ plans }: Props): JSX.Element {
   }, [planSearchParam, plans]);
 
   return (
-    <div className="bg-white">
-      <main className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-xl py-12 sm:py-16">
-          <div className="mb-2 flex justify-center">
-            <CheckBadgeIcon
-              aria-hidden={true}
-              className="text-success h-16 w-16"
-            />
-          </div>
-          <Heading className="text-center" level="heading4">
-            {/* TODO: i18n */}
-            Welcome to the Premium Club!
-          </Heading>
-          <Section>
-            <div className="mt-12">
-              <Heading
-                className="text-base font-semibold text-neutral-500"
-                color="custom"
-                level="custom">
-                {/* TODO: i18n */}
-                Next Steps
-              </Heading>
-              <Section>
-                <ul
-                  className="mt-4 divide-y divide-neutral-200 border-t border-b border-neutral-200"
-                  role="list">
-                  {links.map((link) => (
-                    <li
-                      key={link.title}
-                      className="relative flex items-start space-x-4 py-6">
-                      <div className="flex-shrink-0">
-                        <span className="bg-brand-lightest flex h-12 w-12 items-center justify-center rounded-lg">
-                          <link.icon
-                            aria-hidden="true"
-                            className="text-brand-darker h-6 w-6"
-                          />
-                        </span>
-                      </div>
-                      <div className="min-w-0 flex-1">
-                        {/* TODO: i18n */}
-                        <Heading
-                          className="text-base font-medium"
-                          level="custom">
-                          <span className="focus-within:ring-brand rounded-sm focus-within:ring-2 focus-within:ring-offset-2">
-                            <Anchor
-                              className="focus:outline-none"
-                              href={link.href}
-                              variant="unstyled">
-                              <span
-                                aria-hidden="true"
-                                className="absolute inset-0"
-                              />
-                              {link.title}
-                            </Anchor>
-                          </span>
-                        </Heading>
-                        <Section>
-                          <p className="text-base text-neutral-500">
-                            {link.description}
-                          </p>
-                        </Section>
-                      </div>
-                      <div className="flex-shrink-0 self-center">
-                        <RiArrowRightSLine
-                          aria-hidden="true"
-                          className="h-5 w-5 text-neutral-400"
-                        />
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </Section>
-            </div>
-          </Section>
+    <main className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-xl py-12 sm:py-16">
+        <div className="mb-2 flex justify-center">
+          <CheckBadgeIcon
+            aria-hidden={true}
+            className="text-success h-16 w-16"
+          />
         </div>
-      </main>
-    </div>
+        <Heading className="text-center" level="heading4">
+          {/* TODO: i18n */}
+          Welcome to the Premium Club!
+        </Heading>
+        <Section>
+          <div className="mt-12">
+            <Heading
+              className={clsx('text-base', themeTextSecondaryColor)}
+              color="custom"
+              level="custom">
+              {/* TODO: i18n */}
+              Next Steps
+            </Heading>
+            <Section>
+              <ul
+                className={clsx(
+                  'mt-4 divide-y border-t border-b',
+                  themeLineColor,
+                  themeDivideColor,
+                )}
+                role="list">
+                {links.map((link) => (
+                  <li
+                    key={link.title}
+                    className="relative flex items-start space-x-4 py-6">
+                    <div className="flex-shrink-0">
+                      <span
+                        className={clsx(
+                          'bg-brand flex h-12 w-12 items-center justify-center rounded-lg',
+                          themeTextInvertColor,
+                        )}>
+                        <link.icon aria-hidden="true" className="h-6 w-6" />
+                      </span>
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      {/* TODO: i18n */}
+                      <Heading className="text-base font-medium" level="custom">
+                        <span className="focus-within:ring-brand rounded-sm focus-within:ring-2 focus-within:ring-offset-2">
+                          <Anchor
+                            className="focus:outline-none"
+                            href={link.href}
+                            variant="unstyled">
+                            <span
+                              aria-hidden="true"
+                              className="absolute inset-0"
+                            />
+                            {link.title}
+                          </Anchor>
+                        </span>
+                      </Heading>
+                      <Section>
+                        <Text color="secondary" display="block">
+                          {link.description}
+                        </Text>
+                      </Section>
+                    </div>
+                    <div className="flex-shrink-0 self-center">
+                      <RiArrowRightSLine
+                        aria-hidden="true"
+                        className={clsx('h-5 w-5', themeTextSecondaryColor)}
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </Section>
+          </div>
+        </Section>
+      </div>
+    </main>
   );
 }
--- apps/web/src/components/ui/theme.tsx
+++ apps/web/src/components/ui/theme.tsx
@@ -1,6 +1,9 @@
 import clsx from 'clsx';
 
 export const themeTextInvertColor = clsx('text-white dark:text-neutral-900');
+export const themeTextSecondaryColor = clsx(
+  'text-neutral-600 dark:text-neutral-400',
+);
 export const themeBackgroundColor = clsx('bg-white dark:bg-neutral-950');
 export const themeBackgroundEmphasized = clsx(
   'bg-neutral-50 dark:bg-neutral-900',
