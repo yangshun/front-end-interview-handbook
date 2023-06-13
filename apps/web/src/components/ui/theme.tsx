943f6569b9b28c91537eeba215f489fdd7bed9cf
Yangshun <tay.yang.shun@gmail.com>
2023-06-13 19:01:25 +0800

    ui(theme): add theme variables

--- apps/web/src/components/ui/theme.tsx
+++ apps/web/src/components/ui/theme.tsx
@@ -6,12 +6,22 @@ export const themeTextBrandColor = clsx('text-brand-dark dark:text-brand');
 export const themeTextSecondaryColor = clsx(
   'text-neutral-600 dark:text-neutral-400',
 );
+export const themeTextSecondaryInvertColor = clsx(
+  'text-neutral-400 dark:text-neutral-600',
+);
 export const themeBackgroundColor = clsx('bg-white dark:bg-neutral-950');
+export const themeBackgroundLayerColor = clsx('bg-white dark:bg-neutral-900');
 export const themeBackgroundEmphasized = clsx(
   'bg-neutral-50 dark:bg-neutral-900',
 );
 export const themeBackgroundEmphasizedHover = clsx(
   'hover:bg-neutral-50 dark:hover:bg-neutral-900',
+);
+export const themeBackgroundLayerEmphasized = clsx(
+  'bg-neutral-50 dark:bg-neutral-800',
+);
+export const themeBackgroundLayerEmphasizedHover = clsx(
+  'hover:bg-neutral-50 dark:hover:bg-neutral-800',
 );
 export const themeLineColor = clsx(
   'border-neutral-200 dark:border-neutral-800',
