fcda0e9832fface8344a390f763c87b79a9fc536
Yangshun <tay.yang.shun@gmail.com>
2023-06-26 12:31:23 +0800

    dashboard: allow hiding of onboarding panel

--- apps/web/src/components/questions/listings/auxilliary/QuestionsPreparationOnboarding.tsx
+++ apps/web/src/components/questions/listings/auxilliary/QuestionsPreparationOnboarding.tsx
@@ -1,7 +1,10 @@
-import { FormattedMessage } from 'react-intl';
+import { RiCloseLine } from 'react-icons/ri';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useSessionStorage } from 'usehooks-ts';
 
 import { usePreparationAreas } from '~/data/PreparationAreas';
 
+import Button from '~/components/ui/Button';
 import Card from '~/components/ui/Card';
 import CardContainer from '~/components/ui/Card/CardContainer';
 import Text from '~/components/ui/Text';
@@ -9,49 +12,75 @@ import Text from '~/components/ui/Text';
 import { QuestionCount } from '../stats/QuestionCount';
 
 export default function QuestionsPreparationOnboarding() {
+  const intl = useIntl();
   const areas = usePreparationAreas();
+  const [shouldHide, setShouldHide] = useSessionStorage(
+    'gfe:dashboard-onboarding',
+    false,
+  );
+
+  const areas_ = [
+    {
+      description: areas.coding.description,
+      icon: areas.coding.icon,
+      label: areas.coding.name,
+      value: areas.coding.value,
+    },
+    {
+      description: areas.quiz.description,
+      icon: areas.quiz.icon,
+      label: areas.quiz.name,
+      value: areas.quiz.value,
+    },
+    {
+      description: areas['system-design'].description,
+      icon: areas['system-design'].icon,
+      label: areas['system-design'].name,
+      value: areas['system-design'].value,
+    },
+    {
+      description: areas.behavioral.description,
+      icon: areas.behavioral.icon,
+      label: areas.behavioral.name,
+      value: areas.behavioral.value,
+    },
+  ];
+
+  if (shouldHide) {
+    return null;
+  }
 
   return (
     <CardContainer>
       <Card className="flex flex-col gap-6 p-8" padding={false}>
-        <Text className="max-w-3xl" color="secondary" display="block">
-          <FormattedMessage
-            defaultMessage="We recommend these {numberOfAreas} areas of learning to prepare you holistically for your front end interview rounds. Explore guides and {count}+ practice questions here."
-            description="Message in onboarding card on prepare page"
-            id="nvhCqI"
-            values={{
-              count: QuestionCount,
-              numberOfAreas: 4,
-            }}
+        <div className="flex justify-between gap-x-6">
+          <Text className="max-w-3xl" color="secondary" display="block">
+            <FormattedMessage
+              defaultMessage="We recommend these {numberOfAreas} areas of learning to prepare you holistically for your front end interview rounds. Explore guides and {count}+ practice questions here."
+              description="Message in onboarding card on prepare page"
+              id="nvhCqI"
+              values={{
+                count: QuestionCount,
+                numberOfAreas: areas_.length,
+              }}
+            />
+          </Text>
+          <Button
+            className="-mr-3 -mt-2"
+            icon={RiCloseLine}
+            isLabelHidden={true}
+            label={intl.formatMessage({
+              defaultMessage: 'Close',
+              description: 'Close button label',
+              id: 'PyDwDF',
+            })}
+            size="lg"
+            variant="tertiary"
+            onClick={() => setShouldHide(true)}
           />
-        </Text>
+        </div>
         <div className="grid gap-4 lg:grid-cols-4">
-          {[
-            {
-              description: areas.coding.description,
-              icon: areas.coding.icon,
-              label: areas.coding.name,
-              value: areas.coding.value,
-            },
-            {
-              description: areas.quiz.description,
-              icon: areas.quiz.icon,
-              label: areas.quiz.name,
-              value: areas.quiz.value,
-            },
-            {
-              description: areas['system-design'].description,
-              icon: areas['system-design'].icon,
-              label: areas['system-design'].name,
-              value: areas['system-design'].value,
-            },
-            {
-              description: areas.behavioral.description,
-              icon: areas.behavioral.icon,
-              label: areas.behavioral.name,
-              value: areas.behavioral.value,
-            },
-          ].map(({ icon: Icon, label, description, value }) => (
+          {areas_.map(({ icon: Icon, label, description, value }) => (
             <div
               key={value}
               className="flex flex-col gap-2 rounded-lg bg-neutral-200/40 py-5 px-6 dark:bg-neutral-800/40">
@@ -62,7 +91,7 @@ export default function QuestionsPreparationOnboarding() {
                 </Text>
               </div>
               <Text
-                className="line-clamp-3"
+                className="!line-clamp-3"
                 color="secondary"
                 display="block"
                 size="body2">
