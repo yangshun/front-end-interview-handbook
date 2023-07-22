aad89b00606d947865b3dc4b3c7c82ad417a8c2a
Yangshun <tay.yang.shun@gmail.com>
2023-07-22 10:07:40 +0800

    qns(js): tweaks to event-emitter-ii

--- packages/questions/javascript/event-emitter-ii/src/event-emitter-ii.js
+++ packages/questions/javascript/event-emitter-ii/src/event-emitter-ii.js
@@ -2,40 +2,41 @@ export default class EventEmitter {
   constructor() {
     // Creating objects via {} will include unwanted properties
     // on the prototype (such as `.toString`).
-    this.events = Object.create(null);
-    // Use an incrementing number to unique identify each listener.
+    this._events = Object.create(null);
+    // Use an incrementing number to uniquely identify each listener.
     this._key = 0;
   }
 
   on(eventName, listener) {
-    if (!(eventName in this.events)) {
-      this.events[eventName] = {};
+    if (!(eventName in this._events)) {
+      this._events[eventName] = {};
     }
 
     const listenerId = this._key;
-    this.events[eventName][listenerId] = listener;
+    this._events[eventName][listenerId] = listener;
     this._key++;
 
     return {
       // Use arrow function so that `this` is preserved.
       off: () => {
-        delete this.events[eventName][listenerId];
+        delete this._events[eventName][listenerId];
       },
     };
   }
 
   emit(eventName, ...args) {
-    // Early return for non-existing eventNames or events without listeners.
+    // Early return for non-existing eventNames or
+    // events without listeners.
     if (
-      !(eventName in this.events) ||
-      Object.keys(this.events[eventName]).length === 0
+      !(eventName in this._events) ||
+      Object.keys(this._events[eventName]).length === 0
     ) {
       return;
     }
 
     // Make a clone of the listeners in case one of the
     // listeners calls sub.off() and changes the listeners.
-    const listeners = { ...this.events[eventName] };
+    const listeners = { ...this._events[eventName] };
     Object.values(listeners).forEach((listener) => {
       listener.apply(null, args);
     });
--- packages/questions/javascript/event-emitter-ii/src/eventEmitterFunctionPrototype.js
+++ packages/questions/javascript/event-emitter-ii/src/eventEmitterFunctionPrototype.js
@@ -1,24 +1,24 @@
 export default function EventEmitter() {
   // Creating objects via {} will include unwanted properties
   // on the prototype (such as `.toString`).
-  this.events = Object.create(null);
+  this._events = Object.create(null);
   // Use an incrementing number to unique identify each listener.
   this._key = 0;
 }
 
 EventEmitter.prototype.on = function (eventName, listener) {
-  if (!(eventName in this.events)) {
-    this.events[eventName] = {};
+  if (!(eventName in this._events)) {
+    this._events[eventName] = {};
   }
 
   const indexForListener = this._key;
-  this.events[eventName][indexForListener] = listener;
+  this._events[eventName][indexForListener] = listener;
   this._key++;
 
   return {
     // Use arrow function so that `this` is preserved.
     off: () => {
-      delete this.events[eventName][indexForListener];
+      delete this._events[eventName][indexForListener];
     },
   };
 };
@@ -26,15 +26,15 @@ EventEmitter.prototype.on = function (eventName, listener) {
 EventEmitter.prototype.emit = function (eventName, ...args) {
   // Early return for non-existing eventNames or events without listeners.
   if (
-    !(eventName in this.events) ||
-    Object.keys(this.events[eventName]).length === 0
+    !(eventName in this._events) ||
+    Object.keys(this._events[eventName]).length === 0
   ) {
     return;
   }
 
   // Make a clone of the listeners in case one of the listeners
   // calls sub.off() and changes the listeners.
-  const listeners = { ...this.events[eventName] };
+  const listeners = { ...this._events[eventName] };
   Object.values(listeners).forEach((listener) => {
     listener.apply(null, args);
   });
