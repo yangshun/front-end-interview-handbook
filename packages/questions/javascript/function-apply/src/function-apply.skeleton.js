b45567a11ed18719a5bc6ff73e59dea702bdf91d
Yangshun <tay.yang.shun@gmail.com>
2023-07-27 06:21:04 +0800

    qns(js): fix function-apply

--- packages/questions/javascript/function-apply/src/function-apply.skeleton.js
+++ packages/questions/javascript/function-apply/src/function-apply.skeleton.js
@@ -3,6 +3,6 @@
  * @param {Array<any>} args
  * @return {any}
  */
-Function.prototype.myApply = function (thisArg, ...args) {
+Function.prototype.myApply = function (thisArg, args) {
   throw 'Not implemented!';
 };
