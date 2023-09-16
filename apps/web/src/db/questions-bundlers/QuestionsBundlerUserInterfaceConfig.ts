867183ae2fed6be614a4d5c2d9466fda50b76c4d
Yangshun <tay.yang.shun@gmail.com>
2023-09-16 17:08:45 +0800

    qns(ui): add script to generate v2 artificats

--- apps/web/src/db/questions-bundlers/QuestionsBundlerUserInterfaceConfig.ts
+++ apps/web/src/db/questions-bundlers/QuestionsBundlerUserInterfaceConfig.ts
@@ -15,12 +15,22 @@ export const QUESTIONS_OUT_DIR_USER_INTERFACE = path.join(
   'questions',
   'user-interface',
 );
+export const QUESTIONS_OUT_DIR_USER_INTERFACE_V2 = path.join(
+  process.cwd(),
+  'src',
+  '__generated__',
+  'questions',
+  'user-interface-v2',
+);
 
 export function getQuestionSrcPathUserInterface(slug: string) {
   return path.join(QUESTIONS_SRC_DIR_USER_INTERFACE, slug);
 }
 export function getQuestionOutPathUserInterface(slug: string) {
   return path.join(QUESTIONS_OUT_DIR_USER_INTERFACE, slug);
+}
+export function getQuestionOutPathUserInterfaceV2(slug: string) {
+  return path.join(QUESTIONS_OUT_DIR_USER_INTERFACE_V2, slug);
 }
 
 export function getQuestionsListOutFilenameUserInterface(_locale: string) {
--- apps/web/src/scripts/questions-ui-setup.ts
+++ apps/web/src/scripts/questions-ui-setup.ts
@@ -13,6 +13,7 @@ import { readMDXFile } from '../db/questions-bundlers/QuestionsBundler';
 import { readQuestionMetadataUserInterface } from '../db/questions-bundlers/QuestionsBundlerUserInterface';
 import {
   getQuestionOutPathUserInterface,
+  getQuestionOutPathUserInterfaceV2,
   getQuestionSrcPathUserInterface,
   QUESTIONS_SRC_DIR_USER_INTERFACE,
 } from '../db/questions-bundlers/QuestionsBundlerUserInterfaceConfig';
@@ -30,6 +31,12 @@ const SUPPORTED_FRAMEWORKS = new Set<QuestionFramework>(['vanilla', 'react']);
 
 type Setup = Readonly<{
   activeFile?: string;
+  visibleFiles?: Array<string>;
+}>;
+
+type GFEConfig = Readonly<{
+  activeFile?: string;
+  environment: string;
   visibleFiles?: Array<string>;
 }>;
 
@@ -87,7 +94,12 @@ async function generateSetupForQuestion(slug: string) {
   const allFilesForQuestion = glob
     .sync(path.join(frameworksPath, '**/*.*'))
     .filter(
-      (path_) => !(path_.endsWith('mdx') || path_.endsWith('setup.json')),
+      (path_) =>
+        !(
+          path_.endsWith('mdx') ||
+          path_.endsWith('setup.json') ||
+          path_.endsWith('greatfrontend.json')
+        ),
     );
 
   // Group folders for a question by (framework, setup).
@@ -204,6 +216,142 @@ async function generateSetupForQuestion(slug: string) {
   ]);
 }
 
+async function generateSetupForQuestionV2(slug: string) {
+  // TODO: Make this work.
+  // const { default: remarkGfm } = await import('remark-gfm');
+
+  const frameworksPath = path.join(
+    getQuestionSrcPathUserInterface(slug),
+    'frameworks',
+  );
+
+  const allFilesForQuestion = glob
+    .sync(path.join(frameworksPath, '**/*.*'))
+    .filter(
+      (path_) =>
+        !(
+          path_.endsWith('mdx') ||
+          path_.endsWith('setup.json') ||
+          path_.endsWith('greatfrontend.json')
+        ),
+    );
+
+  // Group folders for a question by (framework, setup).
+  const groupedFiles = lodash.groupBy(allFilesForQuestion, (filePath) => {
+    const parts = path.relative(frameworksPath, filePath).split('/');
+
+    return parts[0] + '/' + parts[1];
+  });
+
+  const setups = await Promise.all(
+    Object.entries(groupedFiles).map(async ([key, paths]) => {
+      const parts = key.split('/');
+      const framework = parts[0] as QuestionFramework;
+      const setupType = parts[1] as QuestionUserInterfaceSetupType;
+
+      assert(
+        SUPPORTED_FRAMEWORKS.has(framework),
+        `Unsupported framework: ${framework}`,
+      );
+      // Assert(
+      //   SUPPORTED_SETUP_TYPE.has(setupType),
+      //   `Unsupported setup type: ${setupType}`,
+      // );
+
+      const [writeupMdx, files, workspace] = await Promise.all([
+        // Read either description or solution Markdown file.
+        readMDXFile(
+          path.join(frameworksPath, framework, setupType, 'index.mdx'),
+          {},
+        ),
+        // Read files needed.
+        paths.reduce<Record<string, SandpackFile>>((accFiles, fullPath) => {
+          const relativePath = path.relative(
+            path.join(frameworksPath, framework, setupType, 'setup'),
+            fullPath,
+          );
+          const contents = fs.readFileSync(fullPath).toString();
+
+          // Sandpack requires all file paths to be from the root,
+          // hence we add a leading slash.
+          accFiles['/' + relativePath] = {
+            code: contents,
+          };
+
+          return accFiles;
+        }, {}),
+        // Read greatfrontend.json.
+        (() => {
+          const greatfrontendConfigPath = path.join(
+            frameworksPath,
+            framework,
+            setupType,
+            'setup',
+            'greatfrontend.json',
+          );
+
+          try {
+            return JSON.parse(
+              fs.readFileSync(greatfrontendConfigPath).toString(),
+            ) as GFEConfig;
+          } catch {
+            return null;
+          }
+        })(),
+      ]);
+
+      return {
+        files,
+        framework,
+        setupType,
+        workspace,
+        writeup: writeupMdx,
+      };
+    }),
+  );
+
+  await Promise.all([
+    (async () => {
+      const metadata = await readQuestionMetadataUserInterface(slug);
+
+      const outPath = path.join(
+        getQuestionOutPathUserInterfaceV2(slug),
+        `metadata.json`,
+      );
+
+      fs.mkdirSync(path.dirname(outPath), { recursive: true });
+      fs.writeFileSync(outPath, JSON.stringify(metadata, null, 2));
+    })(),
+    ...setups.map(
+      async ({ framework, setupType, files, writeup, workspace }) => {
+        workspace?.visibleFiles?.forEach((file) => {
+          if (!(file in files)) {
+            throw Error(
+              `Visible file "${file}" not found in files for ${setupType} of ${framework}`,
+            );
+          }
+        });
+
+        const outPath = path.join(
+          getQuestionOutPathUserInterfaceV2(slug),
+          framework,
+          `${setupType}.json`,
+        );
+
+        fs.mkdirSync(path.dirname(outPath), { recursive: true });
+
+        const bundle = {
+          files,
+          workspace,
+          writeup,
+        };
+
+        fs.writeFileSync(outPath, JSON.stringify(bundle, null, 2));
+      },
+    ),
+  ]);
+}
+
 function validateSandpackSetup(
   setup: QuestionUserInterfaceSandpackSetup,
   framework: QuestionFramework,
@@ -228,6 +376,7 @@ export async function generateUserInterfaceQuestionsSetup(): Promise<void> {
   await Promise.all(
     directories.map(async (dirent) => {
       await generateSetupForQuestion(dirent.name);
+      await generateSetupForQuestionV2(dirent.name);
     }),
   );
 }
