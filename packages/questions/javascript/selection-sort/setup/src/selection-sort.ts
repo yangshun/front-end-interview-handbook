49e56abf5eb12363135b211dcae7ff65ea278c1a
Yangshun <tay.yang.shun@gmail.com>
2023-09-22 15:08:21 +0800

    qns(js): migrate selection sort

---
+++ packages/questions/javascript/selection-sort/setup/src/selection-sort.ts
@@ -0,0 +1,23 @@
+export default function selectionSort(arr: Array<number>): Array<number> {
+  // Iterate through the unsorted portion of the array.
+  for (let i = 0; i < arr.length; i++) {
+    // Initialize index of min element to the start
+    // of the unsorted section.
+    let minIndex = i;
+    // Find the min element.
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the found min element with the first element of the
+    // unsorted section.
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+
+    // By the end of each outer loop iteration,
+    // [0, i] are sorted.
+  }
+
+  return arr;
+}
