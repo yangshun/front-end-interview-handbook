31ebc3e67612879e632a61d68f268098262e86bf
Yangshun <tay.yang.shun@gmail.com>
2023-09-22 15:25:43 +0800

    qns(js): migrate topo sort

--- packages/questions/javascript/topological-sort/setup/src/topological-sort.ts
+++ packages/questions/javascript/topological-sort/setup/src/topological-sort.ts
@@ -1,10 +1,61 @@
 export default function topologicalSort(
   graph: Record<string, Array<string>>,
 ): Array<string> {
-  throw 'Not implemented!';
+  // Initialize a Map object to store each node's incoming and outgoing edges,
+  // an array to store the output topological sort order,
+  // and a Queue object to store nodes to be processed
+  const nodes = new Map<string, { in: number; out: Set<string> }>();
+  const queue = new Queue<string>();
+  const order = [];
+
+  // Iterating over all the keys in the input graph object
+  // add each key to the "nodes" Map object
+  // with properties "in" with value 0
+  // and "out" with the value of set of neighboring nodes.
+  Object.keys(graph).forEach((node) => {
+    nodes.set(node, { in: 0, out: new Set(graph[node]) });
+  });
+
+  // Set the Map with the correct `in` values.
+  // Iterating over all the keys in the input graph object,
+  // for each node increase the `in` property of its neighbor node by 1.
+  Object.keys(graph).forEach((node) => {
+    graph[node].forEach((neighbor) => {
+      nodes.get(neighbor)!.in += 1;
+    });
+  });
+
+  // Iterate over the nodes and add all the nodes with `in: 0` to the queue.
+  nodes.forEach((value, node) => {
+    if (value.in === 0) {
+      queue.enqueue(node);
+    }
+  });
+
+  // While queue is not empty.
+  while (queue.length()) {
+    // Dequeue a node from the front of the queue.
+    const node = queue.dequeue()!;
+
+    // For each neighbor of this dequeued node decrease its `in` property by 1,
+    // if the `in` becomes 0, enqueue the neighbor node.
+    nodes.get(node)?.out.forEach((neighbor) => {
+      nodes.get(neighbor)!.in -= 1;
+      if (nodes.get(neighbor)!.in === 0) {
+        queue.enqueue(neighbor);
+      }
+    });
+
+    // Add the dequeued node to the output array.
+    order.push(node);
+  }
+
+  // Return topological-ordered array if it has the same length as
+  // the number of keys in the graph, otherwise there is a cycle
+  // and we return an empty array.
+  return order.length === Object.keys(graph).length ? order : [];
 }
 
-// `Queue` data structure is provided in case you want to use it.
 class Node<T> {
   value: T | undefined;
   next: Node<T> | null;
