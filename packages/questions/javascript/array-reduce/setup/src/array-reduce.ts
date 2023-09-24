3cb5195cc076554009f1ec6013fbb764da4c480c
Yangshun <tay.yang.shun@gmail.com>
2023-09-24 18:49:17 +0800

    qns(js): migrate array-reduce

---
+++ packages/questions/javascript/array-reduce/setup/src/array-reduce.ts
@@ -0,0 +1,31 @@
+interface Array<T> {
+  myReduce<U>(
+    callbackFn: (
+      previousValue: U,
+      currentValue: T,
+      currentIndex: number,
+      array: T[],
+    ) => U,
+    initialValue?: U,
+  ): U;
+}
+
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  const noInitialValue = initialValue === undefined;
+  const len = this.length;
+
+  if (noInitialValue && len === 0) {
+    throw new TypeError('Reduce of empty array with no initial value');
+  }
+
+  let acc = noInitialValue ? this[0] : initialValue;
+  let startingIndex = noInitialValue ? 1 : 0;
+
+  for (let k = startingIndex; k < len; k++) {
+    if (Object.hasOwn(this, k)) {
+      acc = callbackFn(acc, this[k], k, this);
+    }
+  }
+
+  return acc;
+};
