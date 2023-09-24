86ee3a9582312818b5a1686f40bc9b0746fd8039
Yangshun <tay.yang.shun@gmail.com>
2023-09-24 18:45:31 +0800

    qns(js): migrate array-filter

---
+++ packages/questions/javascript/array-filter/setup/src/array-filter.ts
@@ -0,0 +1,25 @@
+interface Array<T> {
+  myFilter(
+    callbackFn: (value: T, index: number, array: Array<T>) => boolean,
+    thisArg?: any,
+  ): Array<T>;
+}
+
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+  const len = this.length;
+  const results = [];
+
+  for (let k = 0; k < len; k++) {
+    // Cache value in case the callback function modifies it.
+    const kValue = this[k];
+    if (
+      // Ignore index if value is not defined for index (e.g. in sparse arrays).
+      Object.hasOwn(this, k) &&
+      callbackFn.call(thisArg, kValue, k, this)
+    ) {
+      results.push(kValue);
+    }
+  }
+
+  return results;
+};
