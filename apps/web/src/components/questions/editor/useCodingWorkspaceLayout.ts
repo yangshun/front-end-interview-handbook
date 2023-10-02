0b852d9655acc9509d9f21530da308e9c4aac2a6
Yangshun <tay.yang.shun@gmail.com>
2023-10-02 10:31:59 +0800

    workspace: delete v1 files

--- apps/web/src/components/questions/editor/useCodingWorkspaceLayout.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export type CodingWorkspaceLayout = 'horizontal' | 'vertical';
-
-export default function useCodingWorkspaceLayout(
-  initialLayout: CodingWorkspaceLayout,
-) {
-  return useState<CodingWorkspaceLayout>(initialLayout);
-}
