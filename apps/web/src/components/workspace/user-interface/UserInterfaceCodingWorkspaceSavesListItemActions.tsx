5d7553215a3e30b78982a28fd0c7163920e13dc1
Yangshun <tay.yang.shun@gmail.com>
2023-10-05 07:14:52 +0800

    workspace(ui): allow save rename

--- apps/web/src/components/workspace/user-interface/UserInterfaceCodingWorkspaceSavesListItemActions.tsx
+++ apps/web/src/components/workspace/user-interface/UserInterfaceCodingWorkspaceSavesListItemActions.tsx
@@ -5,6 +5,7 @@ import {
   RiEditLine,
   RiMoreLine,
 } from 'react-icons/ri';
+import { useIntl } from 'react-intl';
 
 import { trpc } from '~/hooks/trpc';
 
@@ -12,7 +13,9 @@ import ConfirmationDialog from '~/components/common/ConfirmationDialog';
 import { useToast } from '~/components/global/toasts/ToastsProvider';
 import type { QuestionMetadata } from '~/components/questions/common/QuestionsTypes';
 import Button from '~/components/ui/Button';
+import Dialog from '~/components/ui/Dialog';
 import DropdownMenu from '~/components/ui/DropdownMenu';
+import TextInput from '~/components/ui/TextInput';
 
 import type { QuestionUserInterfaceSave } from '@prisma/client';
 
@@ -25,11 +28,36 @@ export default function UserInterfaceCodingWorkspaceSavesListItemActions({
   metadata,
   save,
 }: Props) {
+  const intl = useIntl();
   const { showToast } = useToast();
 
   const userInterfaceSaveDeleteMutation =
     trpc.questionSave.userInterfaceDelete.useMutation();
   const [isDeleting, setIsDeleting] = useState(false);
+
+  const userInterfaceSaveUpdateMutation =
+    trpc.questionSave.userInterfaceUpdate.useMutation();
+  const [isEditing, setIsEditing] = useState(false);
+  // Note that this value will be stale if the name is updated in the background.
+  const [newSaveName, setNewSaveName] = useState(save.name);
+
+  function saveEdits() {
+    userInterfaceSaveUpdateMutation.mutate(
+      {
+        name: newSaveName,
+        saveId: save.id,
+      },
+      {
+        onSuccess: (data) => {
+          setIsEditing(false);
+          showToast({
+            title: `"${save.name}" renamed to "${data?.name}"`,
+            variant: 'info',
+          });
+        },
+      },
+    );
+  }
 
   return (
     <div className="flex justify-end gap-x-2">
@@ -53,7 +81,9 @@ export default function UserInterfaceCodingWorkspaceSavesListItemActions({
         <DropdownMenu.Item
           icon={RiEditLine}
           label="Rename"
-          onClick={() => {}}
+          onClick={() => {
+            setIsEditing(true);
+          }}
         />
         <DropdownMenu.Item
           color="error"
@@ -81,7 +111,7 @@ export default function UserInterfaceCodingWorkspaceSavesListItemActions({
               onSuccess: (data) => {
                 setIsDeleting(false);
                 showToast({
-                  title: `Successfully deleted "${data?.name}"`,
+                  title: `Deleted "${data?.name}"`,
                   variant: 'info',
                 });
               },
@@ -90,6 +120,49 @@ export default function UserInterfaceCodingWorkspaceSavesListItemActions({
         }}>
         This is an irreversible action. Are you sure?
       </ConfirmationDialog>
+
+      <Dialog
+        isShown={isEditing}
+        primaryButton={
+          <Button
+            isDisabled={userInterfaceSaveUpdateMutation.isLoading}
+            isLoading={userInterfaceSaveUpdateMutation.isLoading}
+            label="OK"
+            size="md"
+            variant="primary"
+            onClick={() => saveEdits()}
+          />
+        }
+        secondaryButton={
+          <Button
+            isDisabled={userInterfaceSaveUpdateMutation.isLoading}
+            label={intl.formatMessage({
+              defaultMessage: 'Cancel',
+              description: 'Cancel button label',
+              id: '0GT0SI',
+            })}
+            size="md"
+            variant="secondary"
+            onClick={() => setIsEditing(false)}
+          />
+        }
+        title="Rename"
+        onClose={() => setIsEditing(false)}>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            saveEdits();
+          }}>
+          <TextInput
+            autoFocus={true}
+            isDisabled={userInterfaceSaveUpdateMutation.isLoading}
+            isLabelHidden={true}
+            label="Name"
+            value={newSaveName}
+            onChange={setNewSaveName}
+          />
+        </form>
+      </Dialog>
     </div>
   );
 }
