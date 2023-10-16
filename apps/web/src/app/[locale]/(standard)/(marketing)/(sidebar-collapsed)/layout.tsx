31c427ef58787dc65100dc352214fd23c0c33d0c
Yangshun <tay.yang.shun@gmail.com>
2023-10-16 09:50:17 +0800

    app: separate collapsed layout to a different route group

--- apps/web/src/app/[locale]/(standard)/(marketing)/(sidebar-collapsed)/layout.tsx
+++ apps/web/src/app/[locale]/(standard)/(marketing)/(sidebar-collapsed)/layout.tsx
@@ -4,10 +4,10 @@ type Props = Readonly<{
   children: React.ReactNode;
 }>;
 
-export default function SidebarLayout({ children }: Props) {
+export default function SidebarCollapsedLayout({ children }: Props) {
   return (
     <div className="flex">
-      <SidebarContainer />
+      <SidebarContainer initialCollapsed={true} />
       <div className="relative w-0 grow">{children}</div>
     </div>
   );
---
+++ apps/web/src/components/common/SidebarContainer.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import clsx from 'clsx';
+import { useState } from 'react';
+
+import FooterlessContainerHeight from '~/components/common/FooterlessContainerHeight';
+import Sidebar from '~/components/common/Sidebar';
+import { themeLineColor } from '~/components/ui/theme';
+
+type Props = Readonly<{
+  initialCollapsed?: boolean;
+}>;
+
+export default function SidebarContainer({ initialCollapsed = false }: Props) {
+  const [isCollapsed, setIsCollapsed] = useState(initialCollapsed);
+
+  return (
+    <aside
+      className={clsx(
+        'sticky z-20 hidden shrink-0 overflow-visible border-r md:block',
+        themeLineColor,
+        isCollapsed ? 'w-[68px]' : 'w-60',
+      )}
+      style={{
+        height: FooterlessContainerHeight,
+        top: `var(--navbar-height)`,
+      }}>
+      <Sidebar
+        isCollapsed={isCollapsed}
+        onCollapseChange={() => setIsCollapsed(!isCollapsed)}
+      />
+    </aside>
+  );
+}
