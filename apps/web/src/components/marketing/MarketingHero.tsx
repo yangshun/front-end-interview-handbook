da3eeba9b899f552414c4906e1d85d0fa2154c87
Yangshun <tay.yang.shun@gmail.com>
2023-10-18 15:40:11 +0800

    perf: revert to static particles in hero

--- apps/web/src/components/marketing/MarketingHero.tsx
+++ apps/web/src/components/marketing/MarketingHero.tsx
@@ -64,6 +64,28 @@ function MarketingHeroBackground(props: React.SVGProps<SVGSVGElement>) {
         />
       </mask>
       <g mask="url(#mask0_401_76334)">
+        <circle cx="245" cy="192" fill="#F1F5F9" fillOpacity="0.32" r="2" />
+        <circle cx="250" cy="249" fill="#F1F5F9" fillOpacity="0.32" r="2" />
+        <circle cx="148" cy="333" fill="#F1F5F9" fillOpacity="0.12" r="3" />
+        <circle cx="337" cy="362" fill="#F1F5F9" fillOpacity="0.48" r="2" />
+        <circle cx="366" cy="116" fill="#F1F5F9" fillOpacity="0.48" r="2" />
+        <circle cx="899" cy="188" fill="#F1F5F9" fillOpacity="0.08" r="2" />
+        <circle cx="491" cy="401" fill="#F1F5F9" fillOpacity="0.12" r="2" />
+        <circle cx="491" cy="401" fill="#F1F5F9" fillOpacity="0.32" r="2" />
+        <circle cx="818" cy="154" fill="#F1F5F9" fillOpacity="0.24" r="2" />
+        <circle cx="988" cy="393" fill="#F1F5F9" fillOpacity="0.24" r="2" />
+        <circle cx="995" cy="475" fill="#F1F5F9" fillOpacity="0.32" r="1" />
+        <circle cx="869" cy="414" fill="#F1F5F9" fillOpacity="0.48" r="2" />
+        <circle cx="1088" cy="209" fill="#F1F5F9" fillOpacity="0.48" r="2" />
+        <circle cx="975" cy="127" fill="#F1F5F9" fillOpacity="0.32" r="1" />
+        <circle cx="1259" cy="339" fill="#F1F5F9" fillOpacity="0.12" r="2" />
+        <circle cx="1107" cy="525" fill="#F1F5F9" fillOpacity="0.4" r="1" />
+        <circle cx="866" cy="484" fill="#F1F5F9" fillOpacity="0.32" r="1" />
+        <circle cx="718" cy="428" fill="#F1F5F9" fillOpacity="0.64" r="1" />
+        <circle cx="445" cy="479" fill="#F1F5F9" fillOpacity="0.72" r="1" />
+        <circle cx="384" cy="453" fill="#F1F5F9" fillOpacity="0.16" r="2" />
+        <circle cx="183" cy="498" fill="#F1F5F9" fillOpacity="0.32" r="1" />
+        <circle cx="299" cy="409" fill="#F1F5F9" fillOpacity="0.24" r="1" />
         <g filter="url(#filter0_f_401_76334)">
           <path
             d="M1433.72 442.763C1430.04 439.762 1424.96 439.152 1420.68 441.195C1416.39 443.238 1413.66 447.569 1413.66 452.324V550.894H983.296C976.505 550.894 971 556.41 971 563.215V661.785C971 668.59 976.505 674.106 983.296 674.106H1413.66V772.676C1413.66 777.431 1416.39 781.762 1420.68 783.805C1424.96 785.848 1430.04 785.238 1433.72 782.237L1630.46 622.061C1633.33 619.722 1635 616.209 1635 612.5C1635 608.791 1633.33 605.278 1630.46 602.939L1433.72 442.763Z"
@@ -216,9 +238,6 @@ export default function MarketingHero() {
         aria-hidden="true"
         className="pointer-events-none absolute -z-10 -mb-28 -mt-28 flex h-[calc(100%_+_112px)] w-full justify-center overflow-hidden rounded-b-3xl lg:rounded-b-[48px]">
         <MarketingHeroBackground className="h-full min-w-[1200px]" />
-        <div className="absolute h-full w-full">
-          <MarketingParticles className="relative h-full w-full" />
-        </div>
       </div>
       <div className="relative pb-8 pt-0 sm:pb-16 md:pb-20">
         <div className="mx-auto max-w-7xl px-4 pt-16 sm:px-6 lg:pt-24">
