287d0474b91b92e9d440aae449354c22b58fa48e
Yangshun <tay.yang.shun@gmail.com>
2023-11-25 16:32:18 +0800

    mdx: don't open anchor hrefs in a new window

--- apps/web/src/components/mdx/MDXLink.tsx
+++ apps/web/src/components/mdx/MDXLink.tsx
@@ -7,5 +7,12 @@ export default function MDXLink({
   ref: _ref, // TODO: Ignore for now since MDX won't pass it in.
   ...props
 }: ComponentProps<'a'>) {
-  return <Anchor href={href} target="_blank" {...props} />;
+  return (
+    <Anchor
+      href={href}
+      // Don't open in external page if anchor link within page.
+      target={href?.startsWith('#') ? undefined : '_blank'}
+      {...props}
+    />
+  );
 }
