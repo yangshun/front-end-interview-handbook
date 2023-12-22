f3b2a53cf6f46af53f5fb1ae1bea9477cc84b62a
Wei Qing <48304907+weiquu@users.noreply.github.com>
2023-12-22 11:22:26 +0800

    [web] rewards: add db table and update rewards trpc (#197)

--- apps/web/src/server/routers/rewards.ts
+++ apps/web/src/server/routers/rewards.ts
@@ -1,6 +1,8 @@
 import { Octokit } from 'octokit';
 import { z } from 'zod';
 
+import prisma from '~/server/prisma';
+
 import { router, userProcedure } from '../trpc';
 
 // ID for https://github.com/greatfrontend/awesome-front-end-system-design
@@ -14,7 +16,7 @@ export const rewardsRouter = router({
         username: z.string(),
       }),
     )
-    .query(async ({ input: { username } }) => {
+    .query(async ({ input: { username }, ctx: { user } }) => {
       const { status } = await fetch(
         `https://api.github.com/users/${username}/following/${ORG_NAME}`,
         {
@@ -26,7 +28,26 @@ export const rewardsRouter = router({
         },
       );
 
-      return status === 204;
+      if (status !== 204) {
+        return false;
+      }
+
+      // Insert task into db
+      const campaign = 'SOCIAL_MEDIA_PREMIUM_20';
+      const task = 'GITHUB_FOLLOW';
+      const userId = user.id;
+      const identifier = username;
+
+      await prisma.rewardsTaskCompletion.create({
+        data: {
+          campaign,
+          identifier,
+          task,
+          userId,
+        },
+      });
+
+      return true;
     }),
   checkGitHubStarred: userProcedure
     .input(
@@ -34,7 +55,7 @@ export const rewardsRouter = router({
         username: z.string(),
       }),
     )
-    .query(async ({ input: { username } }) => {
+    .query(async ({ input: { username }, ctx: { user } }) => {
       const octokit = new Octokit({});
 
       const data = await octokit.paginate(
@@ -49,6 +70,42 @@ export const rewardsRouter = router({
         },
       );
 
-      return data.some((repo: any) => repo.id === REPO_ID);
+      if (!data.some((repo: any) => repo.id === REPO_ID)) {
+        return false;
+      }
+
+      // Insert task into db
+      const campaign = 'SOCIAL_MEDIA_PREMIUM_20';
+      const task = 'GITHUB_STAR';
+      const userId = user.id;
+      const identifier = username;
+
+      await prisma.rewardsTaskCompletion.create({
+        data: {
+          campaign,
+          identifier,
+          task,
+          userId,
+        },
+      });
+
+      return true;
+    }),
+  getTasksCompleted: userProcedure
+    .input(
+      z.object({
+        campaign: z.enum(['SOCIAL_MEDIA_PREMIUM_20']),
+      }),
+    )
+    .query(async ({ input: { campaign }, ctx: { user } }) => {
+      const userId = user.id;
+      const tasks = await prisma.rewardsTaskCompletion.findMany({
+        where: {
+          campaign,
+          userId,
+        },
+      });
+
+      return tasks;
     }),
 });
