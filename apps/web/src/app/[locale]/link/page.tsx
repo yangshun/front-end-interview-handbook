9626f3a3c3fd8d1265bd86bdb09c401529fdac7f
Yangshun <tay.yang.shun@gmail.com>
2024-01-07 14:28:35 +0800

    [web] mdx: improve external link page

--- apps/web/src/app/[locale]/link/page.tsx
+++ apps/web/src/app/[locale]/link/page.tsx
@@ -8,6 +8,7 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import LogoLink from '~/components/global/Logo';
 import Button from '~/components/ui/Button';
 import Heading from '~/components/ui/Heading';
+import Section from '~/components/ui/Heading/HeadingContext';
 import Navbar from '~/components/ui/Navbar/Navbar';
 import Text from '~/components/ui/Text';
 import { themeTextSecondaryColor } from '~/components/ui/theme';
@@ -28,8 +29,10 @@ export default function ExternalLinkPage() {
     <div className="flex flex-col min-h-screen">
       <Navbar isLoading={false} links={[]} logo={<LogoLink />} />
       <div className="flex items-center justify-center h-full flex-1 flex-col gap-4">
-        <RiLink className={clsx('h-10 w-10', themeTextSecondaryColor)} />
-        <div className="max-w-[360px] md:px-0 px-4 flex flex-col justify-center items-center">
+        <RiLink
+          className={clsx('h-10 w-10 shrink-0', themeTextSecondaryColor)}
+        />
+        <div className="max-w-[360px] md:px-0 px-4 flex flex-col justify-center items-center gap-1">
           <Heading level="heading5">
             <FormattedMessage
               defaultMessage="You're about to open a link:"
@@ -37,18 +40,22 @@ export default function ExternalLinkPage() {
               id="pWwyKQ"
             />
           </Heading>
-          <Heading className="truncate md:max-w-lg max-w-full" level="heading5">
-            {externalUrl}
-          </Heading>
-          <Text
-            className="dark:text-neutral-200 text-neutral-500 text-center"
-            size="body2">
-            <FormattedMessage
-              defaultMessage="You're about to open an external website. Be cautious and keep your personal information safe"
-              description="Subtitle for external link page"
-              id="p9Ml7d"
-            />
-          </Text>
+          <Section>
+            <Heading
+              className="truncate md:max-w-lg max-w-full"
+              level="heading5">
+              {externalUrl}
+            </Heading>
+            <Text
+              className="dark:text-neutral-200 text-neutral-500 text-center"
+              size="body2">
+              <FormattedMessage
+                defaultMessage="You're about to open an external website. Be cautious and keep your personal information safe"
+                description="Subtitle for external link page"
+                id="p9Ml7d"
+              />
+            </Text>
+          </Section>
         </div>
         <Button
           label={intl.formatMessage({
