0ea3f0fa027844180bbd1d4afc7eb9cc838aede6
Wei Qing <48304907+weiquu@users.noreply.github.com>
2024-01-23 15:05:16 +0900

    [web] projects: Add tooltip to RelativeTimestamp (#285)

--- apps/web/src/components/projects/common/RelativeTimestamp.tsx
+++ apps/web/src/components/projects/common/RelativeTimestamp.tsx
@@ -1,9 +1,27 @@
+import Tooltip from '~/components/ui/Tooltip';
+
 import { getRelativeTimestamp } from './relativeTimestampValues';
 
 type Props = Readonly<{
   timestamp: Date;
 }>;
 
+function getTooltipLabel(timestamp: Date) {
+  return new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    hour: 'numeric',
+    hour12: true,
+    minute: '2-digit',
+    month: 'long',
+    weekday: 'long',
+    year: 'numeric',
+  }).format(timestamp);
+}
+
 export default function RelativeTimestamp({ timestamp }: Props) {
-  return <span>{getRelativeTimestamp(timestamp)}</span>;
+  return (
+    <Tooltip label={getTooltipLabel(timestamp)}>
+      <span>{getRelativeTimestamp(timestamp)}</span>
+    </Tooltip>
+  );
 }
