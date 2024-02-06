3748a048581741c310bb1cbc6dcb2f9ba004253e
Yangshun <tay.yang.shun@gmail.com>
2024-02-06 12:12:19 +0800

    [web] ui(dialog): update widths

--- apps/web/src/components/ui/Dialog/Dialog.tsx
+++ apps/web/src/components/ui/Dialog/Dialog.tsx
@@ -39,7 +39,7 @@ const widthClasses: Record<DialogWidth, string> = {
   'screen-md': 'sm:max-w-sm md:max-w-screen-sm lg:max-w-screen-md',
   'screen-sm': 'sm:max-w-sm md:max-w-screen-sm',
   'screen-xl':
-    'sm:max-w-sm md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl',
+    'sm:max-w-sm md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-xl',
   sm: 'sm:max-w-sm',
 };
 
