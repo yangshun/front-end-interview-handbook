6d07899828d52d8e8f39a4577809ae33c6e281d5
Wei Qing <48304907+weiquu@users.noreply.github.com>
2024-02-09 12:51:14 +0900

    [web] projects(rep): implement core calculations (#320)

---
+++ apps/web/src/components/projects/common/projectsReputationUtils.test.ts
@@ -0,0 +1,313 @@
+import {
+  projectsReputationLevel,
+  projectsReputationPointsToNextLevel,
+  projectsReputationTierForLevel,
+} from './projectsReputationUtils';
+
+describe('projectsReputationTierForLevel', () => {
+  describe('initiate', () => {
+    test('left end of range', () => {
+      const level = 1;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('initiate');
+    });
+
+    test('middle of range', () => {
+      const level = 10;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('initiate');
+    });
+
+    test('middle of range', () => {
+      const level = 19;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('initiate');
+    });
+  });
+
+  describe('crafter', () => {
+    test('left end of range', () => {
+      const level = 20;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('crafter');
+    });
+
+    test('middle of range', () => {
+      const level = 35;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('crafter');
+    });
+
+    test('middle of range', () => {
+      const level = 49;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('crafter');
+    });
+  });
+
+  describe('architect', () => {
+    test('left end of range', () => {
+      const level = 50;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('architect');
+    });
+
+    test('middle of range', () => {
+      const level = 60;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('architect');
+    });
+
+    test('middle of range', () => {
+      const level = 74;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('architect');
+    });
+  });
+
+  describe('master', () => {
+    test('left end of range', () => {
+      const level = 75;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('master');
+    });
+
+    test('middle of range', () => {
+      const level = 85;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('master');
+    });
+
+    test('right end of range', () => {
+      const level = 99;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('master');
+    });
+  });
+
+  describe('prestige', () => {
+    test('left end of range', () => {
+      const level = 100;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('prestige');
+    });
+
+    test('middle of range', () => {
+      const level = 150;
+
+      expect(projectsReputationTierForLevel(level)).toEqual('prestige');
+    });
+  });
+});
+
+describe('projectsReputationLevel and projectsReputationPointsToNextLevel', () => {
+  describe('initial growth factor', () => {
+    test('level 1', () => {
+      const points = 0;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 1,
+        progress: 0,
+        tier: 'initiate',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(100);
+    });
+
+    test('level 10', () => {
+      const points = 1381;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 10,
+        progress: 9.7,
+        tier: 'initiate',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(214);
+    });
+
+    test('level 19', () => {
+      const points = 5087;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 19,
+        progress: 90,
+        tier: 'initiate',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(56);
+    });
+
+    test('milestone level 20', () => {
+      const points = 5666;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 20,
+        progress: 99.8,
+        tier: 'crafter',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(1);
+    });
+  });
+
+  describe('mid growth factor', () => {
+    test('level 21', () => {
+      const points = 6000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 21,
+        progress: 58.3,
+        tier: 'crafter',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(238);
+    });
+
+    test('level 26', () => {
+      const points = 9500;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 26,
+        progress: 47.9,
+        tier: 'crafter',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(453);
+    });
+
+    test('level 33', () => {
+      const points = 17000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 33,
+        progress: 13,
+        tier: 'crafter',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(1262);
+    });
+
+    test('level 40', () => {
+      const points = 31410;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 40,
+        progress: 100,
+        tier: 'crafter',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(1);
+    });
+
+    test('level 49', () => {
+      const points = 58000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 49,
+        progress: 86,
+        tier: 'crafter',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(525);
+    });
+
+    test('milestone level 50', () => {
+      const points = 61000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 50,
+        progress: 73,
+        tier: 'architect',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(914);
+    });
+  });
+
+  describe('high growth factor', () => {
+    test('level 51', () => {
+      const points = 63000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 51,
+        progress: 30.5,
+        tier: 'architect',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(2472);
+    });
+
+    test('level 66', () => {
+      const points = 140000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 66,
+        progress: 17.8,
+        tier: 'architect',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(6080);
+    });
+
+    test('milestone level 75', () => {
+      const points = 220300;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 75,
+        progress: 0.7,
+        tier: 'master',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(9679);
+    });
+
+    test('level 76', () => {
+      const points = 235000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 76,
+        progress: 49,
+        tier: 'master',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(5218);
+    });
+
+    test('milestone level 100', () => {
+      const points = 700000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 100,
+        progress: 51.3,
+        tier: 'prestige',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(13674);
+    });
+
+    test('level 101', () => {
+      const points = 716621;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 101,
+        progress: 10,
+        tier: 'prestige',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(26522);
+    });
+
+    test('level 115', () => {
+      const points = 1300000;
+
+      expect(projectsReputationLevel(points)).toEqual({
+        level: 115,
+        progress: 15,
+        tier: 'prestige',
+      });
+
+      expect(projectsReputationPointsToNextLevel(points)).toEqual(49568);
+    });
+  });
+});
---
+++ apps/web/src/components/projects/common/projectsReputationUtils.ts
@@ -0,0 +1,226 @@
+export type ProjectsReputationTier =
+  | 'architect'
+  | 'crafter'
+  | 'initiate'
+  | 'master'
+  | 'prestige';
+
+type LevelInfo = {
+  maxPoints: number;
+  tier: ProjectsReputationTier;
+};
+
+type TierInfo = {
+  minLevel: number;
+  tier: ProjectsReputationTier;
+};
+
+function round(value: number, precision: number) {
+  const multiplier = Math.pow(10, precision || 0);
+
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const milestoneLevels = [20, 50, 75, 100];
+const milestoneDiscountFactor = 0.85;
+// Assumption: levels after have constant growth factor and we compute until minimally the last tier
+const numPrecomputedLevels = milestoneLevels[milestoneLevels.length - 1];
+const growthFactorPastLastMilestone = 1.05;
+
+const initialPoints = 100;
+
+// Separated from tiers because some levels within the same tier have different growth factor calculation
+// E.g. level 20 is crafter, but follows the initiate growth factor
+// Assumption: we only decrease growth factor, and don't increase
+const growthFactors = [
+  {
+    decreaseAmount: 0,
+    decreaseLevelsPerSection: 0,
+    endLevel: 20,
+    endingGrowthFactor: 1.1,
+    initialGrowthFactor: 1.1,
+    startLevel: 1,
+  },
+  {
+    decreaseAmount: 0.01,
+    decreaseLevelsPerSection: 5,
+    endLevel: 50,
+    endingGrowthFactor: 1.06,
+    initialGrowthFactor: 1.09,
+    startLevel: 21,
+  },
+  {
+    decreaseAmount: 0,
+    decreaseLevelsPerSection: 0,
+    endLevel: numPrecomputedLevels,
+    endingGrowthFactor: 1.05,
+    initialGrowthFactor: 1.05,
+    startLevel: 51,
+  },
+];
+
+const tierInfo: Array<TierInfo> = [
+  {
+    minLevel: 1,
+    tier: 'initiate',
+  },
+  {
+    minLevel: 20,
+    tier: 'crafter',
+  },
+  {
+    minLevel: 50,
+    tier: 'architect',
+  },
+  {
+    minLevel: 75,
+    tier: 'master',
+  },
+  {
+    minLevel: 100,
+    tier: 'prestige',
+  },
+];
+
+const pointsToNextLevel: Array<number> = Array(numPrecomputedLevels + 1).fill(
+  initialPoints,
+);
+
+for (let i = 0; i < pointsToNextLevel.length; i++) {
+  // Base cases - level 0 is not defined so set to 0, level 1 is already set to initialPoints
+  if (i === 0) {
+    pointsToNextLevel[i] = 0;
+    continue;
+  } else if (i === 1) {
+    continue;
+  }
+
+  const growthFactorInfo = growthFactors.find(
+    (info) => i >= info.startLevel && i <= info.endLevel,
+  )!; // Safe because i is maximally numPrecomputedLevels
+
+  let currentGrowthFactor = growthFactorInfo.initialGrowthFactor;
+
+  if (growthFactorInfo.decreaseLevelsPerSection) {
+    currentGrowthFactor = Math.max(
+      growthFactorInfo.endingGrowthFactor,
+      growthFactorInfo.initialGrowthFactor -
+        growthFactorInfo.decreaseAmount *
+          Math.floor(
+            (i - growthFactorInfo.startLevel) /
+              growthFactorInfo.decreaseLevelsPerSection,
+          ),
+    );
+  }
+
+  // We require the previous level to calculate the next level because of rounding
+  const points = pointsToNextLevel[i - 1] * currentGrowthFactor;
+
+  if (milestoneLevels.includes(i)) {
+    pointsToNextLevel[i] = Math.round(points * milestoneDiscountFactor);
+  } else {
+    pointsToNextLevel[i] = Math.round(points);
+  }
+}
+
+const maxPointsOfALevelAndTier: Array<LevelInfo> = Array(
+  numPrecomputedLevels + 1,
+).fill({ maxPoints: 0, tier: 'initiate' });
+
+for (let i = 1; i < maxPointsOfALevelAndTier.length; i++) {
+  maxPointsOfALevelAndTier[i] = {
+    maxPoints: maxPointsOfALevelAndTier[i - 1].maxPoints + pointsToNextLevel[i],
+    tier: tierInfo.findLast((info) => i >= info.minLevel)!.tier, // Safe because i is minimally 1
+  };
+}
+
+export function projectsReputationTierForLevel(
+  level: number,
+): ProjectsReputationTier {
+  if (level > numPrecomputedLevels) {
+    return 'prestige';
+  }
+
+  return maxPointsOfALevelAndTier[level].tier;
+}
+
+export function projectsReputationLevel(points: number): Readonly<{
+  level: number;
+  progress: number;
+  tier: ProjectsReputationTier;
+}> {
+  let level = maxPointsOfALevelAndTier.findIndex(
+    (value) => value.maxPoints > points,
+  );
+
+  if (level !== -1) {
+    const pointsNeededForLevel =
+      maxPointsOfALevelAndTier[level].maxPoints -
+      maxPointsOfALevelAndTier[level - 1].maxPoints;
+    const progress = round(
+      ((points - maxPointsOfALevelAndTier[level - 1].maxPoints) /
+        pointsNeededForLevel) *
+        100,
+      1,
+    );
+    const tier = projectsReputationTierForLevel(level);
+
+    return {
+      level,
+      progress,
+      tier,
+    };
+  }
+
+  let maxPointsOfLevelCurr =
+    maxPointsOfALevelAndTier[numPrecomputedLevels].maxPoints;
+  let pointsToNextLevelCurr = pointsToNextLevel[numPrecomputedLevels];
+
+  level = numPrecomputedLevels;
+
+  while (points >= maxPointsOfLevelCurr) {
+    level++;
+    pointsToNextLevelCurr = Math.round(
+      pointsToNextLevelCurr * growthFactorPastLastMilestone,
+    );
+    maxPointsOfLevelCurr += pointsToNextLevelCurr;
+  }
+
+  const progress = round(
+    100 - ((maxPointsOfLevelCurr - points) / pointsToNextLevelCurr) * 100,
+    1,
+  );
+  const tier = projectsReputationTierForLevel(level);
+
+  return {
+    level,
+    progress,
+    tier,
+  };
+}
+
+export function projectsReputationPointsToNextLevel(points: number): number {
+  let level = maxPointsOfALevelAndTier.findIndex(
+    (value) => value.maxPoints > points,
+  );
+
+  if (level !== -1) {
+    return maxPointsOfALevelAndTier[level].maxPoints - points;
+  }
+
+  let maxPointsOfLevelCurr =
+    maxPointsOfALevelAndTier[numPrecomputedLevels].maxPoints;
+  let pointsToNextLevelCurr = pointsToNextLevel[numPrecomputedLevels];
+
+  level = numPrecomputedLevels;
+
+  while (points >= maxPointsOfLevelCurr) {
+    level++;
+    pointsToNextLevelCurr = Math.round(
+      pointsToNextLevelCurr * growthFactorPastLastMilestone,
+    );
+    maxPointsOfLevelCurr += pointsToNextLevelCurr;
+  }
+
+  return maxPointsOfLevelCurr - points;
+}
