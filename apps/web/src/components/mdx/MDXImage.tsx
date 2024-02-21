28f372793205189908784a6f22e29b30d87c1200
Wei Qing <48304907+weiquu@users.noreply.github.com>
2024-02-21 20:31:36 +0900

    [web] ui(MDXImage): migrate MDXImage to use Radix dialog (#348)

--- apps/web/src/components/mdx/MDXImage.tsx
+++ apps/web/src/components/mdx/MDXImage.tsx
@@ -2,12 +2,11 @@
 
 import clsx from 'clsx';
 import type { ComponentProps } from 'react';
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import { Dialog, Transition } from '@headlessui/react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export default function MDXImage({ alt, ...props }: ComponentProps<'img'>) {
-  const [isShown, setIsShown] = useState(false);
   const [canExpand, setCanExpand] = useState(false);
   const ref = useRef<HTMLImageElement>(null);
 
@@ -24,73 +23,45 @@ export default function MDXImage({ alt, ...props }: ComponentProps<'img'>) {
   }, [props.src]);
 
   return (
-    <>
-      <img
-        ref={ref}
-        alt={alt}
-        className={clsx(
-          'mx-auto w-full max-w-lg',
-          canExpand && 'cursor-zoom-in',
-        )}
-        onClick={canExpand ? () => setIsShown(true) : undefined}
-        onLoad={onLoad}
-        {...props}
-      />
-      <ExpandedImageDialog isShown={isShown} onClose={() => setIsShown(false)}>
+    <Dialog.Root>
+      <Dialog.Trigger asChild={true}>
         <img
+          ref={ref}
           alt={alt}
-          className="mx-auto max-h-screen max-w-full cursor-zoom-out"
-          onClick={() => setIsShown(false)}
+          className={clsx(
+            'mx-auto w-full max-w-lg',
+            canExpand && 'cursor-zoom-in',
+          )}
+          onLoad={onLoad}
           {...props}
         />
-      </ExpandedImageDialog>
-    </>
-  );
-}
-
-type Props = Readonly<{
-  children: React.ReactNode;
-  isShown?: boolean;
-  onClose: () => void;
-}>;
-
-function ExpandedImageDialog({ children, isShown, onClose }: Props) {
-  const cancelButtonRef = useRef(null);
-
-  return (
-    <Transition.Root as={Fragment} show={isShown}>
-      <Dialog
-        as="div"
-        className="z-dialog-backdrop relative"
-        initialFocus={cancelButtonRef}
-        onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0">
-          <div className="fixed inset-0 bg-neutral-500 bg-opacity-75 backdrop-blur-sm transition-opacity dark:bg-neutral-950/60" />
-        </Transition.Child>
-        <div className="z-dialog fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-              <Dialog.Panel className="relative transform overflow-hidden sm:w-3/4">
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+      </Dialog.Trigger>
+      {canExpand && (
+        <Dialog.Portal>
+          <Dialog.Overlay className="z-dialog-backdrop fixed inset-0 bg-neutral-500 bg-opacity-75 backdrop-blur-sm transition-opacity dark:bg-neutral-950/60" />
+          <Dialog.Content
+            className={clsx(
+              'z-dialog fixed inset-0 flex min-h-full items-center justify-center overflow-y-auto outline-none',
+              // TODO: fix transition
+              'transform transition-all',
+              'data-[state=open]:duration-300 data-[state=open]:ease-out',
+              'data-[state=closed]:opacity-0 data-[state=open]:opacity-100',
+              'data-[state=closed]:translate-y-4 data-[state=open]:translate-y-0',
+              'data-[state=closed]:sm:scale-95 data-[state=open]:sm:scale-100',
+              'data-[state=closed]:sm:translate-y-0',
+            )}>
+            <Dialog.Close
+              asChild={true}
+              className="relative transform overflow-hidden sm:w-3/4">
+              <img
+                alt={alt}
+                className="mx-auto max-h-screen max-w-full cursor-zoom-out"
+                {...props}
+              />
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      )}
+    </Dialog.Root>
   );
 }
