4e59de013995e0c10fb3dfb4597427403a362a34
Yangshun <tay.yang.shun@gmail.com>
2024-02-22 12:01:04 +0800

    [web] ui(mdximage): add transitions

--- apps/web/src/components/mdx/MDXImage.tsx
+++ apps/web/src/components/mdx/MDXImage.tsx
@@ -48,7 +48,12 @@ export default function MDXImage({ alt, ...props }: ComponentProps<'img'>) {
             'z-dialog',
             'overflow-y-auto',
             'outline-none',
-            'transform transition-all',
+            'transform transition-all duration-200',
+            'data-[state=open]:animate-in data-[state=closed]:animate-out',
+            'data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0',
+            'data-[state=open]:zoom-in-95 data-[state=closed]:zoom-out-95',
+            'data-[state=open]:slide-in-from-left-1/2 data-[state=closed]:slide-out-to-left-1/2',
+            'data-[state=open]:slide-in-from-top-[48%] data-[state=closed]:slide-out-to-top-[48%]',
           )}>
           <Dialog.Close asChild={true}>
             <img
