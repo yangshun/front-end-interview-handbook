419fade80ab8198bb568c45f610ab03f757d964e
Nitesh Seram <niteshseram@gmail.com>
2024-02-29 12:49:45 +0530

    [web] projects(dashboard): show complete profile popup on hover (#386)

--- apps/web/src/components/projects/dashboard/ProjectsDashboardCompleteProfileCard.tsx
+++ apps/web/src/components/projects/dashboard/ProjectsDashboardCompleteProfileCard.tsx
@@ -11,11 +11,15 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import useUserProfile from '~/hooks/user/useUserProfile';
 
 import Anchor from '~/components/ui/Anchor';
-import Card from '~/components/ui/Card';
 import Chip from '~/components/ui/Chip';
 import ProgressBar from '~/components/ui/ProgressBar';
 import Text from '~/components/ui/Text';
-import { themeGradientBlueGreen } from '~/components/ui/theme';
+import {
+  themeBackgroundCardColor,
+  themeBackgroundColor,
+  themeGlassyBorder,
+  themeGradientBlueGreen,
+} from '~/components/ui/theme';
 
 const progressBarClassName = themeGradientBlueGreen.className;
 
@@ -66,82 +70,89 @@ export default function ProjectsDashboardCompleteProfileCard() {
   }
 
   return (
-    <Card
-      className={clsx('flex flex-col gap-3 px-5 py-4')}
-      disableSpotlight={true}
-      padding={false}
-      pattern={false}>
-      <div className="flex flex-col gap-2">
-        <button
-          className="flex justify-between"
-          type="button"
-          onClick={() => {
-            setIsCardOpen((isOpen) => !isOpen);
-          }}>
-          <div className="flex items-center gap-2">
-            <Text size="body2">
-              <FormattedMessage
-                defaultMessage="Complete your profile"
-                description="Title for Complete profile card on Projects dashboard page"
-                id="XkNVS1"
-              />
-            </Text>
-            <Chip
-              className="!bg-brand-dark dark:!bg-brand-darker"
-              label={intl.formatNumber(numLeft)}
-              size="xs"
-              variant="primary"
-            />
-          </div>
-          {isCardOpen ? <RiArrowUpSLine /> : <RiArrowDownSLine />}
-        </button>
-        <Text color="secondary" size="body3">
-          <FormattedMessage
-            defaultMessage="Completed profiles get 3x more responses"
-            description="Subtitle for Complete profile card on Projects dashboard page"
-            id="HP34xZ"
-          />
-        </Text>
-      </div>
-      {isCardOpen && (
-        <ProgressBar
-          label={intl.formatMessage({
-            defaultMessage: 'Profile completion progress bar',
-            description:
-              'Label for Profile completion progress bar on Projects dashboard page',
-            id: 'hIOUCl',
-          })}
-          progressClass={progressBarClassName}
-          total={numTotal}
-          value={numTotal - numLeft}
-        />
-      )}
-      {isCardOpen && (
-        <div className="flex flex-col gap-4">
-          {profileTasks.map((task) => (
-            <div key={task.title} className="flex items-center gap-4">
+    <div
+      className={clsx('rounded-lg', themeBackgroundColor, themeGlassyBorder)}
+      tabIndex={0}
+      onFocus={() => setIsCardOpen(true)}
+      onMouseEnter={() => setIsCardOpen(true)}
+      onMouseLeave={() => setIsCardOpen(false)}>
+      <div
+        className={clsx(
+          'flex flex-col gap-3 px-5 py-4',
+          themeBackgroundCardColor,
+        )}>
+        <div className="flex flex-col gap-2">
+          <button
+            className="flex justify-between"
+            type="button"
+            onClick={() => {
+              setIsCardOpen((isOpen) => !isOpen);
+            }}>
+            <div className="flex items-center gap-2">
+              <Text size="body2">
+                <FormattedMessage
+                  defaultMessage="Complete your profile"
+                  description="Title for Complete profile card on Projects dashboard page"
+                  id="XkNVS1"
+                />
+              </Text>
               <Chip
-                icon={RiCheckFill}
-                isLabelHidden={true}
-                label={task.isComplete ? 'Completed' : 'Not completed'}
-                size="sm"
-                variant={task.isComplete ? 'success' : 'neutral'}
+                className="!bg-brand-dark dark:!bg-brand-darker"
+                label={intl.formatNumber(numLeft)}
+                size="xs"
+                variant="primary"
               />
-              <Anchor
-                className="flex grow justify-between gap-3"
-                href="/projects/profile/edit"
-                variant="muted">
-                <Text
-                  className={clsx(task.isComplete && 'line-through')}
-                  size="body2">
-                  {task.title}
-                </Text>
-                {!task.isComplete && <RiArrowRightLine />}
-              </Anchor>
             </div>
-          ))}
+            {isCardOpen ? <RiArrowUpSLine /> : <RiArrowDownSLine />}
+          </button>
+          <Text color="secondary" size="body3">
+            <FormattedMessage
+              defaultMessage="Completed profiles get 3x more responses"
+              description="Subtitle for Complete profile card on Projects dashboard page"
+              id="HP34xZ"
+            />
+          </Text>
         </div>
-      )}
-    </Card>
+        {isCardOpen && (
+          <ProgressBar
+            label={intl.formatMessage({
+              defaultMessage: 'Profile completion progress bar',
+              description:
+                'Label for Profile completion progress bar on Projects dashboard page',
+              id: 'hIOUCl',
+            })}
+            progressClass={progressBarClassName}
+            total={numTotal}
+            value={numTotal - numLeft}
+          />
+        )}
+        {isCardOpen && (
+          <div className="flex flex-col gap-4">
+            {profileTasks.map((task) => (
+              <div key={task.title} className="flex items-center gap-4">
+                <Chip
+                  icon={RiCheckFill}
+                  isLabelHidden={true}
+                  label={task.isComplete ? 'Completed' : 'Not completed'}
+                  size="sm"
+                  variant={task.isComplete ? 'success' : 'neutral'}
+                />
+                <Anchor
+                  className="flex grow justify-between gap-3"
+                  href="/projects/profile/edit"
+                  variant="muted">
+                  <Text
+                    className={clsx(task.isComplete && 'line-through')}
+                    size="body2">
+                    {task.title}
+                  </Text>
+                  {!task.isComplete && <RiArrowRightLine />}
+                </Anchor>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
