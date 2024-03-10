7af841b6e0aaac6705f56ea03a8116cbbfc3c1d8
Nitesh Seram <niteshseram@gmail.com>
2024-03-10 15:35:07 +0530

    [web] projects(profile): add pagination in all challenges tab of project progress (#421)
    Co-authored-by: GitHub Actions <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Yangshun Tay <tay.yang.shun@gmail.com>

--- apps/web/src/components/projects/profile/progress/ProjectsProfileProgressAllChallengesTab.tsx
+++ apps/web/src/components/projects/profile/progress/ProjectsProfileProgressAllChallengesTab.tsx
@@ -5,11 +5,14 @@ import { RiRocketLine } from 'react-icons/ri';
 import { useIntl } from 'react-intl';
 
 import { trpc } from '~/hooks/trpc';
+import usePagination from '~/hooks/usePagination';
 
 import FilterButton from '~/components/common/FilterButton';
 import ProjectsChallengeCard from '~/components/projects/challenges/lists/ProjectsChallengeCard';
 import useProjectsSessionStatusFilter from '~/components/projects/profile/progress/useProjectsSessionStatusFilter';
 import EmptyState from '~/components/ui/EmptyState';
+import Pagination from '~/components/ui/Pagination';
+import Spinner from '~/components/ui/Spinner';
 import { themeTextColor } from '~/components/ui/theme';
 
 import { ProjectsChallengeSessionStatus } from '@prisma/client';
@@ -17,6 +20,8 @@ import { ProjectsChallengeSessionStatus } from '@prisma/client';
 type Props = Readonly<{
   userId?: string;
 }>;
+
+const ITEMS_PER_PAGE = 6;
 
 export default function ProjectsProfileProgressAllChallengesTab({
   userId,
@@ -29,13 +34,15 @@ export default function ProjectsProfileProgressAllChallengesTab({
       namespace: 'all-challenges',
     });
 
-  const { data: allSessions } = trpc.projects.sessions.list.useQuery({
-    statuses: [
-      ProjectsChallengeSessionStatus.IN_PROGRESS,
-      ProjectsChallengeSessionStatus.COMPLETED,
-    ],
-    userId,
-  });
+  const { data: allSessions, isLoading } = trpc.projects.sessions.list.useQuery(
+    {
+      statuses: [
+        ProjectsChallengeSessionStatus.IN_PROGRESS,
+        ProjectsChallengeSessionStatus.COMPLETED,
+      ],
+      userId,
+    },
+  );
 
   const shownSessions = allSessions
     ?.filter((session) => {
@@ -49,6 +56,18 @@ export default function ProjectsProfileProgressAllChallengesTab({
         ) === index
       );
     });
+
+  // Pagination
+  const {
+    currentPageItems: currentPageSessions,
+    totalPages,
+    setCurrentPage,
+    currentPage,
+  } = usePagination({
+    deps: [],
+    itemsPerPage: ITEMS_PER_PAGE,
+    totalList: shownSessions,
+  });
 
   const emptyState =
     challengeFilters.size === 0 ||
@@ -126,20 +145,42 @@ export default function ProjectsProfileProgressAllChallengesTab({
           </div>
         </fieldset>
       </div>
-      {shownSessions && shownSessions.length !== 0 ? (
-        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-6">
-          {shownSessions.map((session) =>
-            session.challenge ? (
-              <ProjectsChallengeCard
-                key={session.id}
-                challenge={session.challenge}
-              />
-            ) : null,
-          )}
-        </div>
-      ) : (
-        emptyState
-      )}
+      {(() => {
+        if (isLoading) {
+          return (
+            <div className="flex h-20 w-full items-center justify-center">
+              <Spinner size="md" />
+            </div>
+          );
+        }
+        if (currentPageSessions && currentPageSessions.length !== 0) {
+          return (
+            <div className="flex flex-col gap-4">
+              <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-6">
+                {currentPageSessions.map((session) =>
+                  session.challenge ? (
+                    <ProjectsChallengeCard
+                      key={session.id}
+                      challenge={session.challenge}
+                    />
+                  ) : null,
+                )}
+              </div>
+              {totalPages > 1 && (
+                <div className="flex items-center justify-between">
+                  <Pagination
+                    count={totalPages}
+                    page={currentPage}
+                    onPageChange={(value) => setCurrentPage(value)}
+                  />
+                </div>
+              )}
+            </div>
+          );
+        }
+
+        return emptyState;
+      })()}
     </div>
   );
 }
