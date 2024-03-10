2666d59611ca59d6e2240e5d4e1b5acdd8d46dfa
Yangshun <tay.yang.shun@gmail.com>
2024-03-10 18:34:12 +0800

    [web] projects(purchase): fix migration to sync credits and transactions

--- apps/web/prisma/migrations/20240225114424_projects_reputation_recalculate/migration.sql
+++ apps/web/prisma/migrations/20240225114424_projects_reputation_recalculate/migration.sql
@@ -1,17 +1,20 @@
-CREATE OR REPLACE FUNCTION projects_recalculate_points() returns trigger as $$
+CREATE OR REPLACE FUNCTION projects_recalculate_credits() returns trigger as $$
 BEGIN
     UPDATE public."ProjectsProfile"
-    SET points = (
-        SELECT SUM(points)
-        FROM public."ProjectsReputationPoint"
-        WHERE "profileId" = NEW."profileId"
+    SET credits = (
+        -- Use COALESCE to handle NULL case
+        SELECT COALESCE(SUM(CASE WHEN type = 'CREDIT' THEN amount ELSE -amount END), 0)
+        FROM public."ProjectsChallengeCreditTransaction"
+        -- Select the appropriate profileId
+        WHERE "profileId" = COALESCE(NEW."profileId", OLD."profileId")
     )
-    WHERE id = NEW."profileId";
+    -- Select the appropriate profileId
+    WHERE id = COALESCE(NEW."profileId", OLD."profileId");
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY definer;
 
-CREATE TRIGGER projects_points_all_changes
-AFTER INSERT OR UPDATE OR DELETE ON public."ProjectsReputationPoint"
+CREATE TRIGGER projects_credits_transactions_all_changes
+AFTER INSERT OR UPDATE OR DELETE ON public."ProjectsChallengeCreditTransaction"
 FOR EACH ROW
-EXECUTE FUNCTION projects_recalculate_points();
+EXECUTE FUNCTION projects_recalculate_credits();
