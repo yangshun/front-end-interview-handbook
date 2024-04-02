69443cf37234fae34b1f7757315a5a3777e98910
Nitesh Seram <niteshseram@gmail.com>
2024-04-02 08:51:06 +0530

    [web] projects/submission: make slider knob at the center and use brand color for slider (#511)

--- apps/web/src/components/projects/submissions/ProjectsChallengeSubmissionImageComparisonSlider.tsx
+++ apps/web/src/components/projects/submissions/ProjectsChallengeSubmissionImageComparisonSlider.tsx
@@ -1,8 +1,10 @@
 import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
 import ReactCompareImage from 'react-compare-image';
 import { RiArrowDropLeftFill, RiArrowDropRightFill } from 'react-icons/ri';
 
 import Spinner from '~/components/ui/Spinner';
+import { themeBackgroundBrandColor } from '~/components/ui/theme';
 
 type Props = Readonly<{
   image: Readonly<{
@@ -17,8 +19,33 @@ export default function ProjectsChallengeSubmissionImageComparisonSlider({
   image,
   maxWidth,
 }: Props) {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const handleRef = useRef<HTMLDivElement>(null);
+
+  // To keep the slider knob always vertically center on the scrollable visible area
+  useEffect(() => {
+    const parentRefElement = parentRef.current;
+    const handleScroll = () => {
+      const visibleHeight =
+        parentRef.current?.getBoundingClientRect()?.height ?? 0;
+      const position = visibleHeight / 2 + (parentRefElement?.scrollTop ?? 0);
+
+      if (handleRef.current) {
+        handleRef.current.style.top = `${position}px`;
+      }
+    };
+
+    handleScroll();
+
+    parentRefElement?.addEventListener('scroll', handleScroll);
+
+    return () => {
+      parentRefElement?.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="w-full overflow-y-auto">
+    <div ref={parentRef} className="w-full overflow-y-auto">
       <div
         className="mx-auto max-h-[272px] min-h-[272px] md:max-h-[610px] md:min-h-[610px]"
         style={{
@@ -26,7 +53,14 @@ export default function ProjectsChallengeSubmissionImageComparisonSlider({
         }}>
         <ReactCompareImage
           handle={
-            <div className="bg-brand-dark dark:bg-brand flex h-[42px] w-[42px] items-center justify-center rounded-full">
+            <div
+              ref={handleRef}
+              className={clsx(
+                'flex items-center justify-center',
+                'size-10 rounded-full',
+                'absolute left-1/2 top-1/2 -translate-x-1/2 transform',
+                themeBackgroundBrandColor,
+              )}>
               <div className="flex">
                 <RiArrowDropLeftFill className="size-7 -mr-2" />
                 <RiArrowDropRightFill className="size-7 -ml-2" />
@@ -43,7 +77,7 @@ export default function ProjectsChallengeSubmissionImageComparisonSlider({
               <Spinner size="md" />
             </div>
           }
-          sliderLineColor="#948CF9"
+          sliderLineColor="hsl(var(--brand-default))"
           sliderLineWidth={3}
         />
       </div>
