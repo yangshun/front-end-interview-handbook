41d08ba84e26e27d87e475d1e109f13788dea8ba
Vikas yadav <40167428+thejsdeveloper@users.noreply.github.com>
2024-04-03 04:40:28 +0530

    [web] projects/marketing: project launch date change (#520)

--- apps/web/src/data/FeatureFlags.ts
+++ apps/web/src/data/FeatureFlags.ts
@@ -3,10 +3,9 @@ import { fromUnixTime } from 'date-fns';
 export const JS_COMMUNITY_SOLUTIONS_IS_LIVE =
   process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
 
-const epochTime = 1712556000; // Epoch for 8 APR 2024 14:00:00 SGT
+const epochTime = 1713160800; // Epoch for 15 APR 2024 14:00:00 SGT
 
 export const PROJECT_LAUNCH_DATE = fromUnixTime(epochTime);
 
 // TODO(projects): change it if activation available
 export const PROJECT_ACTIVATION_AVAILABLE = false;
-
