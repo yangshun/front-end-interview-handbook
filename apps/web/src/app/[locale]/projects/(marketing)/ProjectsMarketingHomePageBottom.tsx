fa66396a76a022cac8f3bd8df2d5cda2c208aadf
Nitesh Seram <niteshseram@gmail.com>
2024-04-04 04:49:26 +0530

    [web] projects/marketing: add pricing promotions in the pricing section of marketing page (#530)

--- apps/web/src/app/[locale]/projects/(marketing)/ProjectsMarketingHomePageBottom.tsx
+++ apps/web/src/app/[locale]/projects/(marketing)/ProjectsMarketingHomePageBottom.tsx
@@ -1,12 +1,14 @@
 import MarketingContinuousUpdates from '~/components/interviews/marketing/MarketingContinuousUpdates';
 import MarketingGeneralFAQSection from '~/components/interviews/marketing/MarketingGeneralFAQSection';
 import ProjectsMarketingContactUs from '~/components/projects/marketing/ProjectsMarketingContactUs';
+import ProjectsPricingPromotions from '~/components/projects/purchase/ProjectsPricingPromotions';
 import ProjectsPricingSectionLocalizedContainer from '~/components/projects/purchase/ProjectsPricingSectionLocalizedContainer';
 
 export default function ProjectsMarketingHomePageBottom() {
   return (
     <>
       <ProjectsPricingSectionLocalizedContainer />
+      <ProjectsPricingPromotions />
       <MarketingGeneralFAQSection />
       <MarketingContinuousUpdates />
       <ProjectsMarketingContactUs />
