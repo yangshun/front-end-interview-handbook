d7a613b788144fa8a1d48223526ed80fad65e1ab
Yangshun <tay.yang.shun@gmail.com>
2024-04-04 09:42:21 +0800

    [web] projects/marketing: show beta pass to all non-prod users

--- apps/web/src/data/PromotionConfig.ts
+++ apps/web/src/data/PromotionConfig.ts
@@ -5,7 +5,10 @@ export const STUDENT_DISCOUNT_PERCENTAGE = 40;
 export const REVIEW_CASHBACK_DISCOUNT_PERCENTAGE = 100;
 
 export function hasProjectsBetaAccess(date: number) {
-  if (process.env.NODE_ENV === 'development') {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production'
+  ) {
     return true;
   }
 
