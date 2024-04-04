7204cb356cee8e11cc28168d7e5a311cd08cc6ad
Yangshun <tay.yang.shun@gmail.com>
2024-04-04 12:30:11 +0800

    [web] projects/submission: update image dimensions

--- apps/web/src/components/projects/common/ProjectsImageBreakpoints.ts
+++ apps/web/src/components/projects/common/ProjectsImageBreakpoints.ts
@@ -4,16 +4,17 @@ export const ProjectsImageBreakpointDimensions: Record<
   ProjectsImageBreakpointCategory,
   Readonly<{ height: number; width: number }>
 > = {
+  // These dimensions have to be same as the images exported from Figma.
   desktop: {
-    height: 1080,
+    height: 1024,
     width: 1440,
   },
   mobile: {
-    height: 1080,
+    height: 812,
     width: 375,
   },
   tablet: {
-    height: 1080,
+    height: 1024,
     width: 768,
   },
 };
