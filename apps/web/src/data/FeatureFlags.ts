5dbca2e7445fc82e675800bbd7cb4e0d875e4581
Yangshun <tay.yang.shun@gmail.com>
2024-04-07 18:23:00 +0800

    [web] projects/marketing: update launch date to 22nd Apr 2024

--- apps/web/src/data/FeatureFlags.ts
+++ apps/web/src/data/FeatureFlags.ts
@@ -1,11 +1,7 @@
-import { fromUnixTime } from 'date-fns';
-
 export const JS_COMMUNITY_SOLUTIONS_IS_LIVE =
   process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
 
-const epochTime = 1713160800; // Epoch for 15 APR 2024 14:00:00 SGT
-
-export const PROJECT_LAUNCH_DATE = fromUnixTime(epochTime);
+export const PROJECT_LAUNCH_DATE = new Date('Apr 22 2024 14:00:00');
 export const FEATURE_FLAGS_SHOW_MYSTERY_PRODUCT = true;
 export const FEATURE_FLAGS_PROJECTS_LAUNCHED =
   process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
