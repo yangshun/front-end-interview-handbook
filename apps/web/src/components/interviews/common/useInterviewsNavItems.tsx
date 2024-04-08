15d5d804d7c73250027661777ee62373688a4a70
Yangshun <tay.yang.shun@gmail.com>
2024-04-08 08:13:46 +0800

    [web] interviews/sidebar: disable scrollToTop for features link

--- apps/web/src/components/interviews/common/useInterviewsNavItems.tsx
+++ apps/web/src/components/interviews/common/useInterviewsNavItems.tsx
@@ -69,6 +69,7 @@ export default function useInterviewsNavItems(placement: 'nav' | 'sidebar') {
       });
     },
     position: 'end',
+    scrollToTop: false,
     type: 'link',
   };
   const guides: NavbarPrimaryItem = {
