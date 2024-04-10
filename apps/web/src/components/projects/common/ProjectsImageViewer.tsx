c7cb1bb177678346f6b21b6c0461d68bbe3c70c3
Yangshun <tay.yang.shun@gmail.com>
2024-04-10 20:37:03 +0800

    [web] projects/submission: add scroll-to-zoom functionality for image gallery

--- apps/web/src/components/projects/common/ProjectsImageViewer.tsx
+++ apps/web/src/components/projects/common/ProjectsImageViewer.tsx
@@ -1,15 +1,21 @@
 import clsx from 'clsx';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { clamp } from 'lodash-es';
+import { useEffect, useRef, useState } from 'react';
 import { RiZoomInLine, RiZoomOutLine } from 'react-icons/ri';
 import { useWindowSize } from 'usehooks-ts';
 
 import Button from '~/components/ui/Button';
+
+import { useWheel } from '@use-gesture/react';
 
 type Props = Readonly<{
   alt: string;
   src: string;
   width: number;
 }>;
+
+const MIN_ZOOM_LEVEL = 20;
+const MAX_ZOOM_LEVEL = 200;
 
 export default function ProjectsImageViewer({ alt, src, width }: Props) {
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -20,6 +26,10 @@ export default function ProjectsImageViewer({ alt, src, width }: Props) {
     width: true,
   });
   const windowSize = useWindowSize();
+
+  function setZoomLevelWithClamp(value: number) {
+    setZoomLevel(clamp(value, MIN_ZOOM_LEVEL, MAX_ZOOM_LEVEL));
+  }
 
   useEffect(() => {
     setZoomLevel(50);
@@ -46,22 +56,32 @@ export default function ProjectsImageViewer({ alt, src, width }: Props) {
     imageRef.current?.clientWidth,
   ]);
 
+  useWheel(
+    (data) => {
+      if (data.ctrlKey) {
+        data.event.preventDefault();
+        setZoomLevelWithClamp(zoomLevel + data.event.wheelDeltaY / 50);
+      }
+    },
+    { eventOptions: { passive: false }, target: wrapperRef },
+  );
+
   return (
-    <div className="relative">
-      <div className="absolute bottom-6 left-6 flex flex-col gap-1">
+    <div className="relative max-h-full">
+      <div className="absolute bottom-6 right-6 flex flex-col gap-1">
         <Button
           icon={RiZoomInLine}
           isLabelHidden={true}
           label="Zoom in"
           variant="secondary"
-          onClick={() => setZoomLevel(zoomLevel + 10)}
+          onClick={() => setZoomLevelWithClamp(zoomLevel + 10)}
         />
         <Button
           icon={RiZoomOutLine}
           isLabelHidden={true}
           label="Zoom out"
           variant="secondary"
-          onClick={() => setZoomLevel(zoomLevel - 10)}
+          onClick={() => setZoomLevelWithClamp(zoomLevel - 10)}
         />
       </div>
       <div
@@ -73,11 +93,12 @@ export default function ProjectsImageViewer({ alt, src, width }: Props) {
             'flex',
           imageSmallerThanWrapper.height ? 'items-center' : 'items-baseline',
           imageSmallerThanWrapper.width ? 'justify-center' : 'justify-baseline',
+          'touch-none',
         )}>
         <img
           ref={imageRef}
           alt={alt}
-          className="pointer-events-auto max-w-none touch-none select-none"
+          className="pointer-events-none max-w-none touch-none select-none"
           src={src}
           style={{
             width: (width * zoomLevel) / 100,
