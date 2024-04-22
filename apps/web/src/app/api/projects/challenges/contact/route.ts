f7f80be0dfa6477472404f6974f36e7628665710
Yangshun <tay.yang.shun@gmail.com>
2024-04-22 20:24:34 +0800

    [web] projects/challenge/contact: add API specs

---
+++ apps/web/src/app/api/projects/challenges/contact/route.ts
@@ -0,0 +1,38 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+type FormBody = Readonly<{
+  email?: string;
+  message?: string;
+  name?: string;
+}>;
+
+export async function POST(req: NextRequest) {
+  const { name, email, message }: FormBody = await req.json();
+
+  // Perform simple field validations, as long as they are not empty.
+  if (!name) {
+    return NextResponse.json(
+      { error: 'Name cannot be empty' },
+      { status: 422 },
+    );
+  }
+
+  if (!email) {
+    return NextResponse.json(
+      { error: 'Email cannot be empty' },
+      { status: 422 },
+    );
+  }
+
+  if (!message) {
+    return NextResponse.json(
+      { error: 'Message cannot be empty' },
+      { status: 422 },
+    );
+  }
+
+  return NextResponse.json({
+    message:
+      'Submission successful! We will get back to you in 3-5 days via email.',
+  });
+}
