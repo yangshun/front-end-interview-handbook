57504f5e9de977317ba458bc86ff5ae409152073
Yangshun <tay.yang.shun@gmail.com>
2024-04-22 20:49:56 +0800

    [web] projects/challenge/newsletter: add API specs

---
+++ apps/web/src/app/api/projects/challenges/newsletter/route.ts
@@ -0,0 +1,30 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+type FormBody = Readonly<{
+  email?: string;
+}>;
+
+const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+export async function POST(req: NextRequest) {
+  const { email }: FormBody = await req.json();
+
+  if (!email) {
+    return NextResponse.json(
+      { error: 'Email address is required.' },
+      { status: 422 },
+    );
+  }
+
+  if (!EMAIL_REGEX.test(email)) {
+    return NextResponse.json(
+      { error: 'Email format is invalid.' },
+      { status: 422 },
+    );
+  }
+
+  return NextResponse.json({
+    message: 'Subscription successful! Please check your email to confirm.',
+  });
+}
