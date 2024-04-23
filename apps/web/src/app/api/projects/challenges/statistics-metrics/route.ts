660dc8deace56ac936789eebf3a3b07120c63919
Yangshun <tay.yang.shun@gmail.com>
2024-04-23 08:10:46 +0800

    [web] projects/challenge/statistics: add API specs

---
+++ apps/web/src/app/api/projects/challenges/statistics-metrics/route.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+const baseTimestamp = 1713829932;
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const delta = searchParams.has('latest')
+    ? Math.floor(Date.now() / 1000) - baseTimestamp
+    : 0;
+
+  return NextResponse.json({
+    data: [
+      {
+        metric: 'downloads',
+        value: 25_664_890 + delta * 12,
+      },
+      {
+        metric: 'paid_users',
+        value: 17_219 + delta * 3,
+      },
+      {
+        metric: 'library_images',
+        value: 190_654_321 + delta,
+      },
+    ],
+  });
+}
