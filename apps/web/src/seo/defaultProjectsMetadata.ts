a8d7035333752e908be602363e6ac606974367fe
Yangshun <tay.yang.shun@gmail.com>
2024-05-08 18:32:48 +0800

    [web] projects/seo: remove localized alternates

--- apps/web/src/seo/defaultProjectsMetadata.ts
+++ apps/web/src/seo/defaultProjectsMetadata.ts
@@ -1,24 +1,59 @@
-import defaultMetadata from './defaultMetadata';
+import { getSiteUrl } from './siteUrl';
 
 import type { IntlShape } from '@formatjs/intl';
 
+type PageMetadata = Readonly<{
+  description?: string;
+  imageUrl?: string;
+  locale: string;
+  pathname: string;
+  siteName?: string;
+  template?: string;
+  title: string;
+}>;
+
 export default function defaultProjectsMetadata(
   intl: IntlShape<string>,
-  params: Parameters<typeof defaultMetadata>[0],
+  params: PageMetadata,
 ) {
-  return defaultMetadata({
-    imageUrl: '/img/seo/og-projects.jpg',
-    siteName: intl.formatMessage({
-      defaultMessage: 'GreatFrontEnd Projects',
-      description: 'Title of Projects product',
-      id: 'ctbPZV',
-    }),
-    template: intl.formatMessage({
-      defaultMessage:
-        '%s | GreatFrontEnd Projects - Real-world project challenges',
-      description: 'SEO title with placeholder, please leave the %s untouched',
-      id: 'IWsI3H',
-    }),
-    ...params,
-  });
+  const { description, pathname, locale, title } = params;
+
+  return {
+    alternates: {
+      canonical: pathname,
+    },
+    description,
+    metadataBase: new URL(getSiteUrl()),
+    openGraph: {
+      description,
+      images: '/img/seo/og-projects.jpg',
+      locale,
+      siteName: intl.formatMessage({
+        defaultMessage: 'GreatFrontEnd Projects',
+        description: 'Title of Projects product',
+        id: 'ctbPZV',
+      }),
+      title,
+      type: 'website',
+      url: pathname,
+    },
+    title: {
+      default: title,
+      template: intl.formatMessage({
+        defaultMessage:
+          '%s | GreatFrontEnd Projects - Real-world project challenges',
+        description:
+          'SEO title with placeholder, please leave the %s untouched',
+        id: 'IWsI3H',
+      }),
+    },
+    twitter: {
+      card: 'summary_large_image',
+      creator: '@greatfrontend',
+      description,
+      images: '/img/seo/og-projects.jpg',
+      site: '@greatfrontend',
+      title,
+    },
+  };
 }
