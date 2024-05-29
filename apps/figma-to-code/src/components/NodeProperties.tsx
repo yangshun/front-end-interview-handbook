70ff9828a05239567d58c8a22c56e427e388f64d
Yangshun <tay.yang.shun@gmail.com>
2024-05-29 12:36:00 +0800

    [f2c] remove node properties

--- apps/figma-to-code/src/components/NodeProperties.tsx
+++ apps/figma-to-code/src/components/NodeProperties.tsx
@@ -3,47 +3,8 @@ import { Fragment, h } from 'preact';
 
 import { CopyCodeSection } from './CopyCodeSection';
 import { HTMLCodeSection } from './HTMLCodeSection';
-import { useCopyToClipboard } from '../hooks/useCopyToClipboard';
 import { visitGFENode } from '../nodes/processGFENode';
 import type { GFENode } from '../nodes/types';
-
-function PropertyRow({
-  label,
-  value,
-}: Readonly<{
-  label: string;
-  value: number | string;
-}>) {
-  const [, copyCss] = useCopyToClipboard();
-
-  return (
-    <div
-      className={clsx(
-        'flex gap-2',
-        'px-3 py-1',
-        'w-full',
-        'hover:bg-neutral-100 hover:dark:bg-neutral-700',
-        'transition-colors',
-      )}
-      onClick={() => {
-        copyCss(String(value));
-      }}>
-      <div
-        className={clsx(
-          'shrink-0 w-20 text-neutral-600 dark:text-neutral-300',
-        )}>
-        {label}
-      </div>
-      <div
-        className={clsx(
-          'grow truncate select-text',
-          'text-neutral-950 dark:text-white',
-        )}>
-        {value}
-      </div>
-    </div>
-  );
-}
 
 export function NodeProperties({
   node,
@@ -54,8 +15,7 @@ export function NodeProperties({
     return <div>Loading</div>;
   }
 
-  const { content, properties, cssProperties, tailwindClasses } =
-    visitGFENode(node);
+  const { content, cssProperties, tailwindClasses } = visitGFENode(node);
 
   return (
     <div
@@ -63,18 +23,6 @@ export function NodeProperties({
         'divide-y divide-neutral-200 dark:divide-neutral-700',
         'pb-4',
       )}>
-      <div className="py-3 flex flex-col gap-y-3">
-        <h2 className="px-3 font-semibold">Properties</h2>
-        <div>
-          <PropertyRow label="Width" value={`${node.width}px`} />
-          <PropertyRow label="Height" value={`${node.height}px`} />
-          <Fragment>
-            {properties.map(({ value, label }) => (
-              <PropertyRow label={label} value={value} />
-            ))}
-          </Fragment>
-        </div>
-      </div>
       <CopyCodeSection
         title="CSS"
         code={
