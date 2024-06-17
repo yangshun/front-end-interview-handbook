e97c28a4b6db31fe8c1dc5293247e0f6d251a02c
Vikas yadav <40167428+thejsdeveloper@users.noreply.github.com>
2024-06-17 14:28:09 +0800

    [web] projects/challenges/brief: improve image carousel (#688)
    Co-authored-by: GitHub Actions <github-actions[bot]@users.noreply.github.com>

--- apps/web/src/components/projects/challenges/brief/ProjectsChallengeBriefImageCarousel.tsx
+++ apps/web/src/components/projects/challenges/brief/ProjectsChallengeBriefImageCarousel.tsx
@@ -1,23 +1,67 @@
 import clsx from 'clsx';
 import React, { useEffect, useRef, useState } from 'react';
 import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import Button from '~/components/ui/Button';
 import Text from '~/components/ui/Text';
 import { themeBackgroundBrandColor } from '~/components/ui/theme';
+
+function NavigationButton({
+  direction,
+  onClick,
+}: Readonly<{
+  direction: 'left' | 'right';
+  onClick: () => void;
+}>) {
+  const intl = useIntl();
+
+  const icon = direction === 'left' ? RiArrowLeftSLine : RiArrowRightSLine;
+  const label =
+    direction === 'left'
+      ? intl.formatMessage({
+          defaultMessage: 'Previous image',
+          description: 'Label for previous image',
+          id: '+vnNEk',
+        })
+      : intl.formatMessage({
+          defaultMessage: 'Next image',
+          description: 'Label for next image',
+          id: 'sV/OUK',
+        });
+
+  return (
+    <div
+      className={clsx(
+        'group absolute bottom-0 top-0 w-12',
+        direction === 'left' ? 'left-0' : 'right-0',
+        'flex items-center justify-center',
+        'bg-none hover:bg-neutral-800/40',
+      )}>
+      <Button
+        className="parent hidden text-white group-hover:flex"
+        icon={icon}
+        isLabelHidden={true}
+        label={label}
+        size="lg"
+        variant="tertiary"
+        onClick={onClick}
+      />
+    </div>
+  );
+}
 
 type Props = Readonly<{
   images: Array<string>;
 }>;
 
 export default function ProjectsChallengeBriefImageCarousel({ images }: Props) {
-  const intl = useIntl();
   const ref = useRef<HTMLDivElement>(null);
   const imageListRef = useRef<HTMLDivElement>(null);
   const [current, setCurrent] = useState(0);
   const [imageWidth, setImageWidth] = useState(0);
   const GAP_BETWEEN_IMAGE = 8;
+  const showInformationText = !!imageWidth;
 
   const scrollImage = (direction: 'left' | 'right') => {
     const maxScrollLeft =
@@ -80,76 +124,69 @@ export default function ProjectsChallengeBriefImageCarousel({ images }: Props) {
   });
 
   return (
-    <div ref={ref} className="relative w-full rounded-lg">
-      <div
-        ref={imageListRef}
-        className="no-scrollbar grid snap-x snap-mandatory overflow-x-auto"
-        style={{
-          gap: `${GAP_BETWEEN_IMAGE}px`,
-          gridTemplateColumns: `repeat(${images.length}, 1fr)`,
-        }}>
-        {images.map((image) => (
-          <img
-            key={image}
-            alt=""
-            className="aspect-[1500/1116] w-full snap-center rounded-lg object-cover"
-            decoding="async"
-            loading="lazy"
-            src={image}
-            style={{
-              maxWidth: `${imageWidth}px`,
-              minWidth: `${imageWidth}px`,
-            }}
-          />
-        ))}
-      </div>
-      {images.length > 1 && (
-        <>
-          <div className="absolute left-2 top-1/2 -translate-y-1/2 transform">
-            <Button
-              icon={RiArrowLeftSLine}
-              isLabelHidden={true}
-              label={intl.formatMessage({
-                defaultMessage: 'Previous image',
-                description: 'Label for previous image',
-                id: '+vnNEk',
-              })}
-              variant="secondary"
+    <div className="flex flex-col items-center gap-y-4">
+      <div ref={ref} className="relative w-full overflow-clip rounded-lg">
+        <div
+          ref={imageListRef}
+          className="no-scrollbar grid snap-x snap-mandatory overflow-x-auto"
+          style={{
+            gap: `${GAP_BETWEEN_IMAGE}px`,
+            gridTemplateColumns: `repeat(${images.length}, 1fr)`,
+          }}>
+          {images.map((image) => (
+            <img
+              key={image}
+              alt=""
+              className="aspect-[1500/1116] w-full snap-center rounded-lg object-cover"
+              decoding="async"
+              loading="lazy"
+              src={image}
+              style={{
+                maxWidth: `${imageWidth}px`,
+                minWidth: `${imageWidth}px`,
+              }}
+            />
+          ))}
+        </div>
+        {images.length > 1 && (
+          <>
+            <NavigationButton
+              direction="left"
               onClick={() => scrollImage('left')}
             />
-          </div>
-          <div className="absolute right-2 top-1/2 -translate-y-1/2 transform">
-            <Button
-              icon={RiArrowRightSLine}
-              isLabelHidden={true}
-              label={intl.formatMessage({
-                defaultMessage: 'Next image',
-                description: 'Label for next image',
-                id: 'sV/OUK',
-              })}
-              variant="secondary"
+            <NavigationButton
+              direction="right"
               onClick={() => scrollImage('right')}
             />
-          </div>
-          <div className="absolute bottom-0 right-0 mb-3 me-3 flex rounded-full bg-neutral-300 px-2 py-0.5">
-            <Text color="dark" size="body3" weight="medium">
-              {current + 1}/{images.length}
-            </Text>
-          </div>
-          <div className="absolute bottom-0 left-1/2 mb-3 flex -translate-x-1/2 gap-2">
-            {images.map((image, index) => (
-              <div
-                key={image}
-                className={clsx(
-                  'h-1.5 w-5 flex-1 rounded-lg',
-                  index === current
-                    ? themeBackgroundBrandColor
-                    : 'bg-neutral-800/40',
-                )}
-              />
-            ))}
-          </div>
-        </>
+            <div className="absolute bottom-0 right-0 mb-3 me-3 flex rounded-full bg-neutral-300 px-2 py-0.5">
+              <Text color="dark" size="body3" weight="medium">
+                {current + 1}/{images.length}
+              </Text>
+            </div>
+            <div className="absolute bottom-0 left-1/2 mb-3 flex -translate-x-1/2 gap-2">
+              {images.map((image, index) => (
+                <div
+                  key={image}
+                  className={clsx(
+                    'h-0.5 w-6 flex-1 rounded-lg',
+                    index === current
+                      ? themeBackgroundBrandColor
+                      : 'bg-neutral-800/40',
+                  )}
+                />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+      {showInformationText && (
+        <Text color="secondary" size="body3" weight="normal">
+          <FormattedMessage
+            defaultMessage="Images just for illustration. Actual page(s) could be longer"
+            description="Info for carousel images in project brief"
+            id="Ae22GD"
+          />
+        </Text>
       )}
     </div>
   );
