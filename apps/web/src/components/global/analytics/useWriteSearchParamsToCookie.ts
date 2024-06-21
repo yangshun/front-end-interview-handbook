84ca70b5066e6187b397d44764461bee1a2779d8
Yangshun <tay.yang.shun@gmail.com>
2024-06-22 07:54:03 +0800

    [web] analytics: add normalized tracking source

--- apps/web/src/components/global/analytics/useWriteSearchParamsToCookie.ts
+++ apps/web/src/components/global/analytics/useWriteSearchParamsToCookie.ts
@@ -2,8 +2,17 @@ import jsCookie from 'js-cookie';
 import { useEffect } from 'react';
 
 const ALLOWLISTED_KEYS = new Set([
+  'gnrs', // GFE normalized referral source.
   'fpr', // First promoter query parameter.
 ]);
+
+// Aggregate a single value to track referral source, this is the order of importance.
+const NORMALIZED_REFERRAL_SOURCES = [
+  'gnrs',
+  'fpr',
+  'utm_source',
+  'utm_campaign',
+];
 
 export function shouldPersistQueryParam(key: string) {
   return key.startsWith('utm_') || ALLOWLISTED_KEYS.has(key);
@@ -14,6 +23,24 @@ export function useWriteSearchParamsToCookie() {
     try {
       const searchParams = new URLSearchParams(window.location.search);
       const timestamp = new Date().toISOString();
+
+      // Process referral sources first since `gnrs` can also
+      // appear below.
+      NORMALIZED_REFERRAL_SOURCES.forEach((key) => {
+        const value = searchParams.get(key);
+
+        // Ignore if value already exists.
+        if (jsCookie.get('gnrs') || !value) {
+          return;
+        }
+
+        const payload = {
+          time: timestamp,
+          value,
+        };
+
+        jsCookie.set('gnrs', JSON.stringify(payload), { expires: 14 });
+      });
 
       searchParams.forEach((value, key) => {
         if (!shouldPersistQueryParam(key)) {
