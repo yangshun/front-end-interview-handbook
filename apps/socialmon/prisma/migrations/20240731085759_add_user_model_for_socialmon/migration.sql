4200c3aa854a009fdb0c38b61ebd45675714c17a
Nitesh Seram <niteshseram@gmail.com>
2024-08-01 20:09:35 +0530

    [socialmon] user: add socialmon user model for next-auth (#768)

---
+++ apps/socialmon/prisma/migrations/20240731085759_add_user_model_for_socialmon/migration.sql
@@ -0,0 +1,63 @@
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "email_verified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verificationtokens" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verificationtokens_identifier_token_key" ON "verificationtokens"("identifier", "token");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- apps/socialmon/src/app/lib/auth.ts
+++ apps/socialmon/src/app/lib/auth.ts
@@ -2,6 +2,11 @@ import type { GetServerSidePropsContext } from 'next';
 import { getServerSession, type NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import prisma from '~/server/prisma';
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import type { PrismaClient } from '@prisma/client';
+
 export const AUTHORIZED_EMAILS = [
   'yangshun@greatfrontend.com',
   'gina@greatfrontend.com',
@@ -10,6 +15,7 @@ export const AUTHORIZED_EMAILS = [
 ];
 
 export const authConfig: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma as unknown as PrismaClient),
   callbacks: {
     async signIn({ profile }) {
       if (AUTHORIZED_EMAILS.includes(profile?.email ?? '')) {
