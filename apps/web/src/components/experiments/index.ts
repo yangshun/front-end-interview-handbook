cbb24811c64e900666dd87cc3fbe03259fc0b626
Yangshun <tay.yang.shun@gmail.com>
2024-09-09 15:15:03 +0800

    [web] interviews/sidebar: turn off experiment

--- apps/web/src/components/experiments/index.ts
+++ apps/web/src/components/experiments/index.ts
@@ -11,11 +11,17 @@ export type ABExperiment = Readonly<{
   };
 }>;
 
+const isRunning = false;
+
 export const currentExperiment: ABExperiment = {
   getValue_USE_ON_CLIENT_ONLY() {
+    if (!isRunning) {
+      return currentExperiment.variants.a;
+    }
+
     return jsCookie.get(currentExperiment.name) ?? currentExperiment.variants.a;
   },
-  isRunning: true,
+  isRunning,
   loggingPrefix: '',
   name: 'gfe:2024_sep_interviews_sidebar',
   variants: {
