66c773737f535f692fb24e3c7eab1de90b50df64
DanielJames0302 <137600847+DanielJames0302@users.noreply.github.com>
2024-11-06 16:01:05 +0800

    [web] blog: migrate series page sidebar to common component (#923)

--- apps/web/src/components/blog/series/BlogExploreSeriesSidebar.tsx
+++ apps/web/src/components/blog/series/BlogExploreSeriesSidebar.tsx
@@ -1,45 +1,17 @@
 import clsx from 'clsx';
 import type { BlogCategory } from 'contentlayer/generated';
 
+import type { SideNavigationItems } from '~/components/common/SideNavigation';
+import SideNavigation from '~/components/common/SideNavigation';
 import Section from '~/components/ui/Heading/HeadingContext';
-import {
-  themeBorderColor,
-  themeTextBrandColor,
-  themeTextSecondaryColor,
-} from '~/components/ui/theme';
 
-function LinksList({
-  items,
-  activeItem,
-  onChange,
-}: Readonly<{
-  activeItem: string;
-  items: ReadonlyArray<BlogCategory>;
-  onChange: (value: string) => void;
-}>) {
-  return (
-    <ul
-      className={clsx('flex flex-col gap-y-3 border-l', themeBorderColor)}
-      role="list">
-      {items.map((link) => (
-        <div key={link.href}>
-          <div
-            className={clsx(
-              '-ml-px flex w-full cursor-pointer items-center gap-x-2 border-l text-sm',
-              activeItem === link.source
-                ? clsx(themeTextBrandColor, 'border-current font-semibold')
-                : clsx(
-                    themeTextSecondaryColor,
-                    'border-transparent hover:border-current hover:text-neutral-800 dark:hover:text-white',
-                  ),
-            )}
-            onClick={() => onChange(link.source)}>
-            <span className="pl-4">{link.title}</span>
-          </div>
-        </div>
-      ))}
-    </ul>
-  );
+function convertToSideNavigationItem(
+  tocItems: ReadonlyArray<BlogCategory>,
+): SideNavigationItems<string> {
+  return tocItems.map((item) => ({
+    label: item.title,
+    value: item.source,
+  }));
 }
 
 type BlogExploreSeriesSidebar = Readonly<{
@@ -57,10 +29,10 @@ export function BlogExploreSeriesSidebar({
   return (
     <nav className={clsx('flex w-[250px] shrink-0 flex-col')}>
       <Section>
-        <LinksList
-          activeItem={activeItem}
-          items={navigation}
-          onChange={onChange}
+        <SideNavigation
+          activeValue={activeItem}
+          items={convertToSideNavigationItem(navigation)}
+          onClick={onChange}
         />
       </Section>
     </nav>
