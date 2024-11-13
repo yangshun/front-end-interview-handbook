5a84206849b1db86e0e81f868730fbf715a13871
Yangshun <tay.yang.shun@gmail.com>
2024-11-13 10:38:19 +0800

    [web] ui/mdx: show line numbers conditionally

--- apps/web/src/components/mdx/MDXCodeBlock.tsx
+++ apps/web/src/components/mdx/MDXCodeBlock.tsx
@@ -18,6 +18,8 @@ import useHoverState from '~/hooks/useHoverState';
 import Button from '~/components/ui/Button';
 import TabsUnderline from '~/components/ui/Tabs/TabsUnderline';
 
+const MIN_LINES_TO_SHOW_LINE_NUMBERS = 5;
+
 type LanguagesCode = Partial<Record<Language, React.ReactNode>>;
 type LanguagesLabels = Partial<Record<Language, string>>;
 
@@ -92,7 +94,7 @@ export function convertContentToCode(
 export default function MDXCodeBlock({
   children,
   showCopyButton = true,
-  showLineNumbers = true,
+  showLineNumbers,
   renderExtraButtons,
   language = 'jsx',
   languages = {},
@@ -149,7 +151,7 @@ export default function MDXCodeBlock({
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}>
         {isHovered && (
-          <div className="absolute right-2 top-2 flex gap-x-1 p-1">
+          <div className="absolute right-1 top-1 flex gap-x-1 p-1">
             {renderExtraButtons?.(selectedCode)}
             {showCopyButton && <CopyButton contents={selectedCode} />}
           </div>
@@ -158,55 +160,61 @@ export default function MDXCodeBlock({
           code={selectedCode.trim()}
           language={selectedLanguage}
           theme={themes.dracula}>
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre
-              className={clsx(
-                'code-block__counter',
-                className,
-                showLineNumbers && 'pl-0',
-              )}
-              style={{
-                ...style,
-                // @ts-expect-error: CSS variable
-                '--code-bg-color': style.backgroundColor,
-                '--code-color': style.color,
-              }}>
-              {tokens.map((line, index) => {
-                const lineKey = index;
-                const { className: lineClassName, ...lineProps } = getLineProps(
-                  {
-                    key: lineKey,
-                    line,
-                  },
-                );
+          {({ className, style, tokens, getLineProps, getTokenProps }) => {
+            const showLineNumbersValue =
+              showLineNumbers == null
+                ? tokens.length >= MIN_LINES_TO_SHOW_LINE_NUMBERS
+                : true;
 
-                const lineContents =
-                  renderLineContents != null
-                    ? renderLineContents({ getTokenProps, line })
-                    : line.map((token, tokenKey) => (
-                        <span
-                          {...getTokenProps({
-                            token,
-                          })}
-                          // eslint-disable-next-line react/no-array-index-key
-                          key={tokenKey}
-                        />
-                      ));
+            return (
+              <pre
+                className={clsx(
+                  'code-block__counter',
+                  className,
+                  showLineNumbers && 'pl-0',
+                )}
+                style={{
+                  ...style,
+                  // @ts-expect-error: CSS variable
+                  '--code-bg-color': style.backgroundColor,
+                  '--code-color': style.color,
+                }}>
+                {tokens.map((line, index) => {
+                  const lineKey = index;
+                  const { className: lineClassName, ...lineProps } =
+                    getLineProps({
+                      key: lineKey,
+                      line,
+                    });
 
-                return (
-                  <div
-                    className={clsx(
-                      showLineNumbers && 'code-line__counter',
-                      lineClassName,
-                    )}
-                    {...lineProps}
-                    key={lineKey}>
-                    {lineContents}
-                  </div>
-                );
-              })}
-            </pre>
-          )}
+                  const lineContents =
+                    renderLineContents != null
+                      ? renderLineContents({ getTokenProps, line })
+                      : line.map((token, tokenKey) => (
+                          <span
+                            {...getTokenProps({
+                              token,
+                            })}
+                            // eslint-disable-next-line react/no-array-index-key
+                            key={tokenKey}
+                          />
+                        ));
+
+                  return (
+                    <div
+                      className={clsx(
+                        showLineNumbersValue && 'code-line__counter',
+                        lineClassName,
+                      )}
+                      {...lineProps}
+                      key={lineKey}>
+                      {lineContents}
+                    </div>
+                  );
+                })}
+              </pre>
+            );
+          }}
         </Highlight>
       </div>
     </div>
