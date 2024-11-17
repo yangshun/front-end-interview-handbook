29b9a01f23ff2409b64aa6f2ff4d63b3baff0140
Yangshun <tay.yang.shun@gmail.com>
2024-11-17 14:23:22 +0800

    [web] interviews/recommended: refactor to remove state

--- apps/web/src/components/interviews/recommended/InterviewsRecommendedPrepStrategyPageTitleSection.tsx
+++ apps/web/src/components/interviews/recommended/InterviewsRecommendedPrepStrategyPageTitleSection.tsx
@@ -1,223 +1,16 @@
 import clsx from 'clsx';
-import { usePathname } from 'next/navigation';
-import { type ReactNode, useState } from 'react';
-import { FaCheck } from 'react-icons/fa6';
-import { RiArrowDownSLine, RiArrowRightLine } from 'react-icons/ri';
-
-import { trpc } from '~/hooks/trpc';
-
-import { useGuidesData } from '~/data/Guides';
+import { type ReactNode } from 'react';
 
 import { FormattedMessage, useIntl } from '~/components/intl';
-import Anchor from '~/components/ui/Anchor';
 import Badge from '~/components/ui/Badge';
-import Chip from '~/components/ui/Chip';
-import Popover from '~/components/ui/Popover';
-import Text from '~/components/ui/Text';
-import {
-  themeBorderElementColor,
-  themeOutlineElementBrandColor_FocusVisible,
-  themeTextBrandColor_GroupHover,
-  themeTextSubtleColor,
-} from '~/components/ui/theme';
 import Tooltip from '~/components/ui/Tooltip';
 
-import { useGuideCompletionCount } from '~/db/guides/GuidesProgressClient';
 import type { QuestionProgress } from '~/db/QuestionsProgressTypes';
-import { categorizeQuestionsProgress } from '~/db/QuestionsUtils';
 
+import InterviewsRecommendedPrepStrategyPopover from './InterviewsRecommendedPrepStrategyPopover';
 import InterviewsPageHeaderActions from '../common/InterviewsPageHeaderActions';
 import type { QuestionMetadata } from '../questions/common/QuestionsTypes';
 import QuestionsStudyListPageTitleSection from '../questions/listings/learning/QuestionsStudyListPageTitleSection';
-
-import { useUser } from '@supabase/auth-helpers-react';
-
-function RecommendedItemsPopover({
-  overallProgress,
-}: Readonly<{
-  overallProgress: ReadonlyArray<QuestionProgress>;
-}>) {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  const user = useUser();
-  const { data: questionListSessions } =
-    trpc.questionLists.getActiveSessions.useQuery(undefined, {
-      enabled: !!user,
-    });
-  const { data: recommendedPrepData } =
-    trpc.questionLists.getRecommendedStudyList.useQuery(undefined, {
-      enabled: isPopoverOpen,
-    });
-
-  const sessions = questionListSessions ?? [];
-  const gfe75session = sessions.find(
-    (session) =>
-      recommendedPrepData && session.key === recommendedPrepData.gfe75.listKey,
-  );
-  const blind75session = sessions.find(
-    (session) =>
-      recommendedPrepData &&
-      session.key === recommendedPrepData.blind75.listKey,
-  );
-  const { frontendInterviewPlaybook, systemDesignPlaybook } =
-    useGuideCompletionCount();
-  const questionsProgressAll = categorizeQuestionsProgress(overallProgress);
-
-  const guidesData = useGuidesData();
-
-  const items = [
-    {
-      href: guidesData['front-end-interview-playbook'].href,
-      isCompleted:
-        frontendInterviewPlaybook.total === frontendInterviewPlaybook.completed,
-      isLoading: false,
-      label: guidesData['front-end-interview-playbook'].name,
-    },
-    {
-      href: '/interviews/gfe75',
-      isCompleted:
-        recommendedPrepData &&
-        gfe75session?._count.progress ===
-          recommendedPrepData.gfe75.questionCount,
-      label: 'GFE 75',
-    },
-    {
-      href: '/interviews/blind75',
-      isCompleted:
-        recommendedPrepData &&
-        blind75session?._count.progress ===
-          recommendedPrepData.blind75.questionCount,
-      label: 'Blind 75',
-    },
-    {
-      href: guidesData['front-end-system-design-playbook'].href,
-      isCompleted:
-        recommendedPrepData &&
-        questionsProgressAll['system-design'].size +
-          systemDesignPlaybook.completed ===
-          systemDesignPlaybook.total +
-            recommendedPrepData.systemDesignQuestionCount,
-      label: guidesData['front-end-system-design-playbook'].shortName,
-    },
-  ];
-
-  const pathname = usePathname();
-
-  return (
-    <Popover
-      className="flex flex-col gap-6 !p-6"
-      side="bottom"
-      trigger={
-        <button
-          className={clsx(themeOutlineElementBrandColor_FocusVisible)}
-          type="button"
-          onClick={() => setIsPopoverOpen(true)}>
-          <Tooltip
-            asChild={true}
-            label={
-              <FormattedMessage
-                defaultMessage="Explore other items from our recommended preparation strategy."
-                description="Tooltip for other recommended preparation strategy"
-                id="/7J8Gp"
-              />
-            }>
-            <div className="flex items-center gap-1">
-              <Text
-                className="line-clamp-1 text-ellipsis text-left"
-                size="body3"
-                weight="medium">
-                <FormattedMessage
-                  defaultMessage="{count} other items"
-                  description="Trigger label for other items"
-                  id="59u5/i"
-                  values={{
-                    count: items.length - 1,
-                  }}
-                />
-              </Text>
-              <RiArrowDownSLine
-                aria-hidden={true}
-                className={clsx('size-4 shrink-0', themeTextSubtleColor)}
-              />
-            </div>
-          </Tooltip>
-        </button>
-      }>
-      <Text className="block" color="secondary" size="body3" weight="medium">
-        <FormattedMessage
-          defaultMessage="Recommended prep strategy"
-          description="Label for other items dropdown"
-          id="gBdxyo"
-        />
-      </Text>
-      <div className="flex flex-col gap-6">
-        {items.map(({ label, href, isCompleted }, index) => {
-          const isSelected = pathname ? href.startsWith(pathname) : false;
-
-          return (
-            <div key={label} className="flex w-full gap-4">
-              <div
-                className={clsx(
-                  'relative flex flex-col justify-center self-stretch',
-                )}>
-                {isCompleted ? (
-                  <Chip
-                    icon={FaCheck}
-                    iconClassName="size-4"
-                    isLabelHidden={true}
-                    label="Completed"
-                    size="sm"
-                    variant="success"
-                  />
-                ) : (
-                  <Chip
-                    label={(index + 1).toString()}
-                    size="sm"
-                    variant={isSelected ? 'neutral-active' : 'neutral'}
-                  />
-                )}
-                {index < items.length - 1 && (
-                  <div
-                    className={clsx(
-                      'absolute top-full h-4 w-px translate-y-1 self-center border-l',
-                      themeBorderElementColor,
-                    )}
-                  />
-                )}
-              </div>
-              <Anchor
-                className={clsx(
-                  'flex grow items-center justify-between gap-4',
-                  'group',
-                  'transition-colors',
-                )}
-                href={href}
-                variant="unstyled">
-                <Text
-                  className={clsx(
-                    'block group-hover:text-neutral-900 dark:group-hover:text-neutral-100',
-                  )}
-                  color={isSelected ? 'default' : 'secondary'}
-                  size="body2"
-                  weight={isSelected ? 'bold' : 'normal'}>
-                  {label}
-                </Text>
-                {!isSelected && (
-                  <RiArrowRightLine
-                    className={clsx(
-                      'size-4 shrink-0',
-                      themeTextSubtleColor,
-                      themeTextBrandColor_GroupHover,
-                    )}
-                  />
-                )}
-              </Anchor>
-            </div>
-          );
-        })}
-      </div>
-    </Popover>
-  );
-}
 
 type CommonProps = Readonly<{
   description: ReactNode;
@@ -289,9 +82,10 @@ export default function InterviewsRecommendedPrepStrategyPageTitleSection({
                 variant="neutral-active"
               />
             </Tooltip>
-            <RecommendedItemsPopover overallProgress={overallProgress ?? []} />
+            <InterviewsRecommendedPrepStrategyPopover
+              overallProgress={overallProgress ?? []}
+            />
           </div>
-
           {metadata && (
             <div className="flex flex-1 justify-end">
               <InterviewsPageHeaderActions metadata={metadata} />
---
+++ apps/web/src/components/interviews/recommended/InterviewsRecommendedPrepStrategyPopover.tsx
@@ -0,0 +1,223 @@
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+import { FaCheck } from 'react-icons/fa6';
+import { RiArrowDownSLine, RiArrowRightLine } from 'react-icons/ri';
+
+import { trpc } from '~/hooks/trpc';
+
+import { useGuidesData } from '~/data/Guides';
+
+import { FormattedMessage } from '~/components/intl';
+import Anchor from '~/components/ui/Anchor';
+import Chip from '~/components/ui/Chip';
+import Popover from '~/components/ui/Popover';
+import Text from '~/components/ui/Text';
+import {
+  themeBorderElementColor,
+  themeOutlineElementBrandColor_FocusVisible,
+  themeTextBrandColor_GroupHover,
+  themeTextSubtleColor,
+} from '~/components/ui/theme';
+import Tooltip from '~/components/ui/Tooltip';
+
+import { useGuideCompletionCount } from '~/db/guides/GuidesProgressClient';
+import type { QuestionProgress } from '~/db/QuestionsProgressTypes';
+import { categorizeQuestionsProgress } from '~/db/QuestionsUtils';
+
+import { useUser } from '@supabase/auth-helpers-react';
+
+function InterviewsRecommendedPrepStrategyPopoverContents({
+  overallProgress,
+}: Readonly<{
+  overallProgress: ReadonlyArray<QuestionProgress>;
+}>) {
+  const user = useUser();
+  const { data: questionListSessions } =
+    trpc.questionLists.getActiveSessions.useQuery(undefined, {
+      enabled: !!user,
+    });
+  const { data: recommendedPrepData } =
+    trpc.questionLists.getRecommendedStudyList.useQuery();
+
+  const sessions = questionListSessions ?? [];
+  const gfe75session = sessions.find(
+    (session) =>
+      recommendedPrepData && session.key === recommendedPrepData.gfe75.listKey,
+  );
+  const blind75session = sessions.find(
+    (session) =>
+      recommendedPrepData &&
+      session.key === recommendedPrepData.blind75.listKey,
+  );
+  const { frontendInterviewPlaybook, systemDesignPlaybook } =
+    useGuideCompletionCount();
+  const questionsProgressAll = categorizeQuestionsProgress(overallProgress);
+
+  const guidesData = useGuidesData();
+
+  const items = [
+    {
+      href: guidesData['front-end-interview-playbook'].href,
+      isCompleted:
+        frontendInterviewPlaybook.total === frontendInterviewPlaybook.completed,
+      label: guidesData['front-end-interview-playbook'].name,
+    },
+    {
+      href: '/interviews/gfe75',
+      isCompleted:
+        recommendedPrepData &&
+        gfe75session?._count.progress ===
+          recommendedPrepData.gfe75.questionCount,
+      label: 'GFE 75',
+    },
+    {
+      href: '/interviews/blind75',
+      isCompleted:
+        recommendedPrepData &&
+        blind75session?._count.progress ===
+          recommendedPrepData.blind75.questionCount,
+      label: 'Blind 75',
+    },
+    {
+      href: guidesData['front-end-system-design-playbook'].href,
+      isCompleted:
+        recommendedPrepData &&
+        questionsProgressAll['system-design'].size +
+          systemDesignPlaybook.completed ===
+          systemDesignPlaybook.total +
+            recommendedPrepData.systemDesignQuestionCount,
+      label: guidesData['front-end-system-design-playbook'].shortName,
+    },
+  ];
+
+  const pathname = usePathname();
+
+  return (
+    <>
+      <Text className="block" color="secondary" size="body3" weight="medium">
+        <FormattedMessage
+          defaultMessage="Recommended prep strategy"
+          description="Label for other items dropdown"
+          id="gBdxyo"
+        />
+      </Text>
+      <div className="flex flex-col gap-6">
+        {items.map(({ label, href, isCompleted }, index) => {
+          const isSelected = pathname ? href.startsWith(pathname) : false;
+
+          return (
+            <div key={label} className="flex w-full gap-4">
+              <div
+                className={clsx(
+                  'relative flex flex-col justify-center self-stretch',
+                )}>
+                {isCompleted ? (
+                  <Chip
+                    icon={FaCheck}
+                    iconClassName="size-4"
+                    isLabelHidden={true}
+                    label="Completed"
+                    size="sm"
+                    variant="success"
+                  />
+                ) : (
+                  <Chip
+                    label={(index + 1).toString()}
+                    size="sm"
+                    variant={isSelected ? 'neutral-active' : 'neutral'}
+                  />
+                )}
+                {index < items.length - 1 && (
+                  <div
+                    className={clsx(
+                      'absolute top-full h-4 w-px translate-y-1 self-center border-l',
+                      themeBorderElementColor,
+                    )}
+                  />
+                )}
+              </div>
+              <Anchor
+                className={clsx(
+                  'flex grow items-center justify-between gap-4',
+                  'group',
+                  'transition-colors',
+                )}
+                href={href}
+                variant="unstyled">
+                <Text
+                  className={clsx(
+                    'block group-hover:text-neutral-900 dark:group-hover:text-neutral-100',
+                  )}
+                  color={isSelected ? 'default' : 'secondary'}
+                  size="body2"
+                  weight={isSelected ? 'bold' : 'normal'}>
+                  {label}
+                </Text>
+                {!isSelected && (
+                  <RiArrowRightLine
+                    className={clsx(
+                      'size-4 shrink-0',
+                      themeTextSubtleColor,
+                      themeTextBrandColor_GroupHover,
+                    )}
+                  />
+                )}
+              </Anchor>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default function InterviewsRecommendedPrepStrategyPopover({
+  overallProgress,
+}: Readonly<{
+  overallProgress: ReadonlyArray<QuestionProgress>;
+}>) {
+  return (
+    <Popover
+      className="flex flex-col gap-6 !p-6"
+      side="bottom"
+      trigger={
+        <button
+          className={clsx(themeOutlineElementBrandColor_FocusVisible)}
+          type="button">
+          <Tooltip
+            asChild={true}
+            label={
+              <FormattedMessage
+                defaultMessage="Explore other items from our recommended preparation strategy."
+                description="Tooltip for other recommended preparation strategy"
+                id="/7J8Gp"
+              />
+            }>
+            <div className="flex items-center gap-1">
+              <Text
+                className="line-clamp-1 text-ellipsis text-left"
+                size="body3"
+                weight="medium">
+                <FormattedMessage
+                  defaultMessage="{count} other items"
+                  description="Trigger label for other items"
+                  id="59u5/i"
+                  values={{
+                    count: 3,
+                  }}
+                />
+              </Text>
+              <RiArrowDownSLine
+                aria-hidden={true}
+                className={clsx('size-4 shrink-0', themeTextSubtleColor)}
+              />
+            </div>
+          </Tooltip>
+        </button>
+      }>
+      <InterviewsRecommendedPrepStrategyPopoverContents
+        overallProgress={overallProgress}
+      />
+    </Popover>
+  );
+}
