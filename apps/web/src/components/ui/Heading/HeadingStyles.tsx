ff9d1a028da2b472d85728938fe9f44d0e7eb840
Nitesh Seram <niteshseram@gmail.com>
2024-11-19 10:13:13 +0530

    [web] ui/heading: update heading size breakpoint (#1040)
    Co-authored-by: GitHub Actions <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Yangshun Tay <tay.yang.shun@gmail.com>

--- apps/web/src/components/ui/Heading/HeadingStyles.tsx
+++ apps/web/src/components/ui/Heading/HeadingStyles.tsx
@@ -21,12 +21,12 @@ export type HeadingWeight = 'default' | 'medium';
 const headingLevelClasses: Record<HeadingLevel, string> = {
   custom: '',
   heading1: clsx(
-    'lg:text-6xl lg:-tracking-4 lg:leading-[4rem]',
+    'xl:text-6xl xl:-tracking-4 lg:leading-[4rem]',
     'text-5xl -tracking-3',
   ),
-  heading2: clsx('lg:text-5xl lg:-tracking-3', 'text-4xl -tracking-2'),
-  heading3: clsx('lg:text-4xl lg:-tracking-2', 'text-3xl -tracking-1'),
-  heading4: clsx('lg:text-3xl lg:-tracking-1', 'text-2xl'),
+  heading2: clsx('xl:text-5xl xl:-tracking-3', 'text-4xl -tracking-2'),
+  heading3: clsx('xl:text-4xl xl:-tracking-2', 'text-3xl -tracking-1'),
+  heading4: clsx('xl:text-3xl xl:-tracking-1', 'text-2xl'),
   heading5: clsx('text-2xl'),
   heading6: clsx('text-xl'),
 };
