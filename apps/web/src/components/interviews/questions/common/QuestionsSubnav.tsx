b5f44e74d31ec511d22ad67dc5f957406e1800d2
Nitesh Seram <niteshseram@gmail.com>
2024-12-04 15:21:41 +0530

    [web] interviews/qns: add scroll area to questions sub nav (#1113)

--- apps/web/src/components/interviews/questions/common/QuestionsSubnav.tsx
+++ apps/web/src/components/interviews/questions/common/QuestionsSubnav.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 
 import Anchor from '~/components/ui/Anchor';
+import ScrollArea from '~/components/ui/ScrollArea';
 import { textVariants } from '~/components/ui/Text';
 import {
   themeBackgroundColor,
@@ -27,58 +28,63 @@ export default function QuestionsSubnav({ items }: Props) {
     <div
       className={clsx(
         'relative',
-        'flex items-center gap-6 lg:justify-end',
-        'h-[var(--subnav-height)] w-full px-6',
         'z-fixed sticky top-[var(--global-sticky-height)]',
-        'backdrop-blur',
-        'overflow-x-auto',
       )}>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `:root { --subnav-height: ${SUBNAV_HEIGHT}px; }`,
-        }}
-      />
-      <div
-        className={clsx(
-          'max-lg:hidden',
-          'absolute -right-0 top-0 h-px w-full',
-          themeBackgroundColor,
-        )}
-      />
-      <div
-        className={clsx(
-          'max-lg:hidden',
-          'absolute -right-0 top-0 h-px w-1/2',
-          'bg-gradient-to-r from-transparent to-neutral-200 dark:to-neutral-800',
-        )}
-      />
-      {items.map(({ href, label, value, icon: Icon }) => {
-        const isActive = href === pathname;
-
-        return (
-          <Anchor
-            key={value}
+      <ScrollArea scrollbars="horizontal">
+        <div
+          className={clsx(
+            'flex items-center gap-6 lg:justify-end',
+            'h-[var(--subnav-height)] w-full px-6',
+            'backdrop-blur',
+          )}>
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `:root { --subnav-height: ${SUBNAV_HEIGHT}px; }`,
+            }}
+          />
+          <div
             className={clsx(
-              'inline-flex gap-2 whitespace-nowrap',
-              textVariants({
-                color: isActive ? 'default' : 'secondary',
-                size: 'body2',
-                weight: isActive ? 'medium' : 'normal',
-              }),
+              'max-lg:hidden',
+              'absolute -right-0 top-0 h-px w-full',
+              themeBackgroundColor,
             )}
-            href={href}
-            variant="unstyled">
-            <Icon
-              aria-hidden={true}
-              className={clsx(
-                'size-5 shrink-0',
-                !isActive && themeTextSubtleColor,
-              )}
-            />
-            {label}
-          </Anchor>
-        );
-      })}
+          />
+          <div
+            className={clsx(
+              'max-lg:hidden',
+              'absolute -right-0 top-0 h-px w-1/2',
+              'bg-gradient-to-r from-transparent to-neutral-200 dark:to-neutral-800',
+            )}
+          />
+          {items.map(({ href, label, value, icon: Icon }) => {
+            const isActive = href === pathname;
+
+            return (
+              <Anchor
+                key={value}
+                className={clsx(
+                  'inline-flex gap-2 whitespace-nowrap',
+                  textVariants({
+                    color: isActive ? 'default' : 'secondary',
+                    size: 'body2',
+                    weight: isActive ? 'medium' : 'normal',
+                  }),
+                )}
+                href={href}
+                variant="unstyled">
+                <Icon
+                  aria-hidden={true}
+                  className={clsx(
+                    'size-5 shrink-0',
+                    !isActive && themeTextSubtleColor,
+                  )}
+                />
+                {label}
+              </Anchor>
+            );
+          })}
+        </div>
+      </ScrollArea>
     </div>
   );
 }
