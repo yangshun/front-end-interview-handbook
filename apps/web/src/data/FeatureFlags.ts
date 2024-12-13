27dd03d5b9624a9059d48f90475ac57593b1c0f8
Yangshun <tay.yang.shun@gmail.com>
2024-12-13 19:30:16 +0800

    [web] interviews/qns: show bottom content

--- apps/web/src/data/FeatureFlags.ts
+++ apps/web/src/data/FeatureFlags.ts
@@ -3,8 +3,7 @@ export const INTERVIEWS_JS_COMMUNITY_SOLUTIONS_IS_LIVE =
 export const INTERVIEWS_UI_COMMUNITY_SOLUTIONS_IS_LIVE =
   process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
 
-export const INTERVIEWS_REVAMP_BOTTOM_CONTENT =
-  process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
+export const INTERVIEWS_REVAMP_BOTTOM_CONTENT = true;
 export const INTERVIEWS_AUTH_CHANGES_AVAILABLE =
   process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
 
