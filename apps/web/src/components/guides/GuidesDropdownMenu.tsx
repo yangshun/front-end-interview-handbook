4498b347d1126dac0635f21564db45cab32738c1
Nitesh Seram <niteshseram@gmail.com>
2025-01-08 09:41:30 +0530

    [web] guides: update guides dropdown menu with DS dropdown (#1160)

--- apps/web/src/components/guides/GuidesDropdownMenu.tsx
+++ apps/web/src/components/guides/GuidesDropdownMenu.tsx
@@ -4,19 +4,14 @@ import { RiArrowDownSLine } from 'react-icons/ri';
 
 import { useGuidesData } from '~/data/Guides';
 
-import Anchor from '~/components/ui/Anchor';
-import {
-  dropdownContentClassName,
-  dropdownContentItemClassName,
-} from '~/components/ui/DropdownMenu/dropdownStyles';
+import DropdownMenu from '~/components/ui/DropdownMenu';
 import Text from '~/components/ui/Text';
 import {
-  themeTextBrandColor,
+  themeOutlineElementBrandColor_FocusVisible,
   themeTextSubtleColor,
 } from '~/components/ui/theme';
 
 import type { GuidebookItem } from '@prisma/client';
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
 export default function GuidesDropdownMenu({
   guide,
@@ -39,61 +34,46 @@ export default function GuidesDropdownMenu({
   const label = selectedGuide.shortName;
 
   return (
-    <DropdownMenuPrimitive.Root>
-      <DropdownMenuPrimitive.Trigger
-        className={clsx(
-          'group inline-flex flex-1 items-center justify-between gap-2',
-          'px-2 py-1.5',
-          'transition-colors',
-        )}>
-        <div className="text-[0.8125rem] leading-4">
-          <Text
-            className="line-clamp-1 text-ellipsis text-left"
-            color="secondary"
-            size="inherit"
-            weight="medium">
-            {label}
-          </Text>
-        </div>
-        <RiArrowDownSLine
-          aria-hidden="true"
-          className={clsx('size-4 shrink-0', themeTextSubtleColor)}
-        />
-      </DropdownMenuPrimitive.Trigger>
-      <DropdownMenuPrimitive.Portal>
-        <DropdownMenuPrimitive.Content
-          align="start"
-          className={dropdownContentClassName}
-          sideOffset={8}>
-          {Object.values(guides).map(
-            ({ key, firstPageHref, shortName, icon: ItemIcon }) => {
-              const isSelected = key === guide;
-
-              return (
-                <DropdownMenuPrimitive.Item key={key} asChild={true}>
-                  <Anchor
-                    className={clsx(dropdownContentItemClassName, 'gap-2')}
-                    href={firstPageHref}
-                    variant="unstyled">
-                    <ItemIcon
-                      className={clsx(
-                        'size-4',
-                        isSelected ? themeTextBrandColor : themeTextSubtleColor,
-                      )}
-                    />
-                    <Text
-                      className="block"
-                      color={isSelected ? 'active' : 'default'}
-                      size="body2">
-                      {shortName}
-                    </Text>
-                  </Anchor>
-                </DropdownMenuPrimitive.Item>
-              );
-            },
+    <DropdownMenu
+      trigger={
+        <button
+          className={clsx(
+            'group inline-flex flex-1 items-center justify-between gap-2',
+            'px-2 py-1.5',
+            'transition-colors',
+            themeOutlineElementBrandColor_FocusVisible,
           )}
-        </DropdownMenuPrimitive.Content>
-      </DropdownMenuPrimitive.Portal>
-    </DropdownMenuPrimitive.Root>
+          type="button">
+          <div className="text-[0.8125rem] leading-4">
+            <Text
+              className="line-clamp-1 text-ellipsis text-left"
+              color="secondary"
+              size="inherit"
+              weight="medium">
+              {label}
+            </Text>
+          </div>
+          <RiArrowDownSLine
+            aria-hidden="true"
+            className={clsx('size-4 shrink-0', themeTextSubtleColor)}
+          />
+        </button>
+      }>
+      {Object.values(guides).map(
+        ({ key, firstPageHref, shortName, icon: ItemIcon }) => {
+          const isSelected = key === guide;
+
+          return (
+            <DropdownMenu.Item
+              key={key}
+              href={isSelected ? undefined : firstPageHref}
+              icon={ItemIcon}
+              isSelected={isSelected}
+              label={shortName}
+            />
+          );
+        },
+      )}
+    </DropdownMenu>
   );
 }
