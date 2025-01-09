0646a2e52bb0ffc51191f0150f47b60fb4203415
Nitesh Seram <niteshseram@gmail.com>
2025-01-09 16:02:50 +0530

    [web] auth: update auth password resent and unverified email page UI (#1170)

--- apps/web/src/components/auth/AuthPasswordResetSentPage.tsx
+++ apps/web/src/components/auth/AuthPasswordResetSentPage.tsx
@@ -5,11 +5,13 @@ import clsx from 'clsx';
 import { useAuthSignInUp } from '~/hooks/user/useAuthFns';
 
 import { useIntl } from '~/components/intl';
+import Anchor from '~/components/ui/Anchor';
 import Container from '~/components/ui/Container';
 import Heading from '~/components/ui/Heading';
+import Text from '~/components/ui/Text';
+import { themeRadialWhiteGlowBackground } from '~/components/ui/theme';
 
-import Anchor from '../ui/Anchor';
-import Text from '../ui/Text';
+import AuthEmailAsset from './AuthEmailAsset';
 
 type Props = Readonly<{
   next: string;
@@ -20,33 +22,36 @@ export default function AuthPasswordResetSentPage({ next }: Props) {
   const { signInUpHref } = useAuthSignInUp();
 
   return (
-    <Container
-      className={clsx('flex flex-col', 'py-8 md:py-12 lg:py-16')}
-      width="xl">
-      <Heading className="text-center" level="heading5">
-        {intl.formatMessage({
-          defaultMessage:
-            'An email containing the password reset instructions will be sent if an associated account exists',
-          description: 'Message indicating a successful password reset request',
-          id: 'Q0sZmk',
-        })}
-      </Heading>
-      <div className="mt-4 text-center">
-        <Text size="body2">
-          <Anchor
-            href={signInUpHref({
-              next,
-            })}
-            prefetch={null}>
-            Go back to sign in
-          </Anchor>
-        </Text>
-      </div>
-      <img
-        alt="Email envelope illustration"
-        className="max-w-80 mx-auto mt-16 block"
-        src="/img/marketing/envelope.svg"
-      />
-    </Container>
+    <div
+      className={clsx(
+        'size-full isolate before:!-top-[180px]',
+        themeRadialWhiteGlowBackground,
+      )}>
+      <Container
+        className={clsx('flex flex-col items-center', 'py-8 md:py-12 lg:py-16')}
+        width="xl">
+        <Heading className="text-center" level="heading5">
+          {intl.formatMessage({
+            defaultMessage:
+              'An email containing the password reset instructions will be sent if an associated account exists',
+            description:
+              'Message indicating a successful password reset request',
+            id: 'Q0sZmk',
+          })}
+        </Heading>
+        <div className="mb-10 mt-4 text-center">
+          <Text size="body2">
+            <Anchor
+              href={signInUpHref({
+                next,
+              })}
+              prefetch={null}>
+              Go back to sign in
+            </Anchor>
+          </Text>
+        </div>
+        <AuthEmailAsset />
+      </Container>
+    </div>
   );
 }
--- apps/web/src/components/auth/AuthUnverifiedEmailPage.tsx
+++ apps/web/src/components/auth/AuthUnverifiedEmailPage.tsx
@@ -4,11 +4,13 @@ import clsx from 'clsx';
 
 import { useAuthResendSignInConfirmation } from '~/hooks/user/useAuthResendSignInConfirmation';
 
+import Button from '~/components/ui/Button';
 import Container from '~/components/ui/Container';
 import Heading from '~/components/ui/Heading';
 import Text from '~/components/ui/Text';
+import { themeRadialWhiteGlowBackground } from '~/components/ui/theme';
 
-import Button from '../ui/Button';
+import AuthEmailAsset from './AuthEmailAsset';
 
 type Props = Readonly<{
   email: string;
@@ -19,38 +21,48 @@ export default function AuthUnverifiedEmailPage({ email, redirectTo }: Props) {
   const resendSignupConfirmationMutation = useAuthResendSignInConfirmation();
 
   return (
-    <Container
-      className={clsx('flex flex-col', 'py-8 md:py-12 lg:py-16')}
-      width="xl">
-      <Heading className="text-center" level="heading5">
-        Looks like you haven't verified your email address
-      </Heading>
-      <Text
-        className="text-balance mt-4 block text-center md:mt-6"
-        color="secondary"
-        size="body2">
-        Click the button below to receive an email containing a verification
-        link.
-      </Text>
-      <div className="mt-4 text-center">
-        <Button
-          isDisabled={resendSignupConfirmationMutation.isLoading}
-          isLoading={resendSignupConfirmationMutation.isLoading}
-          label="Send verification email"
-          variant="primary"
-          onClick={() => {
-            resendSignupConfirmationMutation.mutate({
-              email,
-              redirectTo,
-            });
-          }}
-        />
-      </div>
-      <img
-        alt="Email envelope illustration"
-        className="max-w-80 mx-auto mt-16 block"
-        src="/img/marketing/envelope.svg"
-      />
-    </Container>
+    <div
+      className={clsx(
+        'size-full isolate before:!-top-[180px]',
+        themeRadialWhiteGlowBackground,
+      )}>
+      <Container
+        className={clsx(
+          'flex flex-col items-center gap-10',
+          'py-8 md:py-12 lg:py-16',
+        )}
+        width="xl">
+        <Heading className="text-center" level="heading4">
+          Looks like you haven't verified your email address
+        </Heading>
+        <div>
+          <Text
+            className={clsx(
+              'text-balance mt-4 block text-center md:mt-6',
+              'text-sm sm:text-base',
+            )}
+            color="secondary"
+            size="inherit">
+            Click the button below to receive an email containing a verification
+            link.
+          </Text>
+          <div className="mt-4 text-center">
+            <Button
+              isDisabled={resendSignupConfirmationMutation.isLoading}
+              isLoading={resendSignupConfirmationMutation.isLoading}
+              label="Send verification email"
+              variant="primary"
+              onClick={() => {
+                resendSignupConfirmationMutation.mutate({
+                  email,
+                  redirectTo,
+                });
+              }}
+            />
+          </div>
+        </div>
+        <AuthEmailAsset />
+      </Container>
+    </div>
   );
 }
