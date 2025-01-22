4c5c26e181befea34fc26233aa94b5b192557828
Yangshun <tay.yang.shun@gmail.com>
2025-01-22 19:12:12 +0800

    [web] projects/challenge: fix duplicate keys issue

--- apps/web/src/components/projects/challenges/brief/support/ProjectsChallengeBriefGuidesResourcesSupportCard.tsx
+++ apps/web/src/components/projects/challenges/brief/support/ProjectsChallengeBriefGuidesResourcesSupportCard.tsx
@@ -1,27 +1,27 @@
 import { SidebarLinksList_DEPRECATED } from '~/components/global/sidebar/SidebarLinksList_DEPRECATED';
 import ProjectsChallengeBriefSupportCard from '~/components/projects/challenges/brief/support/ProjectsChallengeBriefSupportCard';
 
-export default function ProjectsChallengeBriefGuidesResourcesSupportCard() {
-  const links = [
-    {
-      items: [
-        {
-          label: 'How to use Figma for development',
-          slug: 'figma',
-        },
-        {
-          label: 'How to use DevTools for developments',
-          slug: 'github',
-        },
-        {
-          label: 'Connecting to various Cloud services',
-          slug: 'cloud',
-        },
-      ],
-      label: 'Official guides',
-    },
-  ];
+const links = [
+  {
+    items: [
+      {
+        label: 'How to use Figma for development',
+        slug: 'figma',
+      },
+      {
+        label: 'How to use DevTools for developments',
+        slug: 'github',
+      },
+      {
+        label: 'Connecting to various Cloud services',
+        slug: 'cloud',
+      },
+    ],
+    title: 'Official guides',
+  },
+];
 
+export default function ProjectsChallengeBriefGuidesResourcesSupportCard() {
   return (
     <ProjectsChallengeBriefSupportCard>
       <SidebarLinksList_DEPRECATED
