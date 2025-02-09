d6f1b9ef5952fae81bdd4eeb43e7f50bab80ce83
Yangshun <tay.yang.shun@gmail.com>
2025-02-09 11:37:26 +0800

    [web] mdx/code: remove usage of ScrollArea as it was causing extra vertical spacing

--- apps/web/src/components/mdx/MDXCodeBlock.tsx
+++ apps/web/src/components/mdx/MDXCodeBlock.tsx
@@ -19,8 +19,6 @@ import useHoverState from '~/hooks/useHoverState';
 import Button from '~/components/ui/Button';
 import TabsUnderline from '~/components/ui/Tabs/TabsUnderline';
 
-import ScrollArea from '../ui/ScrollArea';
-
 const MIN_LINES_TO_SHOW_LINE_NUMBERS = 5;
 
 type LanguagesCode = Partial<Record<Language, React.ReactNode>>;
@@ -51,12 +49,15 @@ export type Props = ComponentProps<'pre'> &
 function CopyButton({ contents }: Readonly<{ contents: string }>) {
   const [isCopied, onCopy] = useCopyToClipboardWithRevert(1000);
 
+  const label = isCopied ? 'Copied!' : 'Copy code to clipboard';
+
   return (
     <Button
       icon={isCopied ? FaCheck : RiFileCopyLine}
       isLabelHidden={true}
-      label={isCopied ? 'Copied!' : 'Copy code to clipboard'}
+      label={label}
       size="xs"
+      tooltip={label}
       variant="secondary"
       onClick={() => onCopy(contents)}
     />
@@ -149,76 +150,78 @@ export default function MDXCodeBlock({
           onSelect={setSelectedLanguage}
         />
       )}
-      <ScrollArea scrollbars="horizontal">
-        <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-          {isHovered && (
-            <div className="absolute right-1 top-6 flex gap-x-1 p-1">
-              {renderExtraButtons?.(selectedCode)}
-              {showCopyButton && <CopyButton contents={selectedCode} />}
-            </div>
-          )}
-          <Highlight
-            code={selectedCode.trim()}
-            language={selectedLanguage}
-            theme={themes.dracula}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => {
-              const showLineNumbersValue =
-                showLineNumbers == null
-                  ? tokens.length >= MIN_LINES_TO_SHOW_LINE_NUMBERS
-                  : true;
+      <div
+        className="relative"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}>
+        {isHovered && (
+          <div
+            className={clsx('flex gap-x-1 p-1', 'absolute right-0.5 top-0.5')}>
+            {renderExtraButtons?.(selectedCode)}
+            {showCopyButton && <CopyButton contents={selectedCode} />}
+          </div>
+        )}
+        <Highlight
+          code={selectedCode.trim()}
+          language={selectedLanguage}
+          theme={themes.dracula}>
+          {({ className, style, tokens, getLineProps, getTokenProps }) => {
+            const showLineNumbersValue =
+              showLineNumbers == null
+                ? tokens.length >= MIN_LINES_TO_SHOW_LINE_NUMBERS
+                : true;
 
-              return (
-                <pre
-                  className={clsx(
-                    'code-block__counter',
-                    className,
-                    showLineNumbersValue && 'pl-0',
-                  )}
-                  style={{
-                    ...style,
-                    // @ts-expect-error: CSS variable
-                    '--code-bg-color': style.backgroundColor,
-                    '--code-color': style.color,
-                  }}>
-                  {tokens.map((line, index) => {
-                    const lineKey = index;
-                    const { className: lineClassName, ...lineProps } =
-                      getLineProps({
-                        key: lineKey,
-                        line,
-                      });
+            return (
+              <pre
+                className={clsx(
+                  'code-block__counter',
+                  className,
+                  showLineNumbersValue && 'pl-0',
+                )}
+                style={{
+                  ...style,
+                  // @ts-expect-error: CSS variable
+                  '--code-bg-color': style.backgroundColor,
+                  '--code-color': style.color,
+                }}>
+                {tokens.map((line, index) => {
+                  const lineKey = index;
+                  const { className: lineClassName, ...lineProps } =
+                    getLineProps({
+                      key: lineKey,
+                      line,
+                    });
 
-                    const lineContents =
-                      renderLineContents != null
-                        ? renderLineContents({ getTokenProps, line })
-                        : line.map((token, tokenKey) => (
-                            <span
-                              {...getTokenProps({
-                                token,
-                              })}
-                              // eslint-disable-next-line react/no-array-index-key
-                              key={tokenKey}
-                            />
-                          ));
+                  const lineContents =
+                    renderLineContents != null
+                      ? renderLineContents({ getTokenProps, line })
+                      : line.map((token, tokenKey) => (
+                          <span
+                            {...getTokenProps({
+                              token,
+                            })}
+                            // eslint-disable-next-line react/no-array-index-key
+                            key={tokenKey}
+                          />
+                        ));
 
-                    return (
-                      <div
-                        className={clsx(
-                          showLineNumbersValue && 'code-line__counter',
-                          lineClassName,
-                        )}
-                        {...lineProps}
-                        key={lineKey}>
-                        {lineContents}
-                      </div>
-                    );
-                  })}
-                </pre>
-              );
-            }}
-          </Highlight>
-        </div>
-      </ScrollArea>
+                  return (
+                    <div
+                      className={clsx(
+                        showLineNumbersValue && 'code-line__counter',
+                        lineClassName,
+                      )}
+                      {...lineProps}
+                      key={lineKey}>
+                      {lineContents}
+                    </div>
+                  );
+                })}
+              </pre>
+            );
+          }}
+        </Highlight>
+      </div>
     </div>
   );
 }
