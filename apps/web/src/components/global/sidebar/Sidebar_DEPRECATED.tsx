08bbee2e408a97ab63843bffca7076a52722eaf3
Nitesh Seram <niteshseram@gmail.com>
2025-03-05 18:46:32 +0530

    [web] sponsorships: add sponsorships entrypoint to projects sidebar menu (#1273)

--- apps/web/src/components/global/sidebar/Sidebar_DEPRECATED.tsx
+++ apps/web/src/components/global/sidebar/Sidebar_DEPRECATED.tsx
@@ -6,8 +6,10 @@ import {
   RiMoreLine,
   RiPhoneLine,
   RiScales3Line,
+  RiShakeHandsLine,
 } from 'react-icons/ri';
 
+import { SPONSORSHIPS_AVAILABLE } from '~/data/FeatureFlags';
 import { SocialLinks } from '~/data/SocialLinks';
 
 import SidebarLinkItem_DEPRECATED from '~/components/global/sidebar/SidebarLinkItem_DEPRECATED';
@@ -24,6 +26,92 @@ import NavProductPopover_DEPRECATED from '../navbar/NavProductPopover_DEPRECATED
 export type SidebarItem = NavbarTopLevelItem;
 
 export type SidebarItems = ReadonlyArray<NavbarTopLevelItem>;
+
+export function SidebarDropdownMenu({
+  moreMenuItems,
+  moreEndMenuItems,
+}: Readonly<{
+  moreEndMenuItems?: React.ReactElement | false | null | undefined;
+  moreMenuItems?: React.ReactElement | false | null | undefined;
+}>) {
+  const intl = useIntl();
+
+  return (
+    <DropdownMenu
+      align="end"
+      icon={RiMoreLine}
+      isLabelHidden={true}
+      label="More"
+      showChevron={false}
+      side="right"
+      size="sm">
+      <SidebarColorSchemeSubMenu />
+      {moreMenuItems}
+      <DropdownMenu.Item
+        href="/contact"
+        icon={RiPhoneLine}
+        label={intl.formatMessage({
+          defaultMessage: 'Contact us',
+          description: 'Link to contact page',
+          id: 'dRUyU9',
+        })}
+      />
+      <DropdownMenu.Sub
+        icon={RiScales3Line}
+        label={intl.formatMessage({
+          defaultMessage: 'Legal',
+          description: 'Link to legal page',
+          id: 'J7b0BM',
+        })}>
+        <DropdownMenu.Item
+          href="/legal/privacy-policy"
+          label={intl.formatMessage({
+            defaultMessage: 'Privacy policy',
+            description: 'Link to privacy policy page',
+            id: 'RxU5TE',
+          })}
+        />
+        <DropdownMenu.Item
+          href="/legal/terms"
+          label={intl.formatMessage({
+            defaultMessage: 'Terms of service',
+            description: 'Link to terms of service page',
+            id: 'WYR3gj',
+          })}
+        />
+      </DropdownMenu.Sub>
+      {SPONSORSHIPS_AVAILABLE && (
+        <DropdownMenu.Sub
+          icon={RiShakeHandsLine}
+          label={intl.formatMessage({
+            defaultMessage: 'Partner',
+            description: 'Label for partner',
+            id: '9hx6+J',
+          })}>
+          <DropdownMenu.Item
+            href="/affiliates"
+            label={intl.formatMessage({
+              defaultMessage: 'Become an affiliate',
+              description: 'Link to affiliate marketing program page',
+              id: 'IZH9WZ',
+            })}
+          />
+          <DropdownMenu.Item
+            href="/advertise-with-us"
+            label={intl.formatMessage({
+              defaultMessage: 'Advertise with us',
+              description: 'Link to advertise with us',
+              id: '9OVmVF',
+            })}
+          />
+        </DropdownMenu.Sub>
+      )}
+      {moreEndMenuItems}
+      <Divider />
+      <SidebarAuthDropdownItem />
+    </DropdownMenu>
+  );
+}
 
 export function SidebarCollapsed({
   moreMenuItems,
@@ -78,43 +166,37 @@ export function SidebarCollapsed({
       <div className="flex flex-col items-center gap-6">
         {notificationItem}
         <div className="flex flex-col items-center gap-4">
-          <DropdownMenu
-            align="end"
-            icon={RiMoreLine}
-            isLabelHidden={true}
-            label="More"
-            showChevron={false}
-            side="right"
-            size="sm">
-            <SidebarColorSchemeSubMenu />
-            {moreMenuItems}
-            <Divider />
-            <DropdownMenu.Item
-              href={SocialLinks.linkedin.href}
-              icon={RiLinkedinFill}
-              label="LinkedIn"
-            />
-            {showPremiumDiscord ? (
-              <DropdownMenu.Item
-                color="active"
-                href={SocialLinks.discordPremium.href}
-                icon={SocialLinks.discordPremium.icon}
-                label={intl.formatMessage({
-                  defaultMessage: 'Premium Discord',
-                  description: 'Link to the premium Discord server',
-                  id: 'gvLQbK',
-                })}
-              />
-            ) : (
-              <DropdownMenu.Item
-                href={SocialLinks.discord.href}
-                icon={SocialLinks.discord.icon}
-                label="Discord"
-              />
-            )}
-            <Divider />
-            <SidebarAuthDropdownItem />
-          </DropdownMenu>
+          <SidebarDropdownMenu
+            moreEndMenuItems={
+              <>
+                <Divider />
+                <DropdownMenu.Item
+                  href={SocialLinks.linkedin.href}
+                  icon={RiLinkedinFill}
+                  label="LinkedIn"
+                />
+                {showPremiumDiscord ? (
+                  <DropdownMenu.Item
+                    color="active"
+                    href={SocialLinks.discordPremium.href}
+                    icon={SocialLinks.discordPremium.icon}
+                    label={intl.formatMessage({
+                      defaultMessage: 'Premium Discord',
+                      description: 'Link to the premium Discord server',
+                      id: 'gvLQbK',
+                    })}
+                  />
+                ) : (
+                  <DropdownMenu.Item
+                    href={SocialLinks.discord.href}
+                    icon={SocialLinks.discord.icon}
+                    label="Discord"
+                  />
+                )}
+              </>
+            }
+            moreMenuItems={moreMenuItems}
+          />
         </div>
         <Button
           icon={RiContractRightLine}
@@ -230,50 +312,7 @@ export function SidebarExpanded({
             />
           )}
           {bottomBarItems}
-          <DropdownMenu
-            icon={RiMoreLine}
-            isLabelHidden={true}
-            label="More"
-            showChevron={false}
-            size="sm">
-            <SidebarColorSchemeSubMenu />
-            {moreMenuItems}
-            <DropdownMenu.Item
-              href="/contact"
-              icon={RiPhoneLine}
-              label={intl.formatMessage({
-                defaultMessage: 'Contact us',
-                description: 'Link to contact page',
-                id: 'dRUyU9',
-              })}
-            />
-            <DropdownMenu.Sub
-              icon={RiScales3Line}
-              label={intl.formatMessage({
-                defaultMessage: 'Legal',
-                description: 'Link to legal page',
-                id: 'J7b0BM',
-              })}>
-              <DropdownMenu.Item
-                href="/legal/privacy-policy"
-                label={intl.formatMessage({
-                  defaultMessage: 'Privacy policy',
-                  description: 'Link to privacy policy page',
-                  id: 'RxU5TE',
-                })}
-              />
-              <DropdownMenu.Item
-                href="/legal/terms"
-                label={intl.formatMessage({
-                  defaultMessage: 'Terms of service',
-                  description: 'Link to terms of service page',
-                  id: 'WYR3gj',
-                })}
-              />
-            </DropdownMenu.Sub>
-            <Divider />
-            <SidebarAuthDropdownItem />
-          </DropdownMenu>
+          <SidebarDropdownMenu moreMenuItems={moreMenuItems} />
         </div>
         {onCollapseClick && (
           <Button
