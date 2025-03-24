8b154317cd5ccbbd489e5b0d3b81fb30690e21dc
Yangshun <tay.yang.shun@gmail.com>
2025-03-24 13:43:12 +0800

    [web] projects/challenges: remove users started label from card

--- apps/web/src/components/projects/stats/ProjectsStartedUsersTag.tsx
+++ apps/web/src/components/projects/stats/ProjectsStartedUsersTag.tsx
@@ -1,15 +1,13 @@
 import clsx from 'clsx';
 
-import { FormattedMessage, useIntl } from '~/components/intl';
+import { useIntl } from '~/components/intl';
 import UserAvatar from '~/components/ui/Avatar/UserAvatar';
-import Text from '~/components/ui/Text';
 import Tooltip from '~/components/ui/Tooltip';
 
 import type { ProjectsProfileAvatarDataSlim } from '../types';
 
 type Props = Readonly<{
   avatarBorderClassName: string;
-  className?: string;
   count: number | null;
   profiles: ReadonlyArray<ProjectsProfileAvatarDataSlim>;
 }>;
@@ -17,7 +15,6 @@ type Props = Readonly<{
 export default function ProjectsStartedUsersTag({
   avatarBorderClassName,
   count,
-  className,
   profiles,
 }: Props) {
   const intl = useIntl();
@@ -27,7 +24,19 @@ export default function ProjectsStartedUsersTag({
   }
 
   return (
-    <div className={clsx('flex items-center gap-1.5', className)}>
+    <Tooltip
+      label={intl.formatMessage(
+        {
+          defaultMessage:
+            '{startedCount, plural, one {A user} other {# users}} started working on this challenge',
+          description: 'Number of users who have started the challenge',
+          id: 'EfwT/A',
+        },
+        {
+          startedCount: count,
+        },
+      )}
+      triggerClassName="flex z-[1] text-nowrap">
       <div className="flex items-center -space-x-2.5">
         {profiles.map((profile) => (
           <UserAvatar
@@ -38,30 +47,6 @@ export default function ProjectsStartedUsersTag({
           />
         ))}
       </div>
-      <Tooltip
-        label={intl.formatMessage(
-          {
-            defaultMessage:
-              '{startedCount, plural, one {A user} other {# users}} started working on this challenge',
-            description: 'Number of users who have started the challenge',
-            id: 'EfwT/A',
-          },
-          {
-            startedCount: count,
-          },
-        )}
-        triggerClassName="flex z-[1] text-nowrap">
-        <Text color="subtle" size="body3">
-          <FormattedMessage
-            defaultMessage="{startedCount} started"
-            description="Label for number of users who have started the challenge"
-            id="L1cfzu"
-            values={{
-              startedCount: count,
-            }}
-          />
-        </Text>
-      </Tooltip>
-    </div>
+    </Tooltip>
   );
 }
