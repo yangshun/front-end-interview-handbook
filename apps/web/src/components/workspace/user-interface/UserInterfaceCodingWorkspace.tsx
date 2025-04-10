5d19c807bcaf1e0e4e4fb1739963fb4c92920d49
Harshit Pant <pantharshit00@gmail.com>
2025-04-10 16:12:04 +0530

    [web] workspace: delete non default files on workspace reset (#1373)

--- apps/web/src/components/workspace/user-interface/UserInterfaceCodingWorkspace.tsx
+++ apps/web/src/components/workspace/user-interface/UserInterfaceCodingWorkspace.tsx
@@ -76,6 +76,24 @@ import { useMonaco } from '@monaco-editor/react';
 const UserInterfaceCodingWorkspaceTilesPanelRoot =
   TilesPanelRoot<UserInterfaceCodingWorkspaceTabsType>;
 
+// Find files that are not in defaultFiles but exist in sandpack.files
+const getNonDefaultFiles = (
+  sandpackFiles: SandpackFiles,
+  defaultFiles: SandpackFiles,
+) => {
+  const nonDefaultFiles: Array<string> = [];
+
+  if (sandpackFiles) {
+    Object.keys(sandpackFiles).forEach((filePath) => {
+      if (!defaultFiles[filePath]) {
+        nonDefaultFiles.push(filePath);
+      }
+    });
+  }
+
+  return nonDefaultFiles;
+};
+
 function UserInterfaceCodingWorkspaceImpl({
   canViewPremiumContent,
   isViewingSave,
@@ -181,6 +199,14 @@ function UserInterfaceCodingWorkspaceImpl({
 
   function resetToDefaultCode() {
     deleteCodeFromLocalStorage();
+
+    const nonDefaultFiles = getNonDefaultFiles(sandpack.files, defaultFiles);
+
+    if (nonDefaultFiles.length > 0) {
+      nonDefaultFiles.forEach((filePath) => {
+        sandpack.deleteFile(filePath);
+      });
+    }
     sandpack.updateFile(defaultFiles);
   }
 
