97047da28642b72c854b3367b755ed0b22d40eea
Yangshun <tay.yang.shun@gmail.com>
2025-06-12 19:50:52 +0800

    [web] infra: don't load images from CDN

--- apps/web/src/utils/cdnUrl.ts
+++ apps/web/src/utils/cdnUrl.ts
@@ -1,15 +1,16 @@
 export function cdnUrl(pathname: string): string {
-  if (!pathname.startsWith('/')) {
-    throw new Error('Pathname must be a relative path');
-  }
+  // TODO: Don't load from CDN while we figure out production issue
+  // if (!pathname.startsWith('/')) {
+  //   throw new Error('Pathname must be a relative path');
+  // }
 
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
-    return `https://dev.gfecdn.net${pathname}`;
-  }
+  // if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+  //   return `https://dev.gfecdn.net${pathname}`;
+  // }
 
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-    return `https://www.gfecdn.net${pathname}`;
-  }
+  // if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+  //   return `https://www.gfecdn.net${pathname}`;
+  // }
 
   return pathname;
 }
