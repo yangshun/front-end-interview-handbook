42f333311207df9212f9f2ff31b9fae7df48b7b3
Nitesh Seram <niteshseram@gmail.com>
2025-07-02 16:00:33 +0530

    [web] projects/submission: add dev API to retake screenshots (#1515)

---
+++ apps/web/src/app/api/dev__/projects/screenshots/route.ts
@@ -0,0 +1,71 @@
+import { ProjectsChallengeSubmissionScreenshotStatus } from '@prisma/client';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+import { generateScreenshots } from '~/components/projects/utils/screenshotUtils';
+
+import prisma from '~/server/prisma';
+import { getErrorMessage } from '~/utils/getErrorMessage';
+
+export async function POST(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+
+  if (searchParams.get('api_route_secret') !== process.env.API_ROUTE_SECRET) {
+    return NextResponse.json(
+      { error: 'You are not authorized to call this API' },
+      { status: 401 },
+    );
+  }
+
+  const submissionId = searchParams.get('submission_id');
+
+  if (!submissionId) {
+    return NextResponse.json(
+      { error: 'Submission ID is required' },
+      { status: 400 },
+    );
+  }
+
+  const isShortId = submissionId.length <= 8;
+
+  try {
+    // Need to separate query because UUID fields can't be compared with short strings
+    // Prisma throws "invalid length: expected 32, found 8" error
+    const submission = await (isShortId
+      ? prisma.projectsChallengeSubmission.findUnique({
+          where: { shortId: submissionId },
+        })
+      : prisma.projectsChallengeSubmission.findUnique({
+          where: { id: submissionId },
+        }));
+
+    if (!submission) {
+      return NextResponse.json(
+        { error: 'Submission not found' },
+        { status: 404 },
+      );
+    }
+
+    const deploymentUrlsWithScreenshots = await generateScreenshots(
+      submissionId,
+      submission.deploymentUrls,
+    );
+
+    await prisma.projectsChallengeSubmission.update({
+      data: {
+        deploymentUrls: deploymentUrlsWithScreenshots,
+        screenshotStatus: ProjectsChallengeSubmissionScreenshotStatus.COMPLETED,
+      },
+      where: {
+        id: submission.id,
+      },
+    });
+
+    return NextResponse.json(deploymentUrlsWithScreenshots);
+  } catch (error) {
+    return NextResponse.json(
+      { error: getErrorMessage(error) },
+      { status: 500 },
+    );
+  }
+}
