2db141d1d7d9023bee666d01a7122748f581c356
Yangshun <tay.yang.shun@gmail.com>
2025-07-11 18:34:17 +0800

    [web] sponsorship/request: add more promo codes
    (cherry picked from commit d2c3f7c8364a408cdd5e0184c7ba5c7915f08704)

--- apps/web/src/components/sponsors/SponsorsPromoCodeConfig.ts
+++ apps/web/src/components/sponsors/SponsorsPromoCodeConfig.ts
@@ -5,8 +5,32 @@ export const SponsorsPromoCodeConfig: Record<
     percentOff: number;
   }
 > = {
-  TRIAL25: {
-    code: 'TRIAL25',
+  '10OFF': {
+    code: '10OFF',
+    percentOff: 10,
+  },
+  '15OFF': {
+    code: '15OFF',
+    percentOff: 15,
+  },
+  '20OFF': {
+    code: '20OFF',
+    percentOff: 20,
+  },
+  '25OFF': {
+    code: '25OFF',
     percentOff: 25,
+  },
+  '30OFF': {
+    code: '30OFF',
+    percentOff: 30,
+  },
+  '40OFF': {
+    code: '40OFF',
+    percentOff: 40,
+  },
+  '50OFF': {
+    code: '50OFF',
+    percentOff: 50,
   },
 } as const;
